5e3edb742cfbaaa7440a5db79259431f02798999
Julien Gacon <jgacon@student.ethz.ch>
2015-12-25 10:03:07 +0100

    constructor is now called differently

--- MathGL/FigureClass/main.cpp
+++ MathGL/FigureClass/main.cpp
@@ -1,4 +1,5 @@
 // this is a short example code showing how the library can be used
+# include <iostream>
 # include "figure.hpp"
 
 int main()
@@ -6,11 +7,10 @@ int main()
   Eigen::VectorXd x(5), y(5);
   x << 1,2,3,4,5;
   y << 1,2,1,2,5;
-  std::vector<double> v = {3, 1, 6}; // note: must be vector of doubles!
-  std::vector<double> w = {0.1, 1, 3};
+  std::vector<double> v = {3, 0.5, 6}; // note: must be vector of doubles!
+  std::vector<double> w = {0.3, 1, 3};
 
-  mglGraph gr;
-  Figure fig(&gr);
+  Figure fig;
   fig.title("Testplot");
   fig.plot(x, y, "r#+", "My data");
   fig.plot(v, w, "b*", "Other data");
