88e7e70903f796665f077576a4b849777a9dd173
Julien Gacon <jgacon@student.ethz.ch>
2015-12-25 10:20:12 +0100

    setlog must be called *before* plotting

--- MathGL/FigureClass/main.cpp
+++ MathGL/FigureClass/main.cpp
@@ -8,13 +8,13 @@ int main()
   x << 1,2,3,4,5;
   y << 1,2,1,2,5;
   std::vector<double> v = {3, 0.5, 6}; // note: must be vector of doubles!
-  std::vector<double> w = {0.3, 1, 3};
+  std::vector<double> w = {0.1, 1, 3};
 
   Figure fig;
   fig.title("Testplot");
+  fig.setlog(true, true);
   fig.plot(x, y, "r#+", "My data");
   fig.plot(v, w, "b*", "Other data");
-  fig.setlog(true, true);
   fig.grid();
   fig.xlabel("x-Axis");
   fig.legend();
