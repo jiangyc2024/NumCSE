0fa4299d0539a43d9bdaa94a6b3bcc3704d2e627
Julien Gacon <jgacon@student.ethz.ch>
2016-01-06 09:52:14 +0100

    added a legend to show that latex typesetting is supported

--- LectureCodes/ExampleProject/main.cpp
+++ LectureCodes/ExampleProject/main.cpp
@@ -8,7 +8,8 @@ int main()
   Eigen::VectorXd y = (1./(1 + x.array()*x.array())).matrix();
 
   Figure fig;
-  fig.plot(x, y, "b");
+  fig.plot(x, y, "b", "1/\\(1 + x^2)^{-1}"); // Latex typesetting is supported when typing '\\' before the commands
+  fig.legend();
   fig.save("runge.eps");
 
   return 0;
