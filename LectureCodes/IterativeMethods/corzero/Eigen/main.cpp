ec2aacf6061ab67fc7309ab786dd2d1e019baf17
Thomas Wolf <thommybss@onlinehome.de>
2016-03-06 03:32:32 +0100

    more sophisticated build system

--- LectureCodes/IterativeMethods/corzero/Eigen/main.cpp
+++ LectureCodes/IterativeMethods/corzero/Eigen/main.cpp
@@ -1,23 +1,14 @@
 #include <Eigen/Dense>
 #include <iostream>
+#include "corzero.hpp"
+#include "figure.hpp"
 
 int main()
 {
-	const unsigned int N = 15;
-
-	double x = 0.4; // initial value
-	Eigen::VectorXd y(N);
-
-	for (unsigned int i=0; i<N; ++i)
-	{
-		x = x + (cos(x)+1)/sin(x);
-		y(i) = x;
-	}
+	Eigen::VectorXd rates;
+	Eigen::VectorXd err;
 	
-	Eigen::VectorXd err = y - Eigen::VectorXd::Constant(N, x);
-	Eigen::VectorXd rate = err.bottomRows(N-1).cwiseQuotient(err.topRows(N-1));
-
-	std::cout << rate << std::endl;
+	corzero(0.4, rates, err);
 
 	return 0;
 }
