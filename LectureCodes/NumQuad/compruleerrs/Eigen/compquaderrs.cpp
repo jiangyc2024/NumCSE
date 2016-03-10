42d030f75f0b5ed047ead3b1994962575ee2679b
Julien Gacon <jgacon@student.ethz.ch>
2016-03-10 10:12:44 +0100

    few plot adjustments

--- LectureCodes/NumQuad/compruleerrs/Eigen/compquaderrs.cpp
+++ LectureCodes/NumQuad/compruleerrs/Eigen/compquaderrs.cpp
@@ -15,26 +15,31 @@ void errs() {
                       err_trap_f(steps), err_trap_g(steps);
 
   for (unsigned i = 0; i < steps; ++i) {
-    err_simp_f[i] = std::abs(simpson(f, 0, 1, steps) - I_ex_f);
-    err_simp_g[i] = std::abs(simpson(g, 0, 1, steps) - I_ex_g);
-    err_trap_f[i] = std::abs(trapezoidal(f, 0, 1, steps) - I_ex_ f);
-    err_trap_g[i] = std::abs(trapezoidal(g, 0, 1, steps) - I_ex_g);
+    err_simp_f[i] = std::abs(simpson(f, 0, 1, i) - I_ex_f);
+    err_simp_g[i] = std::abs(simpson(g, 0, 1, i) - I_ex_g);
+    err_trap_f[i] = std::abs(trapezoidal(f, 0, 1, i) - I_ex_f);
+    err_trap_g[i] = std::abs(trapezoidal(g, 0, 1, i) - I_ex_g);
   }
 
   mgl::Figure fig_f, fig_g;
   fig_f.title("NumQuad for f(t) = \\frac{1}{1 + (5t)^2}");
   fig_f.xlabel("\\# of intervals");
   fig_f.ylabel("|quadrature error|");
+  fig_f.setlog(true, true);
   fig_f.plot(err_simp_f).label("Simpson");
   fig_f.plot(err_trap_f).label("Trapezoidal");
+  fig_f.fplot("1/x^2/50","k:").label("O(x^2)");
+  fig_f.fplot("1/x^4/50","k|").label("O(x^4)");
   fig_f.legend();
   fig_f.save("compruleerr1");
 
   fig_g.title("NumQuad for f(t) = \\sqrt{t}");
   fig_g.xlabel("\\# of intervals");
   fig_g.ylabel("|quadrature error|");
+  fig_g.setlog(true, true);
   fig_g.plot(err_simp_g).label("Simpson");
   fig_g.plot(err_trap_g).label("Trapezoidal");
+  fig_g.fplot("1/x^2/20","k|").label("O(x^2)");
   fig_g.legend();
   fig_g.save("compruleerr2");
 
