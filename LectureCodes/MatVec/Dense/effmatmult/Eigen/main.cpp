de892ca293237db59d4df04e232f2ec95d7225ce
thomaset <thomaset@student.ethz.ch>
2016-03-20 19:11:20 +0100

    Eigen code for (tensorproduct) times (vector) smart and slow way

---
+++ LectureCodes/MatVec/Dense/effmatmult/Eigen/main.cpp
@@ -0,0 +1,14 @@
+#define NDEBUG true
+#include <Eigen/Dense>
+#include <figure/figure.hpp>
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include "timer.hpp"
+using namespace std::chrono;
+using namespace Eigen;
+#include "effmatmult.hpp"
+int main(){
+  dottenstiming();
+  return 0;
+}
