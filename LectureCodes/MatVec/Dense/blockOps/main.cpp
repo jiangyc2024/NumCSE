ea853ed403d35ea3b87505b117ea571a30ff37a9
thomaset <thomaset@student.ethz.ch>
2016-03-20 11:16:34 +0100

    blockOps in C++

---
+++ LectureCodes/MatVec/Dense/blockOps/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include "./blockOps.hpp"
+
+int main () {
+  Eigen::MatrixXd M = (10*Eigen::MatrixXd::Random(4,4)).cast<int>().cast<double>();
+  blockAccess(M);
+  return 0;
+}
