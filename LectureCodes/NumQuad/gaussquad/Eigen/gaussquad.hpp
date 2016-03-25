e7cb928d20d7739763a5e327431112bece3cdfda
Julien Gacon <jgacon@student.ethz.ch>
2016-03-25 23:58:19 +0100

    using adjoint ev-solver now

--- LectureCodes/NumQuad/gaussquad/Eigen/gaussquad.hpp
+++ LectureCodes/NumQuad/gaussquad/Eigen/gaussquad.hpp
@@ -3,8 +3,7 @@
 
 
 struct QuadRule {
-  Eigen::VectorXd nodes;
-  Eigen::VectorXd weights;
+  Eigen::VectorXd nodes, weights;
 };
 
 void gaussquad (const unsigned& n, QuadRule& qr)
@@ -14,22 +13,15 @@ void gaussquad (const unsigned& n, QuadRule& qr)
   Eigen::MatrixXd M = Eigen::MatrixXd::Zero(n, n);
   for (unsigned i = 1; i < n; ++i){
     const double b = i/std::sqrt(4.*i*i - 1.);
-    // set matrix entries of GW-Matrix
-    M(i, i - 1) = b;
     M(i - 1, i) = b;
+    // line 19 is optional as the EV-Solver only references 
+    // the lower triangular part of M
+    // M(i, i - 1) = b; 
   }
+  
+  // using EigenSolver for symmetric matrices, exploiting the structure
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(M);
 
-  // intitialize EigenSolver with our Matrix
-  Eigen::EigenSolver<Eigen::MatrixXd> eig(M);
-
-  // the matrix is symmetric and has only real eigenvalues but as this is
-  // a numeric procedure small complex parts can appear 
-  // -> we need to cut them off
-  qr.nodes = eig.eigenvalues().real();
-
-  // same goes for the eigenvectors
-  const Eigen::VectorXd temp = eig.eigenvectors().topRows<1>().real();
-  qr.weights = 2*temp.cwiseProduct(temp);
-
-  return;
+  qr.nodes = eig.eigenvalues();
+  qr.weights = 2*eig.eigenvectors().topRows<1>().pow(2);
 }
