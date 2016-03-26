b21a01f377d1882ffc2ca7afe6037a64d630ee00
Julien Gacon <jgacon@student.ethz.ch>
2016-03-26 12:10:56 +0100

    added plot of slopes

--- LectureCodes/Interpolation/hermintp1/Eigen/hermintp1.hpp
+++ LectureCodes/Interpolation/hermintp1/Eigen/hermintp1.hpp
@@ -1,4 +1,3 @@
-# include <iostream>
 # include <Eigen/Dense>
 # include <figure/figure.hpp>
 # include "../../hermloceval/Eigen/hermloceval.hpp"
@@ -14,7 +13,7 @@ void hermintp1(Function& f, const VectorXd& t) {
   const VectorXd h = t.tail(n - 1) - t.head(n - 1); 
 
   // evaluate f at the nodes and for plotting
-  VectorXd y(t.size()),
+  VectorXd y(n),
            tplot = VectorXd::LinSpaced(500, t(0), t(n - 1)),
            yplot(500);
   for (unsigned i = 0; i < n; ++i) y(i) = f(t(i));
@@ -31,9 +30,9 @@ void hermintp1(Function& f, const VectorXd& t) {
   
   mgl::Figure fig;
   fig.title("Hermite interpolation");
-  fig.legend();
+  fig.legend(0, 0);
   fig.plot(t, y, " ko").label("Data points");
-  fig.plot(tplot, yplot).label("Exact");
+  fig.plot(tplot, yplot).label("f(x)");
 
   // compute and plot the Hermite interpolant with slopes \texttt{c}
   for (unsigned j = 0; j < n - 1; ++j) {
@@ -42,6 +41,27 @@ void hermintp1(Function& f, const VectorXd& t) {
     hermloceval(vx, t(j), t(j+1), y(j), y(j+1), c(j), c(j+1), px);
     fig.plot(vx, px, "r");
   }
+  // manually adding label for pw hermite interpolants
+  fig.addlabel("Piecw. cubic interpolation polynomial", "r");
 
+  // plot segments indicating the slopes \Blue{$c_i$}
+  for (unsigned k = 1; k < n - 1; ++k) {
+    VectorXd t_sl(2), y_sl(2);
+    t_sl << t(k) - 0.3*h(k - 1), t(k) + 0.3*h(k);
+    y_sl << y(k) - 0.3*h(k - 1)*c(k), y(k) + 0.3*h(k)*c(k);
+    fig.plot(t_sl, y_sl, "k");
+  }
+  // slope segments at beginning and end
+  VectorXd t_sl_0(2), y_sl_0(2), 
+           t_sl_n(2), y_sl_n(2);
+
+  t_sl_0 << t(0), t(0) + 0.3*h(0);
+  y_sl_0 << y(0), y(0) + 0.3*h(0)*c(0);
+  t_sl_n << t(n - 1) - 0.3*h(n - 2), t(n - 1);
+  y_sl_n << y(n - 1) - 0.3*h(n - 2)*c(n - 1), y(n - 1);
+  fig.plot(t_sl_0, y_sl_0, "k");
+  fig.plot(t_sl_n, y_sl_n, "k");
+
+  // save figure
   fig.save("hermintp1");
 }
