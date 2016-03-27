be0e4ba7f9c4df16877e905db76c710e36c2626c
Julien Gacon <jgacon@student.ethz.ch>
2016-03-27 11:42:52 +0200

    matlabs polyval in c++

---
+++ LectureCodes/FunctionCollection/polyval.hpp
@@ -0,0 +1,24 @@
+# ifndef POLYVAL_HPP
+# define POLYVAL_HPP
+
+# include <Eigen/Dense>
+
+using Eigen::VectorXd;
+
+/* evaluate polynomial using horner scheme 
+ * coefficients in p must be given like: 
+ * f(x) = p0*x^n + p1*x^(n-1) + ... + pn
+ *
+ * IN:  p = coefficients as stated above
+ *      x = Eigen::VectorXd of points at which to evaluate
+ * OUT: y = f(x)                                           */
+void polyval(const VectorXd& p, const VectorXd& x, VectorXd& y) {
+  const VectorXd ones = VectorXd::Ones(x.size());
+  y = p(0)*ones;
+
+  for (unsigned i = 1; i < p.size(); ++i) {
+    y = x.cwiseProduct(y) + p(i)*ones;
+  }
+}
+
+# endif
