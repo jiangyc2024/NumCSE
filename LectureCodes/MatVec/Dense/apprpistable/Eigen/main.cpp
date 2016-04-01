948286336b7c6c3db413e048dd7470e65de8c6ed
thomaset <thomaset@student.ethz.ch>
2016-04-01 20:39:31 +0200

    Added: Stable recursion for area of regular n-gon in Eigen

---
+++ LectureCodes/MatVec/Dense/apprpistable/Eigen/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <iomanip>
+using namespace std;
+using namespace Eigen;
+#include "apprpistable.hpp"
+
+int main () {
+	std::cout << setprecision(15) << std::fixed;
+	MatrixXd res = apprpistable();
+	std::cout << res << std::endl;
+	return 0;
+}
