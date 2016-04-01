61a848f5a0765bacb5baf4e2b8923d544a7fad58
thomaset <thomaset@student.ethz.ch>
2016-04-01 17:06:01 +0200

    Added: Testing the accuracy of computed roots of a quadratic polynomial in Eigen

---
+++ LectureCodes/MatVec/Dense/compzero/Eigen/compzeros.hpp
@@ -0,0 +1,38 @@
+#pragma once
+//! Eigen Function for testing the computation of the zeros of a parabola
+void compzeros(){
+	int n = 100;
+	MatrixXd res(n,4);
+	VectorXd gamma = VectorXd::LinSpaced(n, 2,992);
+	for(int i = 0; i < n; ++i){
+		double alpha = -(gamma(i) + 1./gamma(i));
+		double beta = 1.;
+		VectorXd z1 =  zerosquadpol(alpha, beta);
+		VectorXd z2 = zerosquadpolstab(alpha, beta);
+		double ztrue = 1./gamma(i), z2true = gamma(i);
+		res(i,0) = gamma(i);
+		res(i,1) = std::abs((z1(0)-ztrue)/ztrue);
+		res(i,2) = std::abs((z2(0)-ztrue)/ztrue);
+		res(i,3) = std::abs((z1(1)-z2true)/z2true);
+	}
+	// Graphical output of relative error of roots computed by unstable
+	// implementation
+	mgl::Figure fig1;
+	fig1.setFontSize(3);
+	fig1.title("Roots of a parabola computed in an unstable manner");
+	fig1.plot(res.col(0),res.col(1), " +r").label("small root");
+	fig1.plot(res.col(0),res.col(3), " *b").label("large root");
+	fig1.xlabel("\\gamma");
+	fig1.ylabel("relative errors in \\xi_1, \\xi_2");
+	fig1.legend(0.05,0.95);
+	fig1.save("zqperrinstab");
+	// Graphical output of relative errors (comparison), small roots
+	mgl::Figure fig2;
+	fig2.title("Roundoff in the computation of zeros of a parabola");
+	fig2.plot(res.col(0), res.col(1), " +r").label("unstable");
+	fig2.plot(res.col(0), res.col(3), " *m").label("stable");
+	fig2.xlabel("\\gamma");
+	fig2.ylabel("relative errors in \\xi_1");
+	fig2.legend(0.05,0.95);
+	fig2.save("zqperr");
+}
