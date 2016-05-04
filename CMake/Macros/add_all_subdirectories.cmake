d5e88ef34138ac390a1c51cd1640988ca7b69fa4
Till Ehrengruber <till@ehrengruber.ch>
2016-05-04 18:43:27 +0200

    cmake targets created by add_executable_numcse are now prefixed by their project name cmake added utility list_replace
    cmake added hierarchical structure of CMakeLists.txt files to include subdirectories
    add ed
    cmake changed all calls in LectureCodes directory to add_executable_numcse
    cmake changed include_directories in LectureCodes to relative paths temporary
    fixed LectureCodes/SingleStepMethods/rungekutta/Eigen/rungekutta.hpp
    cmake added symlink in MathGL/FigureClass/figure due to precompiler include path mess...

--- CMake/Macros/add_all_subdirectories.cmake
+++ CMake/Macros/add_all_subdirectories.cmake
@@ -5,7 +5,17 @@ MACRO(ADD_ALL_SUBDIRECTORIES directory)
     if(EXISTS "${directory}/${SUBDIR}/CMakeLists.txt")
       add_subdirectory(${SUBDIR})
     else()
-      message(WARNING "Skipping ${directory}/${SUBDIR} because no CMakeLists.txt file was found")
+		# check whether the subdirectory contains cpp files
+		execute_process(COMMAND bash -c "find ${directory}/${SUBDIR} | grep .cpp"
+						  RESULT_VARIABLE contains_cpp_code
+						  OUTPUT_QUIET)
+
+		# if the directory contains cpp files we print a warning
+		if(${contains_cpp_code} EQUAL 0)
+			message("Skipping ${directory}/${SUBDIR} (containing cpp code) because no CMakeLists.txt file was found")
+		else() # otherwise just a debug message
+			#message("Skipping ${directory}/${SUBDIR} since it did not contain any cpp code (e.g. not ported yet)")
+		endif()		
     endif()
   endforeach()
 ENDMACRO()
--- CMake/Macros/add_executable_numcse.cmake
+++ CMake/Macros/add_executable_numcse.cmake
@@ -1,17 +1,23 @@
+include(CMake/Macros/list_replace.cmake)
+
 macro (add_executable_numcse _name)
-	message(STATUS "Executable ${_name}")
+    # cmake targets need to be unique
+    # as such we prefix every executable with the current project name
+    SET(parsed_name "${PROJECT_NAME}_${_name}") # add prefix
+    set(argv_parsed ${ARGV})
+    LIST_REPLACE(argv_parsed 0 "${parsed_name}")
 
 	# create directory that will contain the binary
 	set( TARGET_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-	string(REPLACE "${PROJECT_SOURCE_DIR}" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}" TARGET_RUNTIME_OUTPUT_DIRECTORY ${TARGET_RUNTIME_OUTPUT_DIRECTORY})
+	string(REPLACE "${CMAKE_SOURCE_DIR}" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}" TARGET_RUNTIME_OUTPUT_DIRECTORY ${TARGET_RUNTIME_OUTPUT_DIRECTORY})
 
     # invoke built-in add_executable
-    add_executable(${ARGV})
-    if (TARGET ${_name})
-    	set_target_properties(${_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${TARGET_RUNTIME_OUTPUT_DIRECTORY}")
-        target_link_libraries(${_name} ${MATHGL2_LIBRARIES})
-    	#add_dependencies(${_name} Eigen)
-    	#add_dependencies(${_name} MathGL)
+    add_executable(${argv_parsed})
+    if (TARGET ${parsed_name})
+    	set_target_properties(${parsed_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${TARGET_RUNTIME_OUTPUT_DIRECTORY}")
+        target_link_libraries(${parsed_name} ${MATHGL2_LIBRARIES} Figure)
+    	#add_dependencies(${parsed_name} Eigen)
+    	#add_dependencies(${parsed_name} MathGL)
     endif()
 endmacro()
