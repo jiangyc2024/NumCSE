2b9e4779243d3b0c9e5aa253698821c5a5b3108e
Till Ehrengruber <till@ehrengruber.ch>
2016-05-17 00:27:16 +0200

    cmake add_executable_numcse macro will now use unique (but nice) target names that resemble the directory path, but retain their output_name

--- CMake/Macros/add_executable_numcse.cmake
+++ CMake/Macros/add_executable_numcse.cmake
@@ -2,22 +2,37 @@ include(CMake/Macros/list_replace.cmake)
 
 macro (add_executable_numcse _name)
     # cmake targets need to be unique
-    # as such we prefix every executable with the current project name
-    SET(parsed_name "${PROJECT_NAME}_${_name}") # add prefix
+    # as such we prepend a prefix from the source path
+    #  convert to relative path
+    string(REGEX REPLACE "${CMAKE_SOURCE_DIR}/+" "" target_prefix "${CMAKE_CURRENT_SOURCE_DIR}")
+    #  stip eigen suffix
+    string(REGEX REPLACE "/Eigen$" "" target_prefix "${target_prefix}")
+    #  convert CamelCase to hyphen-seperated-lower-case
+    string(REGEX REPLACE "([a-z])([A-Z])" "\\1-\\2" target_prefix "${target_prefix}")
+    string(REGEX REPLACE "/" "_" target_prefix "${target_prefix}")
+    string(TOLOWER "${target_prefix}" target_prefix)
+    #  build target name from prefix and original target name 
+    set(target_name "${target_prefix}_${_name}")
+    #  reassemble argv for add_executable macro with new target name
     set(argv_parsed ${ARGV})
-    LIST_REPLACE(argv_parsed 0 "${parsed_name}")
+    LIST_REPLACE(argv_parsed 0 "${target_name}")
 
 	# create directory that will contain the binary
 	set( TARGET_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
+    # retrieve relative path of runtime output directory (e.g the directory to which the target is compiled to)
 	string(REPLACE "${CMAKE_SOURCE_DIR}" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}" TARGET_RUNTIME_OUTPUT_DIRECTORY ${TARGET_RUNTIME_OUTPUT_DIRECTORY})
 
     # invoke built-in add_executable
     add_executable(${argv_parsed})
-    if (TARGET ${parsed_name})
-    	set_target_properties(${parsed_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${TARGET_RUNTIME_OUTPUT_DIRECTORY}")
-        target_link_libraries(${parsed_name} ${MATHGL2_LIBRARIES} Figure)
-    	#add_dependencies(${parsed_name} Eigen)
-    	#add_dependencies(${parsed_name} MathGL)
+    # link with mathgl and figure class
+    if (TARGET ${target_name})
+    	set_target_properties(${target_name} PROPERTIES
+        RUNTIME_OUTPUT_DIRECTORY "${TARGET_RUNTIME_OUTPUT_DIRECTORY}"
+        OUTPUT_NAME "${_name}")
+
+        target_link_libraries(${target_name} ${MATHGL2_LIBRARIES} Figure)
+    	#add_dependencies(${target_name} Eigen)
+    	#add_dependencies(${target_name} MathGL)
     endif()
 endmacro()
