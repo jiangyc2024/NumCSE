4ae533753325101e5ac052b8854638de6c6d1cdc
Till Ehrengruber <till@ehrengruber.ch>
2016-05-18 10:30:57 +0200

    cmake added macro get_target_name_numcse cmake macro add_executable_numcse uses get_target_name_numcse now cmake we now enforce policy CMP0016 to avoid misconception of add_executable_numcse cmake disabled mathgl warnings cmake fixed build of mathgl on clang even if mathgl is installed system wide cmake fixed LectureCodes/DenseLinearAlgebra/CMakeLists.txt

--- CMake/Macros/add_executable_numcse.cmake
+++ CMake/Macros/add_executable_numcse.cmake
@@ -1,18 +1,11 @@
 include(CMake/Macros/list_replace.cmake)
+include(CMake/Macros/get_target_name_numcse.cmake)
 
 macro (add_executable_numcse _name)
     # cmake targets need to be unique
     # as such we prepend a prefix from the source path
-    #  convert to relative path
-    string(REGEX REPLACE "${CMAKE_SOURCE_DIR}/+" "" target_prefix "${CMAKE_CURRENT_SOURCE_DIR}")
-    #  stip eigen suffix
-    string(REGEX REPLACE "/Eigen$" "" target_prefix "${target_prefix}")
-    #  convert CamelCase to hyphen-seperated-lower-case
-    string(REGEX REPLACE "([a-z])([A-Z])" "\\1-\\2" target_prefix "${target_prefix}")
-    string(REGEX REPLACE "/" "_" target_prefix "${target_prefix}")
-    string(TOLOWER "${target_prefix}" target_prefix)
-    #  build target name from prefix and original target name 
-    set(target_name "${target_prefix}_${_name}")
+    # all of this is done by get_executable_name_numcse
+    get_target_name_numcse(${_name} target_name)
     #  reassemble argv for add_executable macro with new target name
     set(argv_parsed ${ARGV})
     LIST_REPLACE(argv_parsed 0 "${target_name}")
