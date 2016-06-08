32b74bd97084f5dcc75143688dd2d09618042e8e
thomaset <thomaset@student.ethz.ch>
2016-06-08 13:36:39 +0200

    correction of blockgs

--- LectureCodes/MatVec/Dense/blockgs/Eigen/blockgs_recursive.hpp
+++ LectureCodes/MatVec/Dense/blockgs/Eigen/blockgs_recursive.hpp
@@ -4,9 +4,9 @@
 void blockgs(MatrixXd &A, int n){
 	if(n > 1){
 		// \Red{rank-1 modification} of \Blue{$\VC$}
-		A.bottomRightCorner(n-1, n-1) -= A.block(A.rows()-n+1, A.cols()-n, n-1, 1)	* (A.block(A.rows()-n,A.cols()-n+1,1,n-1)/A(A.rows()-n,A.cols()-n));
+		A.bottomRightCorner(n-1, n) -= A.col(A.rows() - n).tail(n-1)
+			* A.row(A.rows()-n).tail(n) / A(A.rows()-n,A.rows()-n);
 		blockgs(A, n-1);
-		// set \Vd  to 0
-		A.block(A.rows()-n+1,A.rows()-n,n-1,1) = MatrixXd::Zero(n-1,1);
+		A.col(A.rows()-n).tail(n-1).setZero(); // set \Vd  to 0
 	}
 }
