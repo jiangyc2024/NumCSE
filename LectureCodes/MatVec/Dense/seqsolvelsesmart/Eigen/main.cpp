e62b16c7eb9b0c0423d566729ed7c71aeffd1ee1
thomaset <thomaset@student.ethz.ch>
2016-06-09 13:42:52 +0200

    Added seqsolvelsesmart in Eigen

---
+++ LectureCodes/MatVec/Dense/seqsolvelsesmart/Eigen/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <Eigen/Dense>
+using namespace std;
+using namespace Eigen;
+
+int main () {
+	int N = 10;
+	int n = 100;
+	VectorXd x(n);
+	x.setOnes();
+	MatrixXd A(n,n);
+	A.setRandom();
+	auto some_function = [](VectorXd &x){return x*42;};
+	VectorXd b = some_function(x);
+	
+	
+#pragma begin<0>
+// Setting: \Blue{$N \gg 1$}, 
+// large matrix \Blue{$\VA\in\bbK^{n,n}$}
+auto A_ludec = A.lu();
+for(int j=0; j<N; ++j){
+	x = A_ludec.solve(b);
+	b = some_function(x);
+}
+#pragma end<0>
+
+
+return 0;
+}
