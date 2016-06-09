f309f7af6ff559aa8e43df68e22e9dcaf1945d2a
thomaset <thomaset@student.ethz.ch>
2016-06-09 14:42:48 +0200

    renaming

--- LectureCodes/MatVec/Dense/linsolvetest/Eigen/plotting.m
+++ LectureCodes/MatVec/Dense/linsolvetest/Eigen/plotting.m
@@ -2,10 +2,10 @@ delimiterIn = ' ';
 headerlinesIn = 0;
 times = importdata('linsolvetest.dat', delimiterIn, headerlinesIn);
 
-figure('name','linsolvetest');
+figure('name','linsolvetest_cpp');
 loglog(times(:,1),times(:,2),'r+',times(:,1),times(:,3),'m*');
 xlabel('{\bf matrix size n}','fontsize',14); 
 ylabel('{\bf runtime for direct solver [s]}','fontsize',14);
 legend('naive lu()','triangularView lu()','location','northwest');
 
-print -depsc2 './linsolvetest.eps';
+print -depsc2 './linsolvetest_cpp.eps';
