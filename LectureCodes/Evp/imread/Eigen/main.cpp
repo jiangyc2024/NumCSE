e94ac0229bbe708040a84399d2c6381e1d0e35ee
Thomas Wolf <wolftho@student.ethz.ch>
2016-06-19 21:34:17 +0200

    segmentation init, readBMP method

---
+++ LectureCodes/Evp/imread/Eigen/main.cpp
@@ -0,0 +1,26 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <libgen.h>
+#include "imread.hpp"
+
+// tests the bmp read functions
+int main(int arc, char** argv)
+{
+	std::string path = std::string(dirname(argv[0])) +	"/test.bmp";
+	auto img = readBMP(path);
+
+	std::cout << "Size: " << img.rows() << " x " << img.cols() << std::endl << std::endl;
+	
+	for (int i=0; i <3; ++i)
+	{
+		std::cout << "color channel " << i << std::endl;
+		Eigen::MatrixXd G = getcolor(img,i);
+
+		std::cout << G << std::endl;
+	}
+
+	std::cout << "greyscale " << std::endl;
+	Eigen::MatrixXd G = greyscale(img);
+
+	std::cout << G << std::endl;
+}
