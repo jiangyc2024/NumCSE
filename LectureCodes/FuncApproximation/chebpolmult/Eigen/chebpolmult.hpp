e873b58dc4d901d4824cf8399b23ddb416776076
Julien Gacon <jgacon@student.ethz.ch>
2016-06-20 10:49:47 +0200

    corrected latex of a comment

--- LectureCodes/FuncApproximation/chebpolmult/Eigen/chebpolmult.hpp
+++ LectureCodes/FuncApproximation/chebpolmult/Eigen/chebpolmult.hpp
@@ -9,7 +9,7 @@ using Eigen::MatrixXd;
 // The values \Blue{$T_k(x_j)$}, are returned in row \Blue{$k+1$} of \texttt{V}.
 void chebpolmult(const int d, const RowVectorXd& x, MatrixXd& V) {
   const unsigned n = x.size();
-  V = MatrixXd::Ones(d + 1, n); // \Blue{$T_0$ \equiv 1}
+  V = MatrixXd::Ones(d + 1, n); // \Blue{$T_0 \equiv 1$}
   V.block(1, 0, 1, n) = x; // \Blue{$T_1(x) = x$}
   for (int k = 1; k < d; ++k) {
     RowVectorXd p = V.block(k, 0, 1, n), // $p = T_{k}$
