db06a0e49a0de1f8cd7b8bb44f3e68d725edbdaf
Thomas Wolf <wolftho@student.ethz.ch>
2016-06-21 02:55:43 +0200

    EVP update

--- LectureCodes/Evp/imgsegmat/Eigen/imgsegmat.hpp
+++ LectureCodes/Evp/imgsegmat/Eigen/imgsegmat.hpp
@@ -1,56 +1,75 @@
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
-
-
-
-std::pair<Eigen::MatrixXd, Eigen::MatrixXd> imgsetmat(Eigen::MatrixXd P)
+template <class SimilarityFunction>
+std::pair<Eigen::SparseMatrix<double>, Eigen::SparseMatrix<double>> 
+	imgsegmat(Eigen::MatrixXd P, SimilarityFunction Sfun)
 {
+	using Triplet = Eigen::Triplet<double>;
+
 	int n = P.rows();
 	int m = P.cols();
+	int N = n*m;
 
-	std::vector<Eigen::Triplet<double>> triplets(4*n*m);
-	spdata = zeros(4*n*m,1); spidx  = zeros(4*n*m,2);
+	std::vector<Triplet> WTriplets;
+	std::vector<Triplet> DTriplets;
+	WTriplets.reserve(4*N);
+	DTriplets.reserve(4*N);
+
 	int k = 1;
-	for (int ni=0; ni<=n; ++ni)
+	for (int ni=0; ni<n; ++ni)
 	{
-		for (int mi=0; mi<=m; ++mi)
+		for (int mi=0; mi<m; ++mi)
 		{
-			int mni = mi*n+ni;
-			if (ni-1>=0) 
+			int mni = mi*n+ni; //global index
+
+			// call Sfun with adjacent pixels
+			if (ni-1>=0) // bottom pixel
 			{
 				double value = Sfun(P(ni,mi),P(ni-1,mi));
-				triplets.push_back(Eigen::Triplet(mni, mni-1));
+				WTriplets.push_back(Triplet(mni, mni-1, value));
+				DTriplets.push_back(Triplet(mni, mni, value));
 				k = k + 1;
 			}
 			
-			if (ni+1<n)
+			if (ni+1<n) // top pixel
 			{
 				double value = Sfun(P(ni,mi),P(ni+1,mi));
-				triplets.push_back(Eigen::Triplet(mni, mni+1));
+				WTriplets.push_back(Triplet(mni, mni+1, value));
+				DTriplets.push_back(Triplet(mni, mni, value));
 				k = k + 1;
 			}
 			
-			if (mi-1>0)
+			if (mi-1>=0) // left pixel
 			{
-				spidx(k,:) = [mni,mni-n];
-				spdata(k) = Sfun(P(ni,mi),P(ni,mi-1));
+				double value = Sfun(P(ni,mi),P(ni,mi-1));
+				WTriplets.push_back(Triplet(mni, mni-n, value));
+				DTriplets.push_back(Triplet(mni, mni, value));
 				k = k + 1;
 			}
 
-			if (mi+1<=m), spidx(k,:) = [mni,mni+n];
-			spdata(k) = Sfun(P(ni,mi),P(ni,mi+1));
-			k = k + 1;
-			end
+			if (mi+1<m) // right pixel
+			{
+				double value = Sfun(P(ni,mi),P(ni,mi+1));
+				WTriplets.push_back(Triplet(mni, mni+n, value));
+				DTriplets.push_back(Triplet(mni, mni, value));
+				k = k + 1;
+			}
 		}
 	}
 
-	// Efficient initialization, see Sect.~\ref{sec:spml}, Ex.~\ref{ex:spinit}
-	Eigen::SparseMatrix<double> W(n*m,n*m);
-	W.setFromTriplets(triplets);
-	W.compress();
 
-	W = sparse(spidx(1:k-1,1),spidx(1:k-1,2),spdata(1:k-1),n*m,n*m);
-	D = spdiags(full(sum(W')'),[0],n*m,n*m);
+	// Efficient initialization, see Sect.~\ref{sec:spml}, Ex.~\ref{ex:spinit}
+	Eigen::SparseMatrix<double> W(N,N);
+	Eigen::SparseMatrix<double> D(N,N);
+	Eigen::SparseMatrix<double> A(N,N);
+
+	W.setFromTriplets(WTriplets.begin(), WTriplets.end());
+	D.setFromTriplets(DTriplets.begin(), DTriplets.end());
+	W.makeCompressed();
+	D.makeCompressed();
+
 	A = D-W;
- 
+	return std::make_pair(A,D);
 }
 
--- LectureCodes/Evp/imread/Eigen/imread.hpp
+++ LectureCodes/Evp/imread/Eigen/imread.hpp
@@ -7,7 +7,6 @@
 // modfied version of http://stackoverflow.com/questions/9296059/read-pixel-value-in-bmp-file
 Eigen::MatrixXi readBMP(std::string filename)
 {
-    int i;
     FILE* f = fopen(filename.c_str(), "rb");
 
 	if (f == NULL)
@@ -27,7 +26,6 @@ Eigen::MatrixXi readBMP(std::string filename)
 
 	int row_padded = (width*3 + 3) & (~3);
 	unsigned char* data = new unsigned char[row_padded];
-	unsigned char tmp;
 
 	for(int i = 0; i < height; i++)
 	{
