08344dacda6333ada2bef9da822fde1a775af33e
Julien Gacon <jgacon@student.ethz.ch>
2016-07-03 12:47:14 +0200

    shortened code a bit by writing commands in the same line

--- LectureCodes/FuncApproximation/trigpolyval/Eigen/trigpolyval.hpp
+++ LectureCodes/FuncApproximation/trigpolyval/Eigen/trigpolyval.hpp
@@ -6,6 +6,7 @@
 using Eigen::VectorXd;
 using Eigen::VectorXcd;
 
+/*LSTBEGIN0*/
 // Evaluation of trigonometric interpolant at numerous points
 // IN : \texttt{t} = vector of nodes \Blue{$t_{0},\ldots,t_n\in[0,1[$}
 //      \texttt{y} = vector of data \Blue{$y_{0},\ldots,y_{n}$}
@@ -18,7 +19,6 @@ void trigpolyval(const VectorXd& t, const VectorXd& y, const VectorXd& x, Vector
     return;
   }
   const int n = (N - 1)/2;
-
   const std::complex<double> i(0,1); // imaginary unit
   // interpolation nodes and evalutation points on unit circle
   VectorXcd tc = ( 2*M_PI*i*t ).array().exp().matrix(),
@@ -26,9 +26,9 @@ void trigpolyval(const VectorXd& t, const VectorXd& y, const VectorXd& x, Vector
   // Rescaled values, according to \Blue{$q(t) = e^{-2\pi int}\cdot p(e^{2\pi it})$}
   VectorXcd z = ((2*n*M_PI*i*t).array().exp() * y.array()).matrix();
   // Evaluation of interpolating polynomial on unit circle, see Code~\ref{barycentricformula}
-  VectorXcd p;
-  intpolyval(tc, z, xc, p);
+  VectorXcd p; intpolyval(tc, z, xc, p);
   // Undo the scaling, see \eqref{eq:scale} 
   VectorXcd qc = ((-2*n*M_PI*i*x).array().exp() * p.array()).matrix();
   q = qc.real(); // imaginary part is zero, cut it off
 }
+/*LSTEND0*/
