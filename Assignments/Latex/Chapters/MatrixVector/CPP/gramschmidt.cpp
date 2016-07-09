057c87f51e1c54cc7aa376c8307b6dc07dc8f734
Till Ehrengruber <till@ehrengruber.ch>
2016-07-10 00:14:45 +0200

    Maximum linewidth 80

--- Assignments/Latex/Chapters/MatrixVector/CPP/gramschmidt.cpp
+++ Assignments/Latex/Chapters/MatrixVector/CPP/gramschmidt.cpp
@@ -1,7 +1,8 @@
 #include <Eigen/Dense>
 #include <iostream>
 
-//! \brief Given a matrix A of linearly independent columns, returns Gram-Schmidt orthonormalization
+//! \brief Given a matrix A of linearly independent columns, returns 
+//         Gram-Schmidt orthonormalization
 //! Ustable GS algorithm. Output is prone to cancellation issues.
 //! \param[in] A Matrix of linearly independent columns
 //! \return Matrix with ONB of $span(a_1, \cdots, a_n)$
@@ -14,12 +15,15 @@ Matrix gramschmidt( const Matrix & A ) {
     Q.col(0).normalize();
     
     for(unsigned int j = 1; j < A.cols(); ++j) {
-        // Replace inner loop over each previous vector in Q with fast matrix-vector multiplication
+        // Replace inner loop over each previous vector in Q with fast 
+        //  matrix-vector multiplication
         Q.col(j) -= Q.leftCols(j) * (Q.leftCols(j).transpose() * A.col(j));
         
-        // Normalize vector if possible (othw. means colums of A almsost lin. dep.
+        // Normalize vector if possible (otherwise means colums of A 
+        //  almost linear dependant)
         if( Q.col(j).norm() <= 10e-14 * A.col(j).norm() ) {
-            std::cerr << "Gram-Schmidt failed because A has lin. dep columns. Bye." << std::endl;
+            std::cerr << "Gram-Schmidt failed because A has linear dependant"
+                      << " columns. Bye." << std::endl;
             break;
         } else {
             Q.col(j).normalize();
