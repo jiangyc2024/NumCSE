f6ce28a6845b343e71b4a2e38cda214448113efb
Julien Gacon <jgacon@student.ethz.ch>
2016-07-21 09:23:00 +0200

    updated timer.h

--- Assignments/Latex/Chapters/MatrixVector/CPP/timer.h
+++ Assignments/Latex/Chapters/MatrixVector/CPP/timer.h
@@ -1,76 +1,127 @@
-#include <chrono>
+# ifndef TIMER_HPP
+# define TIMER_HPP
 
-typedef std::chrono::high_resolution_clock::time_point time_t_;
+# include <chrono>
+# include <vector>
 
-//! Class implementing a simple chronometer, similar to tic-toc of Matlab
-//! Just hit start() to start the timer, stop() to increment the timer (with time since last start/stop or reset)
-//! Hit reset() to reset to 0 elapsed time
-//! Get elapsed time with elapsed() or averaged time with avg()
-template<typename duration_t_ = std::chrono::nanoseconds>
-class timer {
-public:
-//     timer() {
-//         elapsed_ = duration_t_::zero();
-//         leaps_ = 0;
-//     }
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * USAGE: Timer t;                                           *
+ *        t.start();                                         *
+ *        for (bla) { ... stuff happening ...; t.lap(); }    *
+ *        double min = t.min(),                              *
+ *               mean = t.mean();                            *
+ *                                                           *
+ *        OR                                                 *
+ *                                                           *
+ *        Timer t;                                           *
+ *        t.start();                                         *
+ *        ...  stuff happening ...                           *
+ *        t.stop();                                          *  
+ *                                                           *
+ *        NOTE: stop() and lap() are equivalent!             *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-    //! Just save current time
-    void start() {
-        start_ = std::chrono::high_resolution_clock::now();
-    }
-    
-    //! Increment timer by tme since last start, calls start()
-    void stop() {
-        auto tmp = std::chrono::duration_cast<duration_t_>( std::chrono::high_resolution_clock::now() - start_ );
-        if(tmp < min_ or leaps_ == 0) min_ = tmp;
-        elapsed_ += tmp;
-        ++leaps_;
-        start();
-    }
-    
-    //! Reset timer to 0, calls start()
-    void reset() {
-        leaps_ = 0;
-        elapsed_ = duration_t_::zero();
-        start();
-    }
-    
-    //! Get sum of all start()/stop() cycles
-    duration_t_ elapsed() const {
-        return elapsed_;
-    }
-    
-    //! Get min of all start()/stop() cycles
-    duration_t_ min() const {
-        return min_;
-    }
-    
-    //! Get average of all start()/stop() cycles
-    duration_t_ avg() const {
-        return elapsed_ / leaps_;
-    }
-    
-    //! Print elapsed time
-    void print() const {
-        std::cout << elapsed().count() << std::endl;
-    }
-    
-    //! Print averaged elapsed time
-    void print_avg() const {
-        std::cout << avg().count() << std::endl;
-    }
-    
-    //! Print min elapsed time
-    void print_min() const {
-        std::cout << min().count() << std::endl;
-    }
-    
-private:
-    //! Number of cycles
-    unsigned int    leaps_;
-    //! Time of start()
-    time_t_         start_;
-    //! Elapsed until last stop() and minimal duration
-    duration_t_     elapsed_, min_;
+class Timer{
+  typedef std::chrono::nanoseconds prec;
+  typedef std::chrono::high_resolution_clock clock;
+  typedef std::chrono::duration<double> duration_t;
+  private:
+    static const unsigned divisor = 1e9;
+    clock::time_point t_start, t_end;
+    duration_t t_min;
+    std::vector<duration_t> t_laps;
+
+  public:
+
+   Timer();
+   void start();
+   void stop();
+   void lap();
+   void reset();
+
+   double duration() const;
+   double mean() const;
+   double min() const;
 
 };
+
+// start the timer
+void Timer::start(){
+  t_start = clock::now();
+  t_end = t_start;
+}
+
+// stop the timer (equivalent to lap)
+void Timer::stop(){
+  // stop is just another lap
+  lap();
+}
+
+// new lap
+void Timer::lap(){
+  clock::time_point tmp = clock::now();
+
+  // get laptime
+  duration_t laptime;
+  laptime = tmp - t_end;
+  
+  // check if this lap was faster
+  if (t_min > laptime || t_laps.size() == 0) {
+    t_min = laptime;
+  }
+
+  // save time of this lap
+  t_laps.push_back(laptime);
+
+  // save total time 
+  t_end = tmp;
+}
+
+// idle constructor
+Timer::Timer() {}
+
+// resets all values
+void Timer::reset(){
+  t_laps = std::vector<duration_t>();
+  start();
+}
+
+// returns total duration timer has been running
+double Timer::duration() const {
+  if (t_laps.size() > 0){
+    // returning time in seconds! thats what the divisor is for
+    auto dur = std::chrono::duration_cast<prec>(t_end - t_start);
+    return double(dur.count())/divisor;
+  }
+  else {
+    std::cerr << "Before calling Timer::duration() you need to call Timer::lap() or Timer::stop()!\n";
+    return 0.;
+  }
+}
+
+// returns mean of all laps
+double Timer::mean() const {
+  if (t_laps.size() > 0){
+    // save total time in std::chrono units
+    auto total_time = t_laps[0];
+    for (unsigned int i = 1; i < t_laps.size(); ++i) {
+      total_time += t_laps[i];
+    }
+    // convert time to double
+    auto total_dur = std::chrono::duration_cast<prec>(total_time);
+    double avg = double(total_dur.count())/divisor;
+    return avg;
+  }
+  else {
+    std::cerr << "Before calling Timer::mean() you need to call Timer::lap() or Timer::stop()!\n";
+    return 0.;
+  }
+}
+
+// returns minimum of all laps
+double Timer::min() const {
+  auto min = std::chrono::duration_cast<prec>(t_min);
+  return double(min.count())/divisor;
+}
+
+# endif
