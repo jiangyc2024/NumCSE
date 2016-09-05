8364355aa687dd284dbce89edf9c7dbf5646b5cd
Julien Gacon <jgacon@student.ethz.ch>
2016-09-05 16:21:51 +0200

    adjusted style to solution

--- Assignments/Latex/Chapters/NumericalQuadrature/CPP/laserquad_template.cpp
+++ Assignments/Latex/Chapters/NumericalQuadrature/CPP/laserquad_template.cpp
@@ -3,7 +3,6 @@
 
 #include <iomanip>
 #include <iostream>
-
 #include <cmath>
 
 //! \brief Compute \int_a^b f(x) dx \approx \sum w_i f(x_i) (with scaling of w and x)
@@ -13,8 +12,8 @@
 //! \param[in] f integrand
 //! \param[in] Q Structure containing weights and nodes for a quadrature
 //! \return Approximation of integral \int_a^b f(x) dx
-template <class func>
-double evalgaussquad(double a, double b, func&& f, const QuadRule & Q) {
+template <class Function>
+double evalgaussquad(double a, double b, Function&& f, const QuadRule& Q) {
     // TODO: implement gauss quadrature
 }
 
@@ -23,24 +22,24 @@ double evalgaussquad(double a, double b, func&& f, const QuadRule & Q) {
 //! \param[in] f integrand, f(x,y) must be defined
 //! \param[in] N number of quadrature points (in each direction)
 //! \return Approximation of integral \int_\bigtriangleup f(x,b) dx dy
-template <class func>
-double gaussquadtriangle(func&& f, unsigned int N) {
+template <class Function>
+double gaussquadtriangle(Function&& f, unsigned N) {
     // TODO: compute double integral over triangle
 }
 
-int main(void) {
+int main() {
     // Parameters
-    double alpha = 1.;
-    double p = 0, q = 0;
+    const double alpha = 1.;
+    const double p = 0, q = 0;
     // Laser beam intensity
     auto I = [alpha, p, q] (double x, double y) { return std::exp(- alpha * ( (x-p)*(x-p) + (y-q)*(y-q) ) ); };
     
     // Max num of Gauss points to use (in each direction)
-    unsigned int max_N = 13;
+    const unsigned max_N = 13;
     // "Exact" integral
-    double I_ex = 0.366046550000405;
+    const double I_ex = 0.366046550000405;
     
-    for(unsigned int N = 1; N < max_N; ++N) {
+    for (unsigned N = 1; N < max_N; ++N) {
         // TODO: compute intrgral and error
     }
 }
