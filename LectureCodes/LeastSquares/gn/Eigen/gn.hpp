53d7dbe1ce80696043feb88ff6bb0e26b6a97d1e
Julien Gacon <jgacon@student.ethz.ch>
2016-09-07 16:04:12 +0200

    replaced templates by fixed types

--- LectureCodes/LeastSquares/gn/Eigen/gn.hpp
+++ LectureCodes/LeastSquares/gn/Eigen/gn.hpp
@@ -1,20 +1,17 @@
 #include <Eigen/Dense>
-#include <functional>
+#include <Eigen/QR>
+using Eigen::VectorXd; 
+using Eigen::MatrixXd;
 
-template<typename VecType, typename MatType>
-VecType gn(VecType x,
-           std::function<VecType(const VecType &)> F,
-           std::function<MatType(const VecType &)> J,
-           double tol) {
-    using namespace Eigen;
-
-    VecType s = J(x).householderQr().solve(F(x)); // \label{gn:2}
+template <class Function, class Jacobian>
+VectorXd gn(const VectorXd& init, const Function& F, const Jacobian& J, const double tol) {
+  VectorXd x = init;
+  VectorXd s = J(x).householderQr().solve(F(x)); // \label{gn:2}
+  x = x - s;
+  while (s.norm() > tol * x.norm()) { // \label{gn:term}
+    s = J(x).householderQr().solve(F(x)); // \label{gn:5}
     x = x - s;
-    while (s.norm() > tol * x.norm()) // \label{gn:term}
-    {
-        s = J(x).householderQr().solve(F(x)); // \label{gn:5}
-        x = x - s;
-    }
+  }
 
-    return x;
+  return x;
 }
