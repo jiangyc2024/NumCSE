83015d0f47e38b7a1c1beaffd13ba4ed918acff2
Julien Gacon <jgacon@student.ethz.ch>
2016-09-07 16:30:07 +0200

    removed templates & namespace

--- LectureCodes/LeastSquares/lsqtotal/Eigen/lsqtotal.hpp
+++ LectureCodes/LeastSquares/lsqtotal/Eigen/lsqtotal.hpp
@@ -1,27 +1,21 @@
-// computes only solution \Blue{$\Vx$} of fitted consistent LSE
-
-#include <Eigen/Dense>
 #include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
 
-template<typename VecType, typename MatType>
-VecType lsqtotal(const Eigen::MatrixBase<MatType> &A, const VecType &b)
-{
-    using namespace Eigen;
-    using index_t = typename MatrixBase<MatType>::Index;
-    using entry_t = typename MatrixBase<MatType>::Scalar;
-    const index_t m(A.rows()); // No. of rows
-    const index_t n(A.cols()); // No. of columns
+// computes only solution \Blue{$\Vx$} of fitted consistent LSE
+VectorXd lsqtotal(const MatrixXd& A, const VectorXd& b) {
+  const unsigned m = A.rows(), n = A.cols(); // No. of rows and columns
 
-    Matrix<entry_t, Dynamic, Dynamic> Ab(m, n + 1);
-    Ab << A, b; // Ab = [A,b]
-    auto V = Ab.jacobiSvd(ComputeThinU | ComputeThinV).matrixV(); // see \eqref{tlsq:1}
+  MatrixXd Ab(m, n + 1);
+  Ab << A, b; // Ab = [A,b]
+  MatrixXd V = Ab.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).matrixV(); // see \eqref{tlsq:1}
 
-    entry_t s = V(n, n);
-    if (s == 0)
-    {
-        std::cerr << "No solution" << std::endl;
-        exit(1);
-    }
+  double s = V(n, n);
+  if (s == 0) {
+    std::cerr << "No solution!\n"; exit(1);
+  }
 
-    return -V.col(n).head(n) / s; // see \eqref{tlsq:3};
+  return -V.col(n).head(n) / s; // see \eqref{tlsq:3};
 }
