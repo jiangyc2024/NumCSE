e5b51858cd553ff70db832523a1332520460ee55
Prof. Dr. Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2016-09-13 16:51:58 +0200

    Eiditing of comments for lecture document

--- LectureCodes/MatVec/Dense/gramschmidt/Eigen/gramschmidt.hpp
+++ LectureCodes/MatVec/Dense/gramschmidt/Eigen/gramschmidt.hpp
@@ -14,20 +14,20 @@ using namespace Eigen;
 //! @return Matrix with ONB of $span(a_1, \cdots, a_n)$
 template <class Matrix>
 Matrix gramschmidt( const Matrix & A ) {
-    Matrix Q = A;
-    // First vector just gets normalized
-    Q.col(0).normalize();
-    for(unsigned int j = 1; j < A.cols(); ++j) {
-        // Replace inner loop over each previous vector in Q with fast matrix-vector multiplication
-        Q.col(j) -= Q.leftCols(j) * (Q.leftCols(j).transpose() * A.col(j));// \Label{gscpp:op}
-        // Normalize vector if possible (othw. means colums of A almsost lin. dep.
-        if( Q.col(j).norm() <= 10e-14 * A.col(j).norm() ) {// \Label{gscpp:1}
-            std::cerr << "Gram-Schmidt failed because A has lin. dep columns. Bye." << std::endl;
-            break;
-        } else {
-            Q.col(j).normalize();
-        }
-    }
-    return Q;
+  Matrix Q = A;
+  // First vector just gets normalized, Line 1 of \eqref{GS}
+  Q.col(0).normalize();
+  for(unsigned int j = 1; j < A.cols(); ++j) {
+    // Replace inner loop over each previous vector in Q with fast
+    // matrix-vector multiplication (Lines 4, 5 of \eqref{GS})
+    Q.col(j) -= Q.leftCols(j) * (Q.leftCols(j).transpose() * A.col(j));// \Label{gscpp:op}
+    // Normalize vector, if possible.
+    // Otherwise colums of A must have been linearly dependent
+    if( Q.col(j).norm() <= 10e-14 * A.col(j).norm() ) { // \Label{gscpp:1}
+      cerr << "Gram-Schmidt failed: A has lin. dep columns." << endl;
+      break;
+    } else { Q.col(j).normalize(); } // Line 7 of \eqref{GS}
+  }
+  return Q;
 }
 /* SAM_LISTING_END_0 */
--- LectureCodes/MatVec/Dense/lrtrimulteff/Eigen/lrtrimulteff.hpp
+++ LectureCodes/MatVec/Dense/lrtrimulteff/Eigen/lrtrimulteff.hpp
@@ -9,10 +9,11 @@ using namespace Eigen;
 
 /* SAM_LISTING_BEGIN_0 */
 //! Computation of $\Vy = \operatorname{triu}(\VA\VB^{T})\Vx$
-//! Efficient implementation with backward cumulative sum (partial\_sum)
+//! Efficient implementation with backward cumulative sum
+//! (partial\_sum)
 template<class Vec, class Mat>
 void lrtrimulteff(const Mat& A, const Mat& B, const Vec& x, Vec& y){
-  int n = A.rows(), p = A.cols();
+  const int n = A.rows(), p = A.cols();
   assert( n == B.rows() && p == B.cols()); // size missmatch
   for(int l = 0; l < p; ++l){
     Vec tmp = (B.col(l).array() * x.array()).matrix().reverse();
