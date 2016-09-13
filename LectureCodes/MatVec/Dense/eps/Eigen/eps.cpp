795008988cfbc6d09fcbbbb2009127fda007e837
Prof. Dr. Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2016-09-13 18:31:47 +0200

    Editing of comments and line formatting for inclusion in lecture document

--- LectureCodes/MatVec/Dense/eps/Eigen/eps.cpp
+++ LectureCodes/MatVec/Dense/eps/Eigen/eps.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
 #include <limits> // get various properties of arithmetic types
-int main(){	std::cout.precision(15);
-	std::cout << std::numeric_limits<double>::epsilon() << std::endl;}
+int main() {
+  std::cout.precision(15);
+  std::cout << std::numeric_limits<double>::epsilon() << std::endl;
+}
