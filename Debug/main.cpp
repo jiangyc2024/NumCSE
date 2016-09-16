7c71de5c7acbcf3431b849632af68e5f0762dea6
Julien Gacon <jgacon@student.ethz.ch>
2016-09-16 12:09:15 +0200

    Debug for scaletiming,hpp
    some tests using different fonts and different font sizes
    using plain mathgl aswell as the figure wrapper

---
+++ Debug/main.cpp
@@ -0,0 +1,11 @@
+#include "scaletiming.hpp"
+#include "scaletiming1.hpp"
+#include "scaletiming2.hpp"
+int main(){
+  scaletiming();
+  scaletiming1();
+  scaletiming2("");
+  scaletiming2("none");
+  scaletiming2("heros");
+  return 0;
+}
