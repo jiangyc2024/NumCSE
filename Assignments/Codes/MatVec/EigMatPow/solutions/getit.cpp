ddb372d98eecd64c61f4e5bdabb16f90861cef43
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-09-27 16:31:51 +0200

    Sinh templates

---
+++ Assignments/Codes/MatVec/EigMatPow/solutions/getit.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+#include <unsupported/Eigen/MatrixFunctions>
+
+using namespace Eigen;
+
+/* \brief Performs the computation $y = A^k x$.
+ */
+/* SAM_LISTING_BEGIN_1 */
+VectorXd getit(const MatrixXd &A, const VectorXd &x, unsigned int k) {
+
+    // As said in the problem formulation, we may assume that the
+    // following three lines have complexity $O(n^3)$.
+    EigenSolver<MatrixXd> eig = EigenSolver<MatrixXd>(A);
+    const VectorXcd & V = eig.eigenvalues();
+    const MatrixXcd & W = eig.eigenvectors();
+
+    // The following operation requires only a loop over
+    // the dimension of $cx$, which is $n$.
+    VectorXcd cx = x.cast<std::complex<double>>();
+
+    // The first operator* is a matrix vector multiplication
+    // with complexity $O(n^2)$.
+    VectorXcd ret = W *
+        // The componentwise power has complexity $O(n^2)$.
+        // The second operator* is a vector-vector componentwise
+        // multiplication, with complexity $O(n)$.
+        (
+          V.array().pow(k) *
+          // In the following line, a linear system is solved, operation
+          // with complexity $O(n^3)$
+          (W.partialPivLu().solve(cx)).array()
+        ).matrix();
+
+    return ret.real();
+}
+/* SAM_LISTING_END_1 */
+
+int main() {
+    // Some arbitrary data to test getit
+    MatrixXd A(4,4);
+    A << 1,  2,  3,  4,
+         5,  6,  7,  8,
+         9,  10, 11, 12,
+         13, 14, 15, 16;
+    VectorXd x(4);
+    x << 4,  5,  6,  7;
+    unsigned int  k = 9;
+
+    // Testing the implementation with some matrix
+    VectorXd yg = getit(A, x, k);
+    std::cout << "getit(A,x, k) = " << std::endl
+              << yg << std::endl;
+
+    // Checking that getit works
+    VectorXd yp = A.pow(k)*x;
+    std::cout << "A^k x = " << std::endl
+              << yp << std::endl;
+    double err = (yg - yp).norm() / yp.norm();
+    std::cout << "Relative error = " << err << std::endl;
+}
---
+++ Assignments/Codes/MatVec/Sinh/templates_nolabels/sinh.cpp
@@ -0,0 +1,26 @@
+//// 
+//// Copyright (C) 2016 SAM (D-MATH) @ ETH Zurich
+//// Author(s): lfilippo <filippo.leonardi@sam.math.ethz.ch> 
+//// Contributors: tille, jgacon
+//// This file is part of the NumCSE repository.
+//// Report issues to: https://gitlab.math.ethz.ch/NumCSE/NumCSE/issues
+////
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+
+int main() {
+
+    // Lambda function, call with sinh(x)
+    /* SAM_LISING_BEGIN_1 */
+    auto sinh = [] (double x) {
+        double t = std::exp(x);
+        return .5 * (t - 1./t);
+    };
+    /* SAM_LISING_END_1 */
+
+    /* SAM_LISING_BEGIN_2 */
+    // TODO: compute relative error for $10^{-k}, k  =1,...10$
+    /* SAM_LISING_END_2 */
+}
