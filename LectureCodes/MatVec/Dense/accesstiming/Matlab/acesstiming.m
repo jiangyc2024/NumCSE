df934ee314120bac3f14a3f833858813e262c2cf
thomaset <thomaset@student.ethz.ch>
2016-09-27 18:45:30 +0200

    Redoing of accesstiming with optimization flags

--- LectureCodes/MatVec/Dense/accesstiming/Matlab/acesstiming.m
+++ LectureCodes/MatVec/Dense/accesstiming/Matlab/acesstiming.m
@@ -2,7 +2,7 @@
 % We conduct K runs in order to reduce the risk of skewed maesurements
 % due to OS activity during MATLAB run.
 K = 3; res = [];
-for n=2.^(4:13)
+for n=2.^(5:13)
   A = randn(n,n);
   
   t1 = realmax; 
@@ -17,18 +17,19 @@ for n=2.^(4:13)
   end
   res = [res; n, t1 , t2];
 end
+save('../Eigen/timing_matlab.dat','res','-ascii')
 
 % Plot runtimes versus matrix sizes
-figure; plot(res(:,1),res(:,2),'r+', res(:,1),res(:,3),'m*');
-xlabel('{\bf n}','fontsize',14); 
-ylabel('{\bf runtime [s]}','fontsize',14); 
-legend('A(:,j+1) = A(:,j+1) - A(:,j)','A(i+1,:) = A(i+1,:) - A(i,:)',...
-       'location','northwest');
-print -depsc2 '../PICTURES/accessrtlin.eps';
-
-figure; loglog(res(:,1),res(:,2),'r+', res(:,1),res(:,3),'m*');
-xlabel('{\bf n}','fontsize',14); 
-ylabel('{\bf runtime [s]}','fontsize',14); 
-legend('A(:,j+1) = A(:,j+1) - A(:,j)','A(i+1,:) = A(i+1,:) - A(i,:)',...
-       'location','northwest');
-print -depsc2 '../PICTURES/accessrtlog.eps';
+% figure; plot(res(:,1),res(:,2),'r+', res(:,1),res(:,3),'m*');
+% xlabel('{\bf n}','fontsize',14); 
+% ylabel('{\bf runtime [s]}','fontsize',14); 
+% legend('A(:,j+1) = A(:,j+1) - A(:,j)','A(i+1,:) = A(i+1,:) - A(i,:)',...
+%        'location','northwest');
+% print -depsc2 '../PICTURES/accessrtlin.eps';
+% 
+% figure; loglog(res(:,1),res(:,2),'r+', res(:,1),res(:,3),'m*');
+% xlabel('{\bf n}','fontsize',14); 
+% ylabel('{\bf runtime [s]}','fontsize',14); 
+% legend('A(:,j+1) = A(:,j+1) - A(:,j)','A(i+1,:) = A(i+1,:) - A(i,:)',...
+%        'location','northwest');
+% print -depsc2 '../PICTURES/accessrtlog.eps';
