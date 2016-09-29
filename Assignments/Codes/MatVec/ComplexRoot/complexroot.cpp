c7b7d20933c7628dd734109fa561762812d51e7f
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-09-29 21:41:38 +0200

    CMAke

--- Assignments/Codes/MatVec/ComplexRoot/complexroot.cpp
+++ Assignments/Codes/MatVec/ComplexRoot/complexroot.cpp
@@ -5,35 +5,40 @@
 //! \brief Compute complex root
 //! \param[in] w complex number with non negative imaginary parts
 //! \return the square root of w with non negative real and imaginary parts
+/* SAM_LISTING_BEGIN_1 */
 std::complex<double> myroot( std::complex<double> w ) {
     /* SAM_LISTING_BEGIN_1 */
+    #if SOLUTION
     double x,y;
     double u = w.real();
     double v = w.imag();
 
-    if (v==0) return sqrt(u);
+    if (v==0) return std::sqrt(u);
 
     if (u > 0) {
-        x = sqrt((sqrt(u*u+v*v)+u)/2.);
+        x = std::sqrt((std::sqrt(u*u+v*v)+u)/2.);
         y = v/(2*x);
     } else {
-        y = sqrt((sqrt(u*u+v*v)-u)/2.);
+        y = std::sqrt((std::sqrt(u*u+v*v)-u)/2.);
         x = v/(2*y);
     }
 
     return std::complex<double> (x,y);
-    /* SAM_LISTING_END_1 */
+#else // TEMPLATE
+    // TODO: Implement complex root without cancellation risk
+#endif // TEMPLATE
 }
+/* SAM_LISTING_END_1 */
 
 // Test the implementation
 int main() {
-    std::cout << "*** PROBLEM 1, testing:" << std::endl;
+    std::cout << "--> Correctness test:" << std::endl;
 
-    std::complex<double> w(1e20,5);
+    std::complex<double> w(1e20, 5);
     std::cout << "The square root of " << w << " is " << myroot(w) << std::endl ;
-    std::cout << "The correct square root of " << w << " is " << sqrt(w) <<std::endl <<std::endl;
+    std::cout << "The correct square root of " << w << " is " << std::sqrt(w) <<std::endl <<std::endl;
 
-    w=std::complex<double>(-5,1e20);
+    w = std::complex<double>(-5, 1e20);
     std::cout << "The square root of " << w << " is " << myroot(w) << std::endl ;
-    std::cout << "The correct square root of " << w << " is " << sqrt(w) <<std::endl <<std::endl;
+    std::cout << "The correct square root of " << w << " is " << std::sqrt(w) <<std::endl <<std::endl;
 }
