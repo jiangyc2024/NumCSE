a4ceb93e43af02426aca13d3922b638fa3f3c82d
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-09-29 07:44:45 +0200

    Fixed author

--- Assignments/Codes/DirectLSE/Lyapunov/templates_nolabels/rankoneinvit.cpp
+++
@@ -1,7 +0,0 @@
-//// 
-//// Copyright (C) 2016 SAM (D-MATH) @ ETH Zurich
-//// Author(s): lfilippo <filippo.leonardi@sam.math.ethz.ch> 
-//// Contributors: tille, jgacon, dcasati
-//// This file is part of the NumCSE repository.
-//// Report issues to: https://gitlab.math.ethz.ch/NumCSE/NumCSE/issues
-////
