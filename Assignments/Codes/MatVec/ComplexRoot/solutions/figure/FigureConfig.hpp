11c27e0bb4a91b0c8d649e22dc8f095d84e7a02c
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-09-30 09:55:41 +0200

    Now including figure in asisgments directories

---
+++ Assignments/Codes/MatVec/ComplexRoot/solutions/figure/FigureConfig.hpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////
+// (c) Seminar of Applied Mathematics ETH 2016, D-MATH
+// Author: Julien Gacon
+// Co-Author: Baranidharan Mohan 
+///////////////////////////////////////////////////////
+
+
+/* * * * * * * * * * * * * * * * * * * *
+ *    CONFIGURATION FILE FOR FIGURE    *
+ * * * * * * * * * * * * * * * * * * * */
+
+// enable Figure to use Eigen
+# define FIG_HAS_EIGEN 1
