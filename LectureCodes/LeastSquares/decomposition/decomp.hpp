e4edb59deeb21af0658f71c001a24296176a0c18
Prof. Dr. Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2016-09-30 20:47:23 +0200

    New functions and driver codes for demonstrating use of QR-decomposition in Eigen

--- LectureCodes/LeastSquares/decomposition/decomp.hpp
+++ LectureCodes/LeastSquares/decomposition/decomp.hpp
@@ -1,16 +1,33 @@
 # include <Eigen/Dense>
-# include <Eigen/SVD>
-# include <Eigen/QR>
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 
-// get Q,R such that $A = Q R$
-void qr_decomp(const MatrixXd& A, MatrixXd& Q, MatrixXd& R) {
+/* SAM_LISTING_BEGIN_0 */
+# include <Eigen/QR>
+
+// Computation of \com{full QR-decomposition} \eqref{eq:fullqr},
+// dense matrices built for both QR-factors (expensive!)
+std::pair<MatrixXd,MatrixXd> qr_decomp_full(const MatrixXd& A) {
   Eigen::HouseholderQR<MatrixXd> qr(A);
-  Q = qr.householderQ();
-  R = qr.matrixQR().template triangularView<Eigen::Upper>();
+  MatrixXd Q = qr.householderQ();
+  MatrixXd R = qr.matrixQR().template triangularView<Eigen::Upper>();
+  return std::pair<MatrixXd,MatrixXd>(Q,R);
 }
+
+// Computation of \com{economical QR-decomposition} \eqref{eq:ecoqr},
+// dense matrix built for Q-factor (possibly expensive!)
+std::pair<MatrixXd,MatrixXd> qr_decomp_eco(const MatrixXd& A) {
+  using index_t = MatrixXd::Index;
+  const index_t m = A.rows(),n = A.cols();
+  Eigen::HouseholderQR<MatrixXd> qr(A); 
+  MatrixXd Q = (qr.householderQ()*MatrixXd::Identity(m,n)); // \Label[line]{cqr:e1}
+  MatrixXd R = qr.matrixQR().block(0,0,n,n).template triangularView<Eigen::Upper>(); // \Label[line]{cqr:e2}
+  return std::pair<MatrixXd,MatrixXd>(Q,R);
+}
+/* SAM_LISTING_END_0 */
+
+# include <Eigen/SVD>
 
 // get U,S,V such that $A = U S V^H$
 void svd_decomp(const MatrixXd& A, MatrixXd& U, MatrixXd& S, MatrixXd& V) {
@@ -22,6 +39,6 @@ void svd_decomp(const MatrixXd& A, MatrixXd& U, MatrixXd& S, MatrixXd& V) {
   V = svd.matrixV(); // get complete matrix V 
   VectorXd sv = svd.singularValues(); // get singular values as vector
   S = MatrixXd::Zero(A.rows(), A.cols());
-  const unsigned n = s.size(); // no. of singular values
+  const unsigned n = sv.size(); // no. of singular values
   S.block(0,0,n,n) = sv.asDiagonal(); // build matrix S
 }
