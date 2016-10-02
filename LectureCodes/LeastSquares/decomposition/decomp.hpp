112b66a8afafa82684254700a44a18f2f374fef2
Prof. Dr. Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2016-10-02 21:00:02 +0200

    Added new driver codes and demonstration functions for the use of SVD

--- LectureCodes/LeastSquares/decomposition/decomp.hpp
+++ LectureCodes/LeastSquares/decomposition/decomp.hpp
@@ -34,18 +34,34 @@ std::pair<MatrixXd,MatrixXd> qr_decomp_eco(const MatrixXd& A) {
 }
 /* SAM_LISTING_END_0 */
 
+/* SAM_LISTING_BEGIN_1 */
 # include <Eigen/SVD>
 
-// get U,S,V such that $A = U S V^H$
-void svd_decomp(const MatrixXd& A, MatrixXd& U, MatrixXd& S, MatrixXd& V) {
-  // Important! If replacing 'ComputeFull' by 'ComputeThin', 
-  // the methods matrixU() and matrixV() will return the 
-  // matrices U and V but not extended to an orthonormal basis!
+// Computation of (full) SVD \Blue{$\VA=\VU\Sigmabf\VV^\herm$} $\to$ \cref{def:SVD}
+// SVD factors are returned as dense matrices in natural order
+std::tuple<MatrixXd,MatrixXd,MatrixXd> svd_full(const MatrixXd& A) {
   Eigen::JacobiSVD<MatrixXd> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
-  U = svd.matrixU(); // get complete matrix U 
-  V = svd.matrixV(); // get complete matrix V 
+  MatrixXd U = svd.matrixU(); // get unitary (square) matrix \Blue{$\VU$} 
+  MatrixXd V = svd.matrixV(); // get unitary (square) matrix \Blue{$\VV$} 
   VectorXd sv = svd.singularValues(); // get singular values as vector
-  S = MatrixXd::Zero(A.rows(), A.cols());
-  const unsigned n = sv.size(); // no. of singular values
-  S.block(0,0,n,n) = sv.asDiagonal(); // build matrix S
+  MatrixXd Sigma = MatrixXd::Zero(A.rows(), A.cols());
+  const unsigned p = sv.size(); // no. of singular values
+  Sigma.block(0,0,p,p) = sv.asDiagonal(); // set diagonal block of \Blue{$\Sigmabf$}
+  return std::tuple<MatrixXd,MatrixXd,MatrixXd>(U,Sigma,V);
 }
+
+// Computation of economical (thin) SVD \Blue{$\VA=\VU\Sigmabf\VV^\herm$}, see \eqref{eq:svdeco}
+// SVD factors are returned as dense matrices in natural order
+std::tuple<MatrixXd,MatrixXd,MatrixXd> svd_eco(const MatrixXd& A) {
+  Eigen::JacobiSVD<MatrixXd> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
+  MatrixXd U = svd.matrixU(); // get unitary (square) matrix \Blue{$\VU$} 
+  MatrixXd V = svd.matrixV(); // get unitary (square) matrix \Blue{$\VV$} 
+  VectorXd sv = svd.singularValues(); // get singular values as vector
+  MatrixXd Sigma = sv.asDiagonal(); // build diagonal matrix \Blue{$\Sigmabf$}
+  return std::tuple<MatrixXd,MatrixXd,MatrixXd>(U,Sigma,V);
+}
+/* SAM_LISTING_END_1 */
+
+// Important! If replacing 'ComputeFull' by 'ComputeThin', 
+// the methods matrixU() and matrixV() will return the 
+// matrices U and V but not extended to an orthonormal basis!
