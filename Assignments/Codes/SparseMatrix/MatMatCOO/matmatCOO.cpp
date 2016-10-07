14af8697cc23bd6b127a94c570e03a9fd2f2072d
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-10-07 14:49:50 +0200

    matmatCOO

--- Assignments/Codes/SparseMatrix/MatMatCOO/matmatCOO.cpp
+++ Assignments/Codes/SparseMatrix/MatMatCOO/matmatCOO.cpp
@@ -1,38 +1,118 @@
+#include <algorithm>
 #include <iostream>
-#include <iomanip>
-#include <random>
+#include <numeric>
+#include <vector>
 
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
 
 using namespace Eigen;
 
-/* @brief Build an $n \times n$ random matrix $A$ with density $d$ in COO format
- * \param[in] mt Random number generator
- * \param[in] n Size of square matrix $A$
- * \param[in] d Density of matrix $A$, i.e. $nnz(A)/n^2$
- * \param[out] A An $n \times n$ random matrix with density $d$ in COO format
+using trip = Triplet<double>;
+using trip_vec = std::vector<trip>;
+
+/* @brief Build matrix $A$ in COO format
+ * @param[in] A An $n \times n$ matrix
+ * @param[out] triplets The $nnz(A)$-dimensional vector of triplets forming $A$
  */
 /* SAM_LISTING_BEGIN_0 */
-MatrixXd buildA(std::mt19937 & mt, int n, double d);
+trip_vec COO(const MatrixXd &A)
 {
-	std::uniform_int_distribution<int> pos(0, n);
-	std::uniform_real_distribution<double> val(-10, +10);
-
-	int nnz = rand(d*n*n);
+    // Initialization
+    int n = A.rows();
+    trip_vec triplets;
 
 	// Number of nonzero entries
-	for(int i=0; i<nnz; ++i) {
-		pos(mt)
+	int nnz = A.nonzeros();
+	triplets.resize(nnz);
+
+#if SOLUTION
+	int k = 0;
+	for(int i=0; i<m; ++i) { // Row iterator
+		for(int j=0; j<n; ++j) { // Col iterator
+			if(A(i,j) != 0) {
+				trip triplet(i, j, A(i,j));
+              	triplets(k) = triplet;
+				++k;
+             }
+		}
 	}
+#else // TEMPLATE
+    // TODO: build matrix $A$ in COO format
+#endif // TEMPLATE
 
-
-int main() {
-
-	// Random number generator
-	std::random_device rd; // Seed
-    std::mt19937 mt(rd()); // Mersenne Twister 19937 generator
-
-
-
+	return triplets;
 }
+/* SAM_LISTING_END_0 */
+
+/* @brief Compute matrix product $AB$ in COO format:
+ * Naive implementation.
+ * @param[in] A_trips The $nnz(A)$-dimensional vector of triplets forming matrix $A$
+ * @param[in] B_trips The $nnz(B)$-dimensional vector of triplets forming matrix $B$
+ * @param[out] C_trips The $nnz(C)$-dimensional vector of triplets forming matrix $C = AB$
+ */
+/* SAM_LISTING_BEGIN_1 */
+trip_vec COO_prod(const trip_vec &A, const trip_vec &B)
+{
+    // Initialization
+    trip_vec C;
+
+	for(auto const& a: A) {
+		for(auto const& b: B) {
+			if(a.col() == b.row()) {
+				trip triplet(a.row(), b.col(), a.value*b.value());
+				C.push_back(triplet);
+			}
+		}
+     }
+
+	return C;
+}
+// Complexity: O(nnz(A)*nnz(B))
+/* SAM_LISTING_END_1 */
+
+/* @brief Compute matrix product $AB$ in COO format:
+ * Naive implementation.
+ * @param[in] A_trips The $nnz(A)$-dimensional vector of triplets forming matrix $A$
+ * @param[in] B_trips The $nnz(B)$-dimensional vector of triplets forming matrix $B$
+ * @param[out] C_trips The $nnz(C)$-dimensional vector of triplets forming matrix $C = AB$
+ */
+/* SAM_LISTING_BEGIN_2 */
+trip_vec COO_prod(const trip_vec &A, const trip_vec &B)
+{
+	A = std::sort(std::begin(A), std::end(A), [&](const auto& a1, const auto& a2) {return a1.col() > a2.col();});
+	B = std::sort(std::begin(B), std::end(B), [&](const auto& b1, const auto& b2) {return b1.row() > b2.row();});
+	// Complexity: O(n_A*log(n_A) + n_B*log(n_B))
+
+	std::vector<int> vec(A.size() + B.size());
+  	auto it = std::set_intersection(std::begin(A), std::end(A), std::begin(A), std::end(B), std::begin(vec),
+			      [&](const auto& a, const auto& b) {return a.col() > b.row();}));
+	vec.resize(it - vec.begin());
+
+	for(it=vec.begin(); it!=vec.end(); ++it) {
+		
+		auto A_it = std::find_if(std::begin(A), std::end(A),
+						[&](const auto& a){return a.col() == *it;});
+		auto B_it = std::find_if(std::begin(B), std::end(B),
+						[&](const auto& b){return b.row() == *it;});
+
+		for(auto& a_it: A_it) {
+			if(*a_it.col() != *it) {
+				break;
+			}
+			for(auto& b_it: B_it) {
+				if(*b_it.row() != *it) {
+					break;
+				} else {
+					trip triplet(*a_it.row(), *b_it.col(), (*a_it.value)*(*b_it.value()));
+				C.push_back(triplet);
+			}
+		}
+	}
+	// Complexity: O(nnz(A*B))
+
+	return C;
+}
+// Complexity: O(n*log(n) + O(nnz(A*B))
+/* SAM_LISTING_END_2 */
+
