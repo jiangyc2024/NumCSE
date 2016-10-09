0093766c9330c229d1226442ff9aee2fc225833f
Daniele Casati <mail@DanieleCasati.com>
2016-10-09 19:21:24 +0200

    matmat

--- Assignments/Codes/SparseMatrix/MatMatCOO/matmatCOO.cpp
+++ Assignments/Codes/SparseMatrix/MatMatCOO/matmatCOO.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <numeric>
 #include <set>
@@ -85,7 +86,7 @@ trip_vec COOprod_naive(const trip_vec &A, const trip_vec &B)
 /* SAM_LISTING_END_1 */
 
 /* @brief Compute matrix product $AB$ in COO format:
- * Fast implementation.
+ * Efficient implementation.
  * @param[in] A_trips The $nnz(A)$-dimensional vector of triplets forming matrix $A$
  * @param[in] B_trips The $nnz(B)$-dimensional vector of triplets forming matrix $B$
  * @param[out] C_trips The $nnz(C)$-dimensional vector of triplets forming matrix $C = AB$
@@ -129,17 +130,18 @@ trip_vec COOprod_effic(trip_vec &A, trip_vec &B)
 		while(A_it != A.end()) {
 			if(A_it->col() != i) {
 				break;
-			}
-			while(B_it != B.end()) {
-				if(B_it->row() != i) {
-					break;
-				} else {
-					trip triplet(A_it->row(), B_it->col(), (A_it->value())*(B_it->value()));
-					C.push_back(triplet);
+			} else {
+				while(B_it != B.end()) {
+					if(B_it->row() != i) {
+						break;
+					} else {
+						trip triplet(A_it->row(), B_it->col(), (A_it->value())*(B_it->value()));
+						C.push_back(triplet);
+						++B_it;
+					}
 				}
+				++A_it;
 			}
-			++A_it;
-			++B_it;
 		}
 	}
 	// Complexity: O(nnz(A*B))
@@ -170,31 +172,31 @@ int main() {
 	trip_vec A_COO = COO(A);
 	trip_vec B_COO = COO(B);
 
-    // Compute with both solvers
-    std::cout << "--> Check that the solvers are correct" << std::endl;
+    // Compute with standard matrix multiplication and both new multipliers
+    std::cout << "--> Check that the multipliers are correct" << std::endl;
     trip_vec C_eigen, C_naive, C_effic;
 
 	C_eigen = COO(A * B);
     C_naive = COOprod_naive(A_COO, B_COO);
     C_effic = COOprod_effic(A_COO, B_COO);
 
-    std::cout << "Error = " << (x1 - x2).norm() << std::endl;
+  //std::cout << "Error = " << (x1 - x2).norm() << std::endl;
 
-    // Compute runtimes of different solvers
-    std::cout << "--> Runtime comparison of naive vs fast solver" << std::endl;
+    // Compute runtimes of different multipliers
+    std::cout << "--> Runtime comparison of naive vs efficient multiplier" << std::endl;
 	// Number of repetitions
     unsigned int repeats = 3;
 
     // Header
     std::cout << std::setw(20) << "n"
               << std::setw(20) << "time naive [s]"
-              << std::setw(20) << "time fast [s]"
+              << std::setw(20) << "time effic [s]"
               << std::endl;
 
     // Loop over matrix size
-    for(unsigned int k = 4; k <= 12; ++k) {
+/*    for(unsigned int k = 4; k <= 12; ++k) {
         // Timers
-        Timer tm_naive, tm_fast;
+        Timer tm_naive, tm_effic;
         unsigned int n = pow(2,k);
 
         // Repeat test many times
@@ -207,16 +209,16 @@ int main() {
             solveA(a,b,x);
             tm_naive.stop();
             // Compute runtime with efficient solver
-            tm_fast.start();
-            solveA_fast(a,b,x);
-            tm_fast.stop();
+            tm_effic.start();
+            solveA_effic(a,b,x);
+            tm_effic.stop();
         }
 
         // Print runtimes
         std::cout << std::setw(20) << n
                   << std::scientific << std::setprecision(3)
                   << std::setw(20) << tm_naive.min()
-                  << std::setw(20) << tm_fast.min()
+                  << std::setw(20) << tm_effic.min()
                   << std::endl;
-    }
+    }*/
 }
