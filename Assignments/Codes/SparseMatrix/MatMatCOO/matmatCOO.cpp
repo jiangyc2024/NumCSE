48ce6e9d8c100959bcc8ab8aa0bb6946c6018254
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-10-10 11:06:37 +0200

    matmatcoo

--- Assignments/Codes/SparseMatrix/MatMatCOO/matmatCOO.cpp
+++ Assignments/Codes/SparseMatrix/MatMatCOO/matmatCOO.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <numeric>
@@ -9,10 +10,10 @@
 #include <Eigen/Sparse>
 
 #include "timer.h"
-//#if INTERNAL
-//#include <chrono>
-//#include <figure/figure.hpp>
-//#endif // INTERNAL
+// #if INTERNAL
+#include <chrono>
+#include <figure/figure.hpp>
+// #endif // INTERNAL
 
 using namespace Eigen;
 
@@ -20,7 +21,7 @@ using Trip = Triplet<double>;
 using TripVec = std::vector<Trip>;
 
 /* @brief Build matrix $A$ in COO format
- * @param[in] A An $n \times n$ matrix
+ * @param[in] A An $m \times n$ matrix
  * @param[out] triplets The $nnz(A)$-dimensional vector of triplets forming $A$
  */
 /* SAM_LISTING_BEGIN_0 */
@@ -35,7 +36,7 @@ TripVec Mat2COO(const MatrixXd &A)
   //int nnz = A.nonzeros();
   //triplets.resize(nnz);
 
-//#if SOLUTION
+#if SOLUTION
   //int k = 0;
 	for(int i=0; i<m; ++i) { // Row iterator
 		for(int j=0; j<n; ++j) { // Col iterator
@@ -47,9 +48,9 @@ TripVec Mat2COO(const MatrixXd &A)
              }
 		}
 	}
-//#else // TEMPLATE
+#else // TEMPLATE
     // TODO: build matrix $A$ in COO format
-//#endif // TEMPLATE
+#endif // TEMPLATE
 
 	return triplets;
 }
@@ -57,7 +58,7 @@ TripVec Mat2COO(const MatrixXd &A)
 
 /* @brief Build matrix $A$ as Eigen::MatrixXd from COO format
  * @param[in] A An $nnz(A)$-dimensional vector of triplets forming matrix $A$
- * @param[out] A_mat An $n \times n$ matrix
+ * @param[out] A_mat The $m \times n$ matrix from vector of triplets $A$
  */
 MatrixXd COO2Mat(const TripVec &A)
 {
@@ -77,7 +78,29 @@ MatrixXd COO2Mat(const TripVec &A)
 
 	return A_mat;
 }
-/* SAM_LISTING_END_0 */
+
+/* @brief Build random binary matrix $A$ as Eigen::MatrixXd
+ * @param[in] m Number of desired rows for $A$
+ * @param[in] n Number of desired cols for $A$
+ * @param[in] d Desired density ($nnz/(m*n)$) for $A$
+ * @param[out] A An $m \times n$ random binary matrix
+ */
+MatrixXd randMat(int m, int n, double d)
+{
+    // Initialization
+	MatrixXd A = MatrixXd::Zero(m, n);
+
+	int nnz = round(m * n * d);
+
+	for(int k=0; k<nnz; ++k) {
+		int i = ceil(std::rand()/RAND_MAX*m);
+		int j = ceil(std::rand()/RAND_MAX*n);
+
+		A(i, j) = 1;
+	}
+
+	return A;
+}
 
 /* @brief Compute matrix product $AB$ in COO format:
  * Naive implementation.
@@ -90,7 +113,7 @@ TripVec COOprod_naive(const TripVec &A, const TripVec &B)
     // Initialization
     TripVec C;
 
-//#if SOLUTION
+#if SOLUTION
 	for(auto const& a: A) {
 		for(auto const& b: B) {
 			if(a.col() == b.row()) {
@@ -99,9 +122,9 @@ TripVec COOprod_naive(const TripVec &A, const TripVec &B)
 			}
 		}
 	}
-//#else // TEMPLATE
+#else // TEMPLATE
     // TODO: compute matrix product $AB$ in COO format (naive multiplier)
-//#endif // TEMPLATE
+#endif // TEMPLATE
 
 	return C;
 }
@@ -120,7 +143,7 @@ TripVec COOprod_effic(TripVec &A, TripVec &B)
 	// Initialization
     TripVec C;
 	
-//#if SOLUTION
+#if SOLUTION
 	std::sort(A.begin(), A.end(), [&](const Trip& a1, const Trip& a2) {return a1.col() < a2.col();});
 	std::sort(B.begin(), B.end(), [&](const Trip& b1, const Trip& b2) {return b1.row() < b2.row();});
 	// Complexity: O(n_A*log(n_A) + n_B*log(n_B))
@@ -129,7 +152,7 @@ TripVec COOprod_effic(TripVec &A, TripVec &B)
   //auto it = std::set_intersection(A.begin(), A.end(), B.begin(), B.end(), vec.begin(),
   //		      [&](const Trip& a, const Trip& b) {return a.col() < b.row();});
   //vec.resize(it - vec.begin());
-	int i_A = 0, i_B = 0;
+	size_t i_A = 0, i_B = 0;
 	std::set<int> intersect;
 	while(i_A != A.size() && i_B != B.size())
 	{
@@ -170,9 +193,9 @@ TripVec COOprod_effic(TripVec &A, TripVec &B)
 		A_idx = A_it;
 		B_idx = B_it;
 	}
-//#else // TEMPLATE
+#else // TEMPLATE
     // TODO: compute matrix product $AB$ in COO format (efficient multiplier)
-//#endif // TEMPLATE
+#endif // TEMPLATE
 	// Complexity: O(nnz(A*B))
 
 	return C;
@@ -184,18 +207,18 @@ int main() {
     // Initialization
     unsigned int n = 6;
     MatrixXd A(n,n), B(n,n);
-    A << 1, 0, 0, 0, 0, 1,
-         1, 0, 0, 0, 0, 1,
-         1, 0, 0, 0, 0, 1,
-         1, 0, 0, 0, 0, 1,
-         1, 0, 0, 0, 0, 1,
-		 1, 0, 0, 0, 0, 1;
+    A << 1, 0, 0, 0, 0, 0,
+         1, 0, 0, 0, 0, 0,
+         1, 0, 0, 0, 0, 0,
+         1, 0, 0, 0, 0, 0,
+         1, 0, 0, 0, 0, 0,
+		 1, 0, 0, 0, 0, 0;
     B << 1, 1, 1, 1, 1, 1,
          0, 0, 0, 0, 0, 0,
          0, 0, 0, 0, 0, 0,
          0, 0, 0, 0, 0, 0,
          0, 0, 0, 0, 0, 0,
-		 1, 1, 1, 1, 1, 1;
+		 0, 0, 0, 0, 0, 0;
 		  
 	// COO format
 	TripVec A_COO = Mat2COO(A);
@@ -224,12 +247,22 @@ int main() {
 	// Number of repetitions
     unsigned int repeats = 3;
 
+// #if INTERNAL
+    // sizes   will contain the size of the matrix
+    // timings will contain the runtimes in seconds
+    std::vector<double> sizes, timings_eigen, timings_naive, timings_effic;
+// #endif
+    
     // Header
     std::cout << std::setw(20) << "n"
 		      << std::setw(20) << "time eigen [s]"
               << std::setw(20) << "time naive [s]"
               << std::setw(20) << "time effic [s]"
               << std::endl;
+
+    // Seed
+    int seed = static_cast<int> (std::chrono::system_clock::now().time_since_epoch().count());
+    std::srand(seed);
 
     // Loop over matrix size
     for(unsigned int k = 4; k <= 12; ++k) {
@@ -240,8 +273,10 @@ int main() {
         // Repeat test many times
         for(unsigned int r = 0; r < repeats; ++r) {
             // Initialization
-			A = MatrixXd::Random(n,n);
-            B = MatrixXd::Random(n,n);
+//		    A = MatrixXd::Random(n,n);
+//          B = MatrixXd::Random(n,n);
+        	A = randMat(n, n, 0.1);
+        	B = randMat(n, n, 0.1);
 			
 			// COO format
 			TripVec A_COO = Mat2COO(A);
@@ -260,6 +295,14 @@ int main() {
             C_effic = COOprod_effic(A_COO, B_COO);
             tm_effic.stop();
         }
+        
+// #if INTERNAL
+        // Save results in a vector
+        sizes.push_back(n); // Save vector sizes
+        timings_eigen.push_back(tm_eigen.min());
+        timings_naive.push_back(tm_naive.min());
+        timings_effic.push_back(tm_effic.min());
+// #endif
 
         // Print runtimes
         std::cout << std::setw(20) << n
@@ -269,4 +312,58 @@ int main() {
                   << std::setw(20) << tm_effic.min()
                   << std::endl;
     }
+    
+// #if INTERNAL
+    mgl::Figure fig1;
+    fig1.title("Timings of Eigen solver");
+    fig1.ranges(2, 9000, 1e-8, 1e3);
+    fig1.setlog(true, true); // set loglog scale
+    fig1.plot(sizes, timings_eigen, " r+").label("Eigen");
+    fig1.fplot("1e-9*x^3", "k|").label("O(n^3)");
+    fig1.xlabel("Matrix size (n)");
+    fig1.ylabel("Time [s]");
+    fig1.legend(0, 1);
+    fig1.save("structuredLSE_timing_eigen.eps");
+    fig1.save("structuredLSE_timing_eigen.png");
+
+    mgl::Figure fig2;
+    fig2.title("Timings of naive solver");
+    fig2.ranges(2, 9000, 1e-8, 1e3);
+    fig2.setlog(true, true); // set loglog scale
+    fig2.plot(sizes, timings_naive, " r+").label("naive");
+    fig2.fplot("1e-9*x^3", "k|").label("O(n^3)");
+    fig2.xlabel("Matrix size (n)");
+    fig2.ylabel("Time [s]");
+    fig2.legend(0, 1);
+    fig2.save("structuredLSE_timing_naive.eps");
+    fig2.save("structuredLSE_timing_naive.png");
+    
+    mgl::Figure fig3;
+    fig3.title("Timings of efficient solver");
+    fig3.ranges(2, 9000, 1e-8, 1e3);
+    fig3.setlog(true, true); // set loglog scale
+    fig3.plot(sizes, timings_effic, " r+").label("efficient");
+    fig3.fplot("1e-9*x^3", "k|").label("O(n^3)");
+    fig3.xlabel("Matrix size (n)");
+    fig3.ylabel("Time [s]");
+    fig3.legend(0, 1);
+    fig3.save("structuredLSE_timing_effic.eps");
+    fig3.save("structuredLSE_timing_effic.png");
+
+    mgl::Figure fig4;
+    fig4.title("Comparison of timings");
+    fig4.ranges(2, 9000, 1e-8, 1e3);
+    fig4.setlog(true, true); // set loglog scale
+    fig4.plot(sizes, timings_eigen, " g+").label("Eigen");
+    fig4.plot(sizes, timings_naive, " r+").label("naive");
+    fig4.plot(sizes, timings_effic, " b+").label("efficient");
+    fig4.fplot("1e-9*x^3", "k|").label("O(n^3)");
+    fig4.fplot("1e-8*x^2", "k-").label("O(n^2)");
+    fig4.fplot("1e-7*x", "k:").label("O(n)");
+    fig4.xlabel("Matrix size (n)");
+    fig4.ylabel("Time [s]");
+    fig4.legend(0, 1);
+    fig4.save("structuredLSE_comparison.eps");
+    fig4.save("structuredLSE_comparison.png");
+// #endif
 }
