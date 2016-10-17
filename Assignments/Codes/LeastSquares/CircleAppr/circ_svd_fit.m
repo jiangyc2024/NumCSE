ad2814aa3e1a7dec9210e8d29d4e7e30cf3edadb
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-10-17 11:05:44 +0200

    update circleappr

--- Assignments/Codes/LeastSquares/CircleAppr/circ_svd_fit.m
+++
@@ -1,6 +0,0 @@
-function [m,r] = circ_svd_fit(x,y)
-A = [x(:).^2+y(:).^2, x(:), y(:), ones(size(x(:)))];
-[U,S,V] = svd(A);
-v = V(:,end);
-m = -v(2:3)/(2*v(1));
-r = sqrt((v(2)^2 + v(3)^2)/(4*v(1)^2) - v(4)/v(1));
