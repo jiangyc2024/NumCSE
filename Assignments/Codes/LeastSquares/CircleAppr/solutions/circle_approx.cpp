6d47b1cf2fa2c1202e0bbaa826d20326c99addd6
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-10-17 17:51:57 +0200

    spaitest

--- Assignments/Codes/LeastSquares/CircleAppr/solutions/circle_approx.cpp
+++ Assignments/Codes/LeastSquares/CircleAppr/solutions/circle_approx.cpp
@@ -11,6 +11,14 @@ Vector3d circl_alg_fit(const VectorXd &x, const VectorXd & y) {
 //    A.transpose() * A).ldlt().solve(A.transpose() * b) << endl;
 }
 
+
+
+Vector3d circl_geo_fit(const VectorXd &x, const VectorXd & y) {
+//    A.jacobiSvd(ComputeThinU | ComputeThinV).solve(b)
+//            A.colPivHouseholderQr().solve(b) << endl;
+//    A.transpose() * A).ldlt().solve(A.transpose() * b) << endl;
+}
+
 int main(int argc, char **argv) {
 
 }
