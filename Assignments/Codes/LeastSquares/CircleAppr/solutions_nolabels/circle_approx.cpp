a0da727f75d68ff9df417511a35d698a2b7d44b3
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-10-17 11:40:27 +0200

    MLB

--- Assignments/Codes/LeastSquares/CircleAppr/solutions_nolabels/circle_approx.cpp
+++ Assignments/Codes/LeastSquares/CircleAppr/solutions_nolabels/circle_approx.cpp
@@ -10,6 +10,13 @@
 
 using namespace Eigen;
 
+
+Vector3d circl_alg_fit(const VectorXd &x, const VectorXd & y) {
+//    A.jacobiSvd(ComputeThinU | ComputeThinV).solve(b)
+//            A.colPivHouseholderQr().solve(b) << endl;
+//    A.transpose() * A).ldlt().solve(A.transpose() * b) << endl;
+}
+
 int main(int argc, char **argv) {
 
 }
