3309d7d2bf5f5aebd151d067472a542c0f50d36b
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-10-20 16:00:48 +0200

    fixes

--- Assignments/Codes/SparseMatrix/GridFunction/grid_function.cpp
+++ Assignments/Codes/SparseMatrix/GridFunction/grid_function.cpp
@@ -47,7 +47,7 @@ inline index_t to_vector_index(index_t i,
 SparseMatrix<double> build_matrix(const Matrix3d & S,
                                  const shape_t & size) {
     // Will be used to store triplets
-    std::vector<Triplet<double>> triplets;
+    std::vector<Triplet<double> > triplets;
 
     // The matrix $\mathbf{A}$ will have size $N \times N$.
     // N = n*m
@@ -192,7 +192,7 @@ void solve(const SparseMatrix<double> & A,
     X.resizeLike(Y);
 
     // Sparse LU decomposition object
-    SparseLU<SparseMatrix<double>> solver;
+    SparseLU<SparseMatrix<double> > solver;
 
 #if SOLUTION
     // Prepare LU factorization
