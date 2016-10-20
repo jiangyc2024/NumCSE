b686456e4a7d798640869e7e3cb5395c0caa759d
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-10-20 09:45:00 +0200

    Sparsify SPAI

--- Assignments/Codes/LeastSquares/SPAI/spai.cpp
+++ Assignments/Codes/LeastSquares/SPAI/spai.cpp
@@ -20,54 +20,82 @@ using index_t = int;
  */
 /* SAM_LISTING_BEGIN_1 */
 SparseMatrix<double> spai(SparseMatrix<double> & A) {
+    // Size check
     assert(A.rows() == A.cols() &&
     "Matrix must be square!");
+    unsigned int N = A.rows();
 
-    unsigned int n = A.rows();
-
+    // Needed to make sure ___Ptr functions return
+    // arrays specified in CRS format
     A.makeCompressed();
 
+    // Obtain pointers to data of A
     double* valPtr = A.valuePtr();
     index_t* innPtr = A.innerIndexPtr();
     index_t* outPtr = A.outerIndexPtr();
 
+    // Create vector for triplets of B and reserve enough space
     std::vector<Triplet<double>> triplets;
     triplets.reserve(A.nonZeros());
 
 #if SOLUTION
-    // Without this, the code is MUCH slower w.r.t. MATLAB
-    #pragma omp parallel for
-    for(unsigned int i = 0; i < n; ++i) {
-
+    // Loop over each column of $A$
+    for(unsigned int i = 0; i < N; ++i) {
+        // Number of non zeros in current column of $A$
         index_t nnz_i = outPtr[i+1] - outPtr[i];
-
+        // If column is empty: skip column (matrix is not invertible)
         if(nnz_i == 0) continue;
 
-        MatrixXd C = MatrixXd::Zero(n, nnz_i);
+        // Temporarily create a (small, dense) matrix on which normal formula
+        // will be applied. We project the space $\mathcal{P}(A)$
+        // onto $\mathcal{P}(a_i)$
+         MatrixXd C = MatrixXd::Zero(N, nnz_i);
+//        SparseMatrix<double> C(N, nnz_i);
+//        std::vector<Triplet<double>> C_triplets;
+//        C_triplets.reserve(nnz_i*nnz_i);
+
+        // Need to build matrix $C$. To this end we remove all columns
+        // from $A$ for which $a_i = 0$.
+        // To this end: loop over all non-zero entries of the $i$-th column
+        // This loop has length $n$
         for(unsigned int k = outPtr[i]; k < outPtr[i+1]; ++k) {
+            // Row of this non-zero entry
             index_t row_k = innPtr[k];
+            // Number of non-zero entries for $row_k$-th column
             index_t nnz_k = outPtr[row_k+1] - outPtr[row_k];
+            // Loop over all non-zeros of $row_k$th-column
+            // This loop has length complexity $n$
             for(unsigned int l = 0; l < nnz_k; ++l) {
                 unsigned int innIdx = outPtr[row_k] + l;
-                C(innPtr[innIdx], k - outPtr[i]) = valPtr[innIdx];
+                 C(innPtr[innIdx], k - outPtr[i]) = valPtr[innIdx];
+//                C_triplets.emplace_back(Triplet<double>(innPtr[innIdx], k - outPtr[i], valPtr[innIdx]));
             }
         }
+//        C.setFromTriplets(C_triplets.begin(), C_triplets.end());
+//        C.makeCompressed();
+//        SparseMatrix<double> S = C.transpose() * C;
+//        MatrixXd M = MatrixXd(S);
+//        VectorXd xt = C.row(i).transpose();
+
+        // Compute $C^\top C$ and solve normal equation.
+        // Complexity of product:
+        // Complexity of solve:
+        // Size of $b$ is at most $n$.
+//        VectorXd b = M.partialPivLu().solve(xt);
         VectorXd b = (C.transpose() * C).partialPivLu().solve(C.row(i).transpose());
+        // Loop as length $n$.
         for(unsigned int k = 0; k < b.size(); ++k) {
-            #pragma omp critical(triplets)
-            {
-				triplets.push_back(Triplet<double>(innPtr[outPtr[i] + k], i, b(k)));
-            }
+            triplets.emplace_back(Triplet<double>(innPtr[outPtr[i] + k], i, b(k)));
         }
     }
 #else // TEMPLATE
     // TODO: build $B$ in triplet format, exploiting the sparse format of $A$
 #endif // TEMPLATE
 
-    SparseMatrix<double> B = SparseMatrix<double>(n,n);
+    // Build and return SPAI preconditioner
+    SparseMatrix<double> B = SparseMatrix<double>(N,N);
     B.setFromTriplets(triplets.begin(), triplets.end());
     B.makeCompressed();
-
     return B;
 }
 /* SAM_LISTING_END_1 */
