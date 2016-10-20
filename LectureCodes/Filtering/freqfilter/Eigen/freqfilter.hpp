cf7dc63f48d16eda0e01f8a6236847819651442b
Prof. Dr. Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2016-10-20 15:22:08 +0200

    Demo code for use of DFT in Eigen

--- LectureCodes/Filtering/freqfilter/Eigen/freqfilter.hpp
+++ LectureCodes/Filtering/freqfilter/Eigen/freqfilter.hpp
@@ -5,19 +5,20 @@ using Eigen::VectorXd;
 using Eigen::VectorXcd;
 
 /* SAM_LISTING_BEGIN_0 */
-void freqfilter(const VectorXd& y, int k, VectorXd& low, VectorXd& high) {
-  const unsigned m = y.size()/2;
+void freqfilter(const VectorXd& y, int k,
+		VectorXd& low, VectorXd& high) {
+  const unsigned m = y.size()/2; 
   
-  Eigen::FFT<double> fft;
+  Eigen::FFT<double> fft; // DFT helper object
   VectorXcd c = fft.fwd(y);
   
   VectorXcd clow = c;
-  for (int j = -k; j <= +k; ++j) {
-	  clow(m+j) = 0;
-  }
-  
+  // Set high frequency coefficients to zero
+  for (int j = -k; j <= +k; ++j) clow(m+j) = 0; 
+  // Vector of high frequency coefficients
   VectorXcd chigh = c - clow;
-  
+
+  // Recover filtered time-domain signals
   low = fft.inv(clow).real();
   high = fft.inv(chigh).real();
 }
