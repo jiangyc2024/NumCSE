70fe9dbe1329cc295b7a8b88661f3647ba959f97
Julien Gacon <jgacon@student.ethz.ch>
2016-10-22 10:48:10 +0200

    removed a line for better layout in the script

--- LectureCodes/Filtering/naivedft/Eigen/naivedft.hpp
+++ LectureCodes/Filtering/naivedft/Eigen/naivedft.hpp
@@ -4,12 +4,12 @@ using Eigen::VectorXcd;
 
 void naivedft(const VectorXcd& y, VectorXcd& c) {
   const long n = y.size();
-  const std::complex<double> i(0,1); // imaginary unit
+  const std::complex<double> i(0,1); 
   c.resize(n);
   std::complex<double> w = std::exp(-2*M_PI/n*i),
                        s = w;
-  c(0) = y.sum();
 
+  c(0) = y.sum();
   for (long j = 1; j < n; ++j) {
     c(j) = y(n-1);
     for (long k = n-2; k >= 0; --k) {
