12132192787d28362e8e55e687907b5c7362894d
Prof. Dr. Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2016-10-26 15:35:23 +0200

    Added codes for norm constrained least squares problems Revised codes connected with Givens rotations

--- LectureCodes/LeastSquares/givensrot/Eigen/qrgivens.hpp
+++ LectureCodes/LeastSquares/givensrot/Eigen/qrgivens.hpp
@@ -17,20 +17,20 @@ using namespace std;
 using namespace Eigen;
 
 /* SAM_LISTING_BEGIN_0 */
-//! QR decomposition of \emph{square} matrix \texttt{A}
+//! QR decomposition of \emph{square} matrix \texttt{A} by successive Givens transformations
 void qrgivens(const MatrixXd& A, MatrixXd& Q, MatrixXd& R){
-	unsigned int n = A.rows();
-	Q.setIdentity(); // Assemble rotations in matrix, alternative see Rem.~\ref{rem:orthstore}
-	Matrix2d G;
-	Vector2d tmp, xDummy;
-	R = A;
-	for(int i = 0; i < n-1; ++i){
-		for(int j = n-1; j > i; --j){
-			tmp(0) = R(j-1, i); tmp(1) = R(j,i);
-			planerot(tmp, G, xDummy); // see Code~\ref{cpp:planerot}
-			R.block(j-1,0,2,n) = G * R.block(j-1,0,2,n);
-			Q.block(0, j-1, n, 2) = Q.block(0,j-1,n,2) * G.transpose();
-		}
-	}
+  unsigned int n = A.rows();
+  // Assemble rotations in a dense matrix.
+  // For (more efficient) alternatives see Rem.~\cref{rem:storeQ}
+  Q.setIdentity(); 
+  Matrix2d G; Vector2d tmp, xDummy;
+  R = A; // In situ transformation
+  for(int i = 0; i < n-1; ++i) {
+    for(int j = n-1; j > i; --j) {
+      tmp(0) = R(j-1, i); tmp(1) = R(j,i);
+      planerot(tmp, G, xDummy); // see Code~\ref{cpp:planerot}
+      R.block(j-1,0,2,n) = G * R.block(j-1,0,2,n);
+      Q.block(0, j-1, n, 2) = Q.block(0,j-1,n,2) * G.transpose();
+    }}
 }
 /* SAM_LISTING_END_0 */
