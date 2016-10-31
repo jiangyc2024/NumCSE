6c1549c03f272d89fba63f6eb57c2750f6ec661a
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-10-31 14:53:32 +0100

    Autofocus extra

---
+++ Assignments/Codes/Filtering/Autofocus/test_pgm.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+
+#include <Eigen/Dense>
+
+#include "pgm.hpp"
+
+using namespace Eigen;
+
+int main() {
+
+    std::ifstream file("image.pgm");
+    std::ofstream file_out("image_edited.pgm");
+
+    PGMObject p;
+    file >> p;
+
+    MatrixXd M;
+    p.get_data(M);
+
+    p.set_data(M.transpose() / 2);
+
+    file_out << p;
+}
---
+++ Utils/FFT/deblur.hpp
@@ -0,0 +1,1 @@
+../../LectureCodes/Filtering/deblur/Eigen/deblur.hpp
