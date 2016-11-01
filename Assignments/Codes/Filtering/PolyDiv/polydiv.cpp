13a7f965d0ee0950841b8b95069c7d63beaaff6a
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-11-01 20:22:52 +0100

    polydiv

--- Assignments/Codes/Filtering/PolyDiv/polydiv.cpp
+++ Assignments/Codes/Filtering/PolyDiv/polydiv.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <vector>
+#include <iomanip>
 
 #include <Eigen/Dense>
 #include <unsupported/Eigen/FFT>
+
+#include "timer.h"
 
 using namespace Eigen;
 
@@ -15,8 +17,8 @@ using namespace Eigen;
 VectorXd polyMult_naive(const VectorXd & u, const VectorXd & v)
 {
     // Initialization
-    unsigned m = u.size() - 1; // Degree of polynomial
-    unsigned n = v.size() - 1; // Degree of polynomial
+    unsigned m = u.size();
+    unsigned n = v.size();
     unsigned dim = max(m, n);
     
     VectorXd u_tmp = u;
@@ -24,12 +26,12 @@ VectorXd polyMult_naive(const VectorXd & u, const VectorXd & v)
     VectorXd v_tmp = v;
     v_tmp.conservativeResize(dim);
     
-    VectorXd uv(m+n+1);
+    VectorXd uv(m+n-1); // Degree is (m-1) + (n-1)
 
 #if SOLUTION
     for(unsigned i=0; i<uv.size(); ++i) {
-		unsigned fst = max(0, i - n);
-		unsigned lst = min(i, n);
+		unsigned fst = max(0, i - n - 1);
+		unsigned lst = min(i, n - 1);
 		for(unsigned j=fst; j<=lst; ++j) {
 			uv(i) += u_tmp(j) * v_tmp(i-j);
 		}
@@ -50,14 +52,14 @@ VectorXd polyMult_naive(const VectorXd & u, const VectorXd & v)
 VectorXd polyMult_fast(const VectorXd & u, const VectorXd & v)
 {
     // Initialization
-    unsigned m = u.size() - 1; // Degree of polynomial
-    unsigned n = v.size() - 1; // Degree of polynomial
+    unsigned m = u.size();
+    unsigned n = v.size();
     unsigned dim = max(m, n);
     
     VectorXd u_tmp = u;
-    u_tmp.conservativeResize(u.size() + n);
+    u_tmp.conservativeResize(u.size() + n - 1);
     VectorXd v_tmp = v;
-    v_tmp.conservativeResize(v.size() + m);
+    v_tmp.conservativeResize(v.size() + m - 1);
 
 #if SOLUTION
 	Eigen::FFT<double> fft;
@@ -71,30 +73,62 @@ VectorXd polyMult_fast(const VectorXd & u, const VectorXd & v)
 }
 /* SAM_LISTING_END_1 */
 
-/* SAM_LISTING_BEGIN_2 */
 int main() {
-    // Initialization
-    unsigned int n = 5;
-    MatrixXd A(n,n);
-    A << 10, 2, 3, 4, 5,
-         6, 20, 8, 9, 1,
-         1, 2, 30, 4, 5,
-         6, 7, 8, 20, 0,
-         1, 2, 3, 4, 10;
+	unsigned m = 10;
+    unsigned n = 8;
+    // Compute with both functions
+    std::cout << "Check that both polynomial multiplicators are correct"
+			  << std::endl;
+    VectorXd u = VectorXd::Random(m);
+    VectorXd v = VectorXd::Random(n);
 
-    // Test 'buildC'
-    SparseMatrix <double> C = buildC(A);
-    std::cout << "C = " << C << std::endl;
+    VectorXd uv1 = polyMult_naive(u, v);
+    std::cout << "Naive multiplicator: "
+		      << std::endl << uv1 << std::endl;
 
-    // Test 'solveLyapunov'
-    MatrixXd X(n,n);
-    solveLyapunov(A,X);
-    std::cout << "X = " << X << std::endl;
+    VectorXd uv2 = polyMult_fast(u, v);
+    std::cout << "Efficient multiplicator: "
+		      << std::endl << uv2 << std::endl;
 
-    // Verify the solution if you obtain zero
-    MatrixXd I = MatrixXd::Identity(n,n);
-    std::cout << "Correct if close to 0: "
-              << (A*X + X*A.transpose() - I).norm()
+    std::cout << "Error = " << (uv1 - uv2).norm() << std::endl;
+
+	unsigned repeats = 3;
+    // Compute runtimes of different multiplicators
+    std::cout << "Runtime comparison of naive v efficient multiplicator"
+			  << std::endl;
+	VectorXd uv;
+
+    // Header
+    std::cout << std::setw(20) << "n"
+              << std::setw(20) << "time naive [s]"
+              << std::setw(20) << "time fast [s]"
               << std::endl;
+
+    // Loop over matrix size
+    for(unsigned p = 2; p <= 9; ++p) {
+        // Timers
+        Timer tm_naive, tm_fast;
+        unsigned n = pow(2,p);
+
+        // Repeat test many times
+        for(unsigned r = 0; r < repeats; ++r) {
+            u = VectorXd::Random(n);
+            v = VectorXd::Random(n);
+
+            // Compute runtime of naive multiplicator
+            tm_naive.start();
+            uv = polyMult_naive(u, v);
+            tm_naive.stop();
+            // Compute runtime of efficient multiplicator
+            tm_fast.start();
+            uv = polyMult_fast(u, v);
+            tm_fast.stop();
+        }
+        // Print runtimes
+        std::cout << std::setw(20) << n
+                  << std::scientific << std::setprecision(3)
+                  << std::setw(20) << tm_naive.min()
+                  << std::setw(20) << tm_fast.min()
+                  << std::endl;
+    }
 }
-/* SAM_LISTING_END_2 */
