8deeb06351cd58527e1fd49eb5dd21e4cf323c05
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-11-03 00:36:04 +0100

    toeplitz

--- Assignments/Codes/Filtering/Toeplitz/toepmatmult.m
+++
@@ -1,4 +0,0 @@
-function y = toepmatmult(c,r,x)
-    if (size(c) ~= size(r)), error('c,r length mismatch'); end
-    T = toeplitz(c,r);
-    y = T*x;
--- Assignments/Codes/Filtering/Toeplitz/ttmatsolve.m
+++
@@ -1,4 +0,0 @@
-function x = ttmatsolve(h,y)
-    n = size(h,1);
-    T = toeplitz(h,[h(1); zeros(n-1,1)]);
-    x = T\y;
