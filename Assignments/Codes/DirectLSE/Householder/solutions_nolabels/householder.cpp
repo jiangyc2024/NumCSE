294e4fc004879a1e124b7535c9caf2f5ad088b05
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-11-04 12:26:18 +0100

    Householder

--- Assignments/Codes/DirectLSE/Householder/solutions_nolabels/householder.cpp
+++ Assignments/Codes/DirectLSE/Householder/solutions_nolabels/householder.cpp
@@ -1,37 +1,31 @@
+//// 
+//// Copyright (C) 2016 SAM (D-MATH) @ ETH Zurich
+//// Author(s): lfilippo <filippo.leonardi@sam.math.ethz.ch> 
+//// Contributors: tille, jgacon, dcasati
+//// This file is part of the NumCSE repository.
+////
 #include <iostream>
 #include <Eigen/Dense>
 
 using namespace Eigen;
 
-/* SAM_LISTING_BEGIN_0 */
 void applyHouseholder(VectorXd &x, const VectorXd &v) {
-#if SOLUTION
     double d = v.transpose()*x;
     x -= 2*v*d;
-#else // TEMPLATE
-    // TODO
-#endif // TEMPLATE
 }
-/* SAM_LISTING_END_0 */
 
-/* SAM_LISTING_BEGIN_1 */
 template <typename Scalar>
 void applyHouseholder(VectorXd &x,
                       const MatrixBase<Scalar> &V) {
     int n = V.rows();
 
-#if SOLUTION
     for(unsigned int i = 0; i < n; ++i) {
         VectorXd v = V.col(i);
         v(i) = std::sqrt(1. - V.col(i).normSquared());
 
         x += 2.*v.dot(x) / (1. + 2.*v.dot(v)) * v;
     }
-#else // TEMPLATE
-    // TODO
-#endif // TEMPLATE
 }
-/* SAM_LISTING_END_1 */
 
 int main() {
 
--- Assignments/Codes/DirectLSE/Householder/templates/householder.cpp
+++ Assignments/Codes/DirectLSE/Householder/templates/householder.cpp
@@ -5,12 +5,7 @@ using namespace Eigen;
 
 /* SAM_LISTING_BEGIN_0 */
 void applyHouseholder(VectorXd &x, const VectorXd &v) {
-#if SOLUTION
-    double d = v.transpose()*x;
-    x -= 2*v*d;
-#else // TEMPLATE
     // TODO
-#endif // TEMPLATE
 }
 /* SAM_LISTING_END_0 */
 
@@ -20,16 +15,7 @@ void applyHouseholder(VectorXd &x,
                       const MatrixBase<Scalar> &V) {
     int n = V.rows();
 
-#if SOLUTION
-    for(unsigned int i = 0; i < n; ++i) {
-        VectorXd v = V.col(i);
-        v(i) = std::sqrt(1. - V.col(i).normSquared());
-
-        x += 2.*v.dot(x) / (1. + 2.*v.dot(v)) * v;
-    }
-#else // TEMPLATE
     // TODO
-#endif // TEMPLATE
 }
 /* SAM_LISTING_END_1 */
 
