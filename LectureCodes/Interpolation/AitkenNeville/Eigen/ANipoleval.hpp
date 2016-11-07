0d6ec1d9a02308a9e24e37d3356c1b215edcac50
Prof. Dr. Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2016-11-07 10:26:39 +0100

    Enhanced comments for functions dealing with polynomial interpolation

--- LectureCodes/Interpolation/AitkenNeville/Eigen/ANipoleval.hpp
+++ LectureCodes/Interpolation/AitkenNeville/Eigen/ANipoleval.hpp
@@ -5,17 +5,19 @@
 /// Repository: https://gitlab.math.ethz.ch/NumCSE/NumCSE/
 /// Do not remove this header.
 //////////////////////////////////////////////////////////////////////////
+
 # include <Eigen/Dense>
 
 using Eigen::VectorXd;
 /* SAM_LISTING_BEGIN_0 */
 // Aitken-Neville algorithm for evaluation of interpolating polynomial
-// IN:  t, y: interpolation data points
+// IN:  t, y: (vectors of) interpolation data points 
 //      x: (single) evaluation point
 // OUT: value of interpolant in x
 double ANipoleval(const VectorXd& t, VectorXd y, const double x) {
   for (int i = 0; i < y.size(); ++i) {
     for (int k = i - 1; k >= 0; --k) {
+      // Recursion \eqref{eq:ipolrec}
       y(k) = y(k + 1) + (y(k + 1) - y(k))*(x - t(i))/(t(i) - t(k));
     }
   }
--- LectureCodes/Interpolation/DividedDifferencesEval/Eigen/evaldivdiff.hpp
+++ LectureCodes/Interpolation/DividedDifferencesEval/Eigen/evaldivdiff.hpp
@@ -1,15 +1,16 @@
 # include "divdiff.hpp"
 
 using Eigen::VectorXd;
-/* Evaluation of polynomial in Newton basis (divided differences)
- * IN:  t = nodes (mutually different)
- *      y = values in t
- *      x = evaluation points (as Eigen::Vector)
- * OUT: p = values in x                                           */
+/* SAM_LISTING_BEGIN_0 */
+// Evaluation of polynomial in Newton basis (divided differences)
+// IN:  t = nodes (mutually different)
+//      y = values in t
+//      x = evaluation points (as Eigen::Vector)
+// OUT: p = values in x                                           */
 void evaldivdiff(const VectorXd& t, const VectorXd& y, const VectorXd& x, VectorXd& p) {
   const unsigned n = y.size() - 1;
 
-  // get coefficients of polynomial
+  // get Newton coefficients of polynomial (non in-situ implementation!)
   VectorXd coeffs; divdiff(t, y, coeffs);
 
   // evaluate
