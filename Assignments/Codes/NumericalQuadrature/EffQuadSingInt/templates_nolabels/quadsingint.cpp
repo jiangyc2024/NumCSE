fdfc27000322f056316cc832fa3d807d981c5830
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-11-22 03:11:29 +0100

    Deploy and clean

--- Assignments/Codes/NumericalQuadrature/EffQuadSingInt/templates_nolabels/quadsingint.cpp
+++ Assignments/Codes/NumericalQuadrature/EffQuadSingInt/templates_nolabels/quadsingint.cpp
@@ -4,73 +4,53 @@
 //// Contributors: tille, jgacon, dcasati
 //// This file is part of the NumCSE repository.
 ////
-// Include function "gauleg" for the computation of weights/nodes of Gauss quadrature
-#include "gauleg.hpp"
 #include <iomanip>
 #include <iostream>
 #include <cmath>
 
+// Include function "gauleg" for the computation of weights/nodes of Gauss quadrature
+#include "gauleg.hpp"
+
 // Choose between substitution 1. s = sqrt(1 \pm t), 2. t = cos(s)
-#define METHOD 2
+#define METHOD 1
+// Set to true to enable test  with monomials
+#define MONOMIAL_TEST   false
 
 //! @brief Compute integral $\int_{-1}^1 \sqrt(1-t^2) f(t) dt$ using tranfromation
 //! @param[in] n number of Gauss nodes (evaluate f at 2*n points)
 //! @return value of integral
 template <class Function>
-double quadsingint(Function&& f, const unsigned n) {
-  double I = 0.;
-#if METHOD == 1 // s = sqrt(1 \pm t)
-  QuadRule Q = gauleg(n);
-  
-  for(unsigned i = 0; i < n; ++i) {
-    double x = (Q.nodes(i) + 1.) / 2.;
-    double w = Q.weights(i) * x * x * std::sqrt(2. - x * x);
-    I += w * (f(x*x - 1) + f(-x*x + 1));
-  }
-  return I;
-#elif METHOD == 2 // t = cos(s)
-  QuadRule Q = gauleg(2*n);
-    
-  for(unsigned i = 0; i < 2*n; ++i) {
-    double x = sin(Q.nodes(i) * M_PI_2);
-    double w = Q.weights(i) * cos(Q.nodes(i) * M_PI_2) * cos(Q.nodes(i) * M_PI_2);
-    I += w * f(x) * M_PI_2;
-  }
-  return I;
-#endif
+double quadsingint(const Function& f, const unsigned n) {
+    double I = 0.;
+    // TODO: implement exponentially convergent quadrature
+    return I;
 }
 
-int main() {
+int main(int argc, char ** argv) {
     
-  auto f = [] (double t) { return 1. / (2. + std::exp(3*t) ); };
-    
-  // Max num of Gauss points to use (in each direction)
-  const unsigned max_N = 25;
-  // "Exact" integral
-  const double I_ex = 0.483296828976607;
-   
-    // IF you want to test monomials as f, use this data (uncomment this and comment f above:
-//   std::vector<double> ex = { M_PI_2, 0., M_PI_2 / 4, 0, M_PI_2 / 8, 0, M_PI_2 * 5 / 64};
-//   assert(argc > 1);
-//   int n = std::atoi(argv[1]);
-//   std::cout << "Monomial of degree:" << n << std::endl;
-//   auto f = [&n] (double t) { return std::pow(t, n); };
-//   I_ex = ex[n];
-    
-  // Observed: exponential convergence (as expected)
-  std::cout << "N" << std::setw(15) << "I_approx" << std::setw(15) << "error" << std::setw(15) << "q est." << std::endl;
-  double errold, errnew;
-  for(unsigned int N = 1; N < max_N; ++N) {
-    double I_approx = quadsingint(f, N);
-      
-    errold = errnew;
-    errnew = std::abs(I_ex - I_approx);
-    std::cout << std::setw(3) << N << std::setw(15) << I_approx << std::setw(15) << errnew;
-      
-    if( N > 1 ) {
-      std::cout  << std::setw(15) << errnew / errold;
-    }
-    std::cout << std::endl;
-  }
-  return 0;
+    // Max num of Gauss points to use (in each direction)
+    const unsigned max_N = 25;
+    // "Exact" integral
+    double I_ex = 0.483296828976607;
+
+    // If you want to test monomials as f, use this data and set MONOMIAL_TEST to true
+#if MONOMIAL_TEST
+    std::vector<double> ex = { M_PI_2, 0., M_PI_2 / 4, 0, M_PI_2 / 8, 0, M_PI_2 * 5 / 64};
+    assert(argc > 1);
+    int n = std::atoi(argv[1]);
+    std::cout << "TEST: Monomial of degree:" << n << std::endl;
+    auto f = [&n] (double t) { return std::pow(t, n); };
+    I_ex = ex[n];
+#else // END MONOMIAL_TEST
+    // Test function
+    auto f = [] (double t) { return 1. / (2. + std::exp(3*t) ); };
+#endif
+
+    std::cout << std::setw(3) << "N"
+              << std::setw(15) << "I_approx"
+              << std::setw(15) << "error"
+              << std::endl;
+    double errold, errnew;
+    // TODO: test convergence
+    return 0;
 }
