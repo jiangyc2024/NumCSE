a0b27f263aee43e7810bf68594510358818b186d
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-11-22 04:34:31 +0100

    Add GW

--- Assignments/Codes/NumericalQuadrature/ImproperIntegrals/quadinf.cpp
+++ Assignments/Codes/NumericalQuadrature/ImproperIntegrals/quadinf.cpp
@@ -1,41 +1,16 @@
 #include <iomanip>
 #include <iostream>
 #include <cmath>
+
 #include <Eigen/Sparse>
 #include <Eigen/Eigenvalues>
+
+#include "golubwelsh.hpp"
 
 #define PI  M_PI
 #define PI_HALF  M_PI_2
 
 using vector = Eigen::VectorXd;
-
-//! @brief Golub-Welsh implementation 5.3.35
-//! @param[in] n number of Gauss nodes
-//! @param[out] w weights
-//! @param[out] x nodes for interval $[-1,1]$
-void golubwelsh(const int n, vector& w, vector& x) {
-  w.resize(n);
-  x.resize(n);
-  if(n == 0) {
-    x(0) = 0;
-    w(0) = 2;
-  } else {
-    vector b(n-1);
-    Eigen::MatrixXd J = Eigen::MatrixXd::Zero(n,n);
-
-    for(int i = 1; i < n; ++i) {
-      double d = (i) / sqrt(4. * i * i - 1.);
-      J(i,i-1) = d;
-      J(i-1,i) = d;
-    }
-
-     Eigen::EigenSolver<Eigen::MatrixXd> eig(J);
-
-     x = eig.eigenvalues().real();
-     vector tmp = eig.eigenvectors().real().topRows<1>();
-     w = 2 * tmp.cwiseProduct(tmp);
-  }
-}
 
 //! @brief Compute $\int_a^b f(x) dx \approx \sum w_i f(x_i)$ (with scaling of $w$ and $x$)
 //! @tparam Function template type for function handle f (e.g. lambda func.)
