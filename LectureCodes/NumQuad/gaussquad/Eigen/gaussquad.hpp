5d6d69a61ece67658dabe010053591d83dce914a
Filippo Leonardi <filippo.leonardi@sam.math.ethz.ch>
2016-11-22 02:12:04 +0100

    Fix gaussquad missing

--- LectureCodes/NumQuad/gaussquad/Eigen/gaussquad.hpp
+++ LectureCodes/NumQuad/gaussquad/Eigen/gaussquad.hpp
@@ -1,22 +1,27 @@
+#pragma once
+
 # include <Eigen/Dense>
 # include <Eigen/Eigenvalues>
 
 struct QuadRule {
-  Eigen::VectorXd nodes, weights;
+    Eigen::VectorXd nodes, weights;
 };
 
-void gaussquad (const unsigned n, QuadRule& qr) {
-  Eigen::MatrixXd M = Eigen::MatrixXd::Zero(n, n);
-  for (unsigned i = 1; i < n; ++i){
-    const double b = i/std::sqrt(4.*i*i - 1.);
-    M(i, i - 1) = b;
-    // line 15 is optional as the EV-Solver only references 
-    // the lower triangular part of M
-    // M(i - 1, i) = b; 
-  }
-  // using EigenSolver for symmetric matrices, exploiting the structure
-  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(M);
+QuadRule gaussquad_(const unsigned n) {
+    QuadRule qr;
+    Eigen::MatrixXd M = Eigen::MatrixXd::Zero(n, n);
+    for (unsigned i = 1; i < n; ++i){
+        const double b = i/std::sqrt(4.*i*i - 1.);
+        M(i, i - 1) = b;
+        // line 15 is optional as the EV-Solver only references
+        // the lower triangular part of M
+        // M(i - 1, i) = b;
+    }
+    // using EigenSolver for symmetric matrices, exploiting the structure
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> eig(M);
 
-  qr.nodes = eig.eigenvalues();
-  qr.weights = 2*eig.eigenvectors().topRows<1>().array().pow(2);
+    qr.nodes = eig.eigenvalues();
+    qr.weights = 2*eig.eigenvectors().topRows<1>().array().pow(2);
+
+    return qr;
 }
--- LectureCodes/NumQuad/gaussquad/Eigen/main.cpp
+++ LectureCodes/NumQuad/gaussquad/Eigen/main.cpp
@@ -4,7 +4,7 @@
 int main() {
   QuadRule qr;
   for (unsigned n = 2; n <= 8; n *= 2) {
-    gaussquad(n, qr);
+    qr = gaussquad_(n);
     std::cout << "n = " << n << " -----------------------------------------------\n"
               << "x = " << qr.nodes.transpose() << "\n"
               << "w = " << qr.weights.transpose() << "\n";
