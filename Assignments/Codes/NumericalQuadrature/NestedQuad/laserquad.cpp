27cefcb8399484fba90a42573d8dae0babd2d36e
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-11-24 21:14:32 +0100

    fixed compilation of assignment codes using Utils/gaussquad.hpp

--- Assignments/Codes/NumericalQuadrature/NestedQuad/laserquad.cpp
+++ Assignments/Codes/NumericalQuadrature/NestedQuad/laserquad.cpp
@@ -41,7 +41,8 @@ double evalgaussquad(const double a, const double b,
 template <class Function>
 double gaussquadtriangle(const Function& f, const unsigned N) {
     // Get nodes/weights for integral over dx and dy
-    QuadRule Q = gaussquad(N);   
+    QuadRule Q;
+    gaussquad(N, Q);
 
 #if SOLUTION
     // We define an auxiliary function of x defined
