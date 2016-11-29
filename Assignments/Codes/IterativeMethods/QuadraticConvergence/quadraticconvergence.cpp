17113ea2b704fe339a799086947b8130c546203c
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-11-29 17:59:40 +0100

    fixes

--- Assignments/Codes/IterativeMethods/QuadraticConvergence/quadraticconvergence.cpp
+++ Assignments/Codes/IterativeMethods/QuadraticConvergence/quadraticconvergence.cpp
@@ -11,10 +11,9 @@ using namespace Eigen;
  * @param[out]
  */
 /* SAM_LISTING_BEGIN_0 */
-template <class function>
-VectorXd steffensen(function&& f, double x0) {
+double newton_arctan(double x0) {
 
-    VectorXd x(1); x(0) = x0;
+    double x0_ = x0;
 
 #if SOLUTION
     double upd = 1;
@@ -22,52 +21,35 @@ VectorXd steffensen(function&& f, double x0) {
 
     while(std::abs(upd) > eps) {
 
-        fx = f(x.tail(1)); // Only 2 evaluations of $f$ at each step
-        if(fx != 0) {
-            upd = fx*fx / (f(x.tail(1)+fx)-fx);
-            x_new = x.tail(1) - upd;
-            x.conservativeResize(x.size()+1);
-            x.tail(1) = x_new;
-        } else {
-            upd = 0;
-        }
+        double x1 = x0_ - (2*x0_-(1+x0_*x0_)*std::atan(x0_)) / (1-2*x0_*std::atan(x0_));
+//        double x1 = (-x0_+(1-x0_*x0_)*std::atan(x0_)) / (1-2*x0_*std::atan(x0_));
+
+        upd = (x1 - x0_) / x1;
+        x0_ =  x1;
+        std::cout << "x0 = " << x1 << ", accuracy = " << upd << std::endl;
     }
 #else // TEMPLATE
 // TODO:
 #endif // TEMPLATE
 
-    return x;
+    return x0_;
 }
 /* SAM_LISTING_END_0 */
 
 int main() {
 	// Initialization
-    using Vector = Eigen::VectorXd;
-    // Different definitions of $f$ with the same zero:
-    auto f = [] (double x) { return x*std::exp(x)-1; };
-//    auto f = [] (double x) { return std::exp(x)-1/x; };
-//    auto f = [] (double x) { return x-std::exp(-x); };
-    double x0 = 1;
-    double x_star = 0.567143290409784; // From Matlab: x_star = fzero(f,x0);
+    double x0  = 2; // Initial guess
 
-    // Steffensen's method
-    VectorXd x = steffensen(f, x0);
+    // Netwon's method
+    double x0_ = newton_arctan(x0);
 
-    // Compute errors
-    double residual = f(x);
-    double err = std::abs(x-x_star);
-    double log_err = std::log(err);
-    VectorXd ratios(log_err.size()-2);
-    for(unsigned i=0; i<ratios.size(); ++i) {
-        ratios(i) = (log_err(i+2) - log_err(i+1)) /
-                    (log_err(i+1) - log_err(i));
-    }
-
-    // Print output
-    std::cout << "x" << "\t" << "err" << "\t" << "residuals" <<" \t" << "ratios" << std::endl;
-    std::cout << x(0) << "\t" << err(0) << "\t" << residuals(0) << "\t" << ratios(0) << std::endl;
-    std::cout << x(1) << "\t" << err(1) << "\t" << residuals(1) << "\t" << ratios(1) << std::endl;
-    for(unsigned i=0; i<ratios.size(); ++i) {
-        std::cout << x(i+2) << "\t" << err(i+2) << "\t" << residuals(i+2) << "\t" << ratios(i) << std::endl;
-    }
+//    figure;
+//    x1 = x0-atan(x0)*(1+x0^2);   x2 = x1-atan(x1)*(1+x1^2);
+//    X=[-2:0.01:2];
+//    plot(X, atan(X),'k',...
+//        X,  2*(X)-(1+(X).^2).*atan((X)),'r--',...
+//        [x0, x1, x1, x2, x2], [atan(x0), 0, atan(x1), 0, atan(x2)],...
+//        [x0,x1],[0,0],'ro',[-2,2], [0,0],'k','linewidth',2);
+//    legend('arctan', 'g', 'Newton critical iteration');axis equal;
+//    print -depsc2 'ex_NewtonArctan.eps'
 }
