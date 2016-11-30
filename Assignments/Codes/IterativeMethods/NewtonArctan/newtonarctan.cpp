7173dd52edd2591b63c2174b03a260ecfd77a8fa
Daniele Casati <daniele.casati@sam.math.ethz.ch>
2016-11-30 14:07:45 +0100

    newton_arctan

--- Assignments/Codes/IterativeMethods/NewtonArctan/newtonarctan.cpp
+++ Assignments/Codes/IterativeMethods/NewtonArctan/newtonarctan.cpp
@@ -4,6 +4,10 @@
 
 #include <Eigen/Dense>
 
+#if INTERNAL
+#include <figure.hpp>
+#endif // INTERNAL
+
 using namespace Eigen;
 
 /* @brief
@@ -11,9 +15,9 @@ using namespace Eigen;
  * @param[out]
  */
 /* SAM_LISTING_BEGIN_0 */
-double newton_arctan(double x0) {
+double newton_arctan(double x0_) {
 
-    double x0_ = x0;
+    double x0 = x0_;
 
 #if SOLUTION
     double upd = 1;
@@ -21,35 +25,53 @@ double newton_arctan(double x0) {
 
     while(std::abs(upd) > eps) {
 
-        double x1 = x0_ - (2*x0_-(1+x0_*x0_)*std::atan(x0_)) / (1-2*x0_*std::atan(x0_));
-//        double x1 = (-x0_+(1-x0_*x0_)*std::atan(x0_)) / (1-2*x0_*std::atan(x0_));
+        double x1 = x0 - (2*x0-(1+x0*x0)*std::atan(x0)) / (1-2*x0*std::atan(x0));
+//        double x1 = (-x0+(1-x0*x0)*std::atan(x0)) / (1-2*x0*std::atan(x0));
 
-        upd = (x1 - x0_) / x1;
-        x0_ =  x1;
+        upd = (x1 - x0) / x1;
+        x0 =  x1;
         std::cout << "x0 = " << x1 << ", accuracy = " << upd << std::endl;
     }
 #else // TEMPLATE
 // TODO:
 #endif // TEMPLATE
 
-    return x0_;
+    return x0;
 }
 /* SAM_LISTING_END_0 */
 
 int main() {
 	// Initialization
-    double x0  = 2; // Initial guess
+    double x0_ = 2; // Initial guess
 
     // Netwon's method
-    double x0_ = newton_arctan(x0);
+    double x0 = newton_arctan(x0_);
+    double x1 = x0 - std::atan(x0)*(1+x0*x0);
+    double x2 = x1 - std::atan(x1)*(1+x1*x1);
 
-//    figure;
-//    x1 = x0-atan(x0)*(1+x0^2);   x2 = x1-atan(x1)*(1+x1^2);
-//    X=[-2:0.01:2];
-//    plot(X, atan(X),'k',...
-//        X,  2*(X)-(1+(X).^2).*atan((X)),'r--',...
-//        [x0, x1, x1, x2, x2], [atan(x0), 0, atan(x1), 0, atan(x2)],...
-//        [x0,x1],[0,0],'ro',[-2,2], [0,0],'k','linewidth',2);
-//    legend('arctan', 'g', 'Newton critical iteration');axis equal;
-//    print -depsc2 'ex_NewtonArctan.eps'
+#if INTERNAL
+    // Figure
+    VectorXd X = VectorXd::LinSpaced(401,-2,+2);
+    VectorXd atan(X.size());
+    for(unsigned i=0; i<X.size(); ++i) {
+        atan(i) = std::atan(X(i));
+    }
+    VectorXd g = X.array().square();
+    g = 2*X - (VectorXd::Ones(X.size()) + g).cwiseProduct(atan);
+    VectorXd X_1(5); X_1 << x0, x1, x1, x2, x2;
+    VectorXd Y_1(5); Y_1 << std::atan(x0), 0, std::atan(x1), 0, std::atan(x2);
+    VectorXd X_2(2); X_2 << x0, x1;
+    VectorXd Y_2(2); Y_2 << 0, 0;
+
+    mgl::Figure fig;
+    fig.title("Newton critical iteration");
+    fig.ranges(-2, +2, -1.2, +1.2);
+    fig.plot(X, atan, "k-").label("arctan");
+    fig.plot(X, g, "r=").label("g");
+    fig.plot(X_1, Y_1, "b-").label("Newton critical iteration");
+    fig.plot(X_2, Y_2, "o ");
+    fig.fplot("0", "k-");
+    fig.legend(0, 1);
+    fig.save("NewtonArctan");
+#endif // INTERNAL
 }
