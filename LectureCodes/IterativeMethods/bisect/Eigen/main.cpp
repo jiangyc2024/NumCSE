1648c6095dd82b1e2539e907f3ccd887f6104c4b
Till Ehrengruber <till@ehrengruber.ch>
2017-03-23 22:21:33 +0100

    Code cleanup

--- LectureCodes/IterativeMethods/bisect/Eigen/main.cpp
+++ LectureCodes/IterativeMethods/bisect/Eigen/main.cpp
@@ -4,6 +4,6 @@
 
 int main()
 {
-	auto F = [](const double x){ return sin(x)*cos(x); };
-	std::cout << "sin(x)*cos(x) has a zero at " << bisect(F, 1, 2, 1e-6) << std::endl;
+	auto F = [] (const double x) { return sin(x)*cos(x); };
+	std::cout << "sin(x)*cos(x) has a zero at " << bisect(F, 1., 2., 1e-6) << std::endl;
 }
