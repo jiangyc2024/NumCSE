f946ef1e06c637353c7e2e15a0c56fc04c06ecb8
Cryoris <jules.gacon@googlemail.com>
2019-09-21 18:41:49 +0200

    change mmstruc/Eigen/main.cpp to use matplotlibcpp instead of Figure

--- LectureCodes/MatVec/Dense/mmstruc/Eigen/main_Figure.cpp
+++ LectureCodes/MatVec/Dense/mmstruc/Eigen/main_Figure.cpp
@@ -1,25 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////
 /// Demonstration code for lecture "Numerical Methods for CSE" @ ETH Zurich
 /// (C) 2016 SAM, D-MATH
-/// Author(s): Thomas Etterlin <thomaset@student.ethz.ch>,
-///            Julien Gacon <jgacon@ethz.ch>
+/// Author(s): Thomas Etterlin <thomaset@student.ethz.ch>
 /// Repository: https://gitlab.math.ethz.ch/NumCSE/NumCSE/
 /// Do not remove this header.
 //////////////////////////////////////////////////////////////////////////
 /* SAM_LISTING_BEGIN_0 */
-#include <string>
 #include <Eigen/Dense>
-#include "matplotlibcpp.h"
 
-namespace plt = matplotlibcpp;
+#include <figure/figure.hpp>
+
 using namespace Eigen;
-
-void spy(const Eigen::MatrixXd& M, const std::string& fname) {
-  plt::figure();
-  plt::spy(M, {{"marker", "o"}, {"markersize", "2"}, {"color", "b"}});
-  plt::title("nnz = " + std::to_string(M.nonZeros()));
-  plt::savefig(fname);
-}
 
 int main () {
 	int n = 100;
@@ -29,10 +20,11 @@ int main () {
 	A.row(n-1) = RowVectorXd::LinSpaced(n,1,n);
 	B = A.colwise().reverse();
 	MatrixXd C = A*A, D = A*B;
-  spy(A, "Aspy_cpp.eps");
-  spy(B, "Bspy_cpp.eps");
-  spy(C, "Cspy_cpp.eps");
-  spy(D, "Dspy_cpp.eps");
+	mgl::Figure fig1, fig2, fig3, fig4;
+	fig1.spy(A);	fig1.save("Aspy_cpp");
+	fig2.spy(B);	fig2.save("Bspy_cpp");
+	fig3.spy(C);	fig3.save("Cspy_cpp");
+	fig4.spy(D);	fig4.save("Dspy_cpp");
 	return 0;
 }
 /* SAM_LISTING_END_0 */
