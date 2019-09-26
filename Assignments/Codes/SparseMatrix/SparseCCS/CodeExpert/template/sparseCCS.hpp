943227c1fbc6d8fe5faccc38fcb429e01535cfc9
Tandri Gauksson <tandri.gauksson@sam.math.ethz.ch>
2019-09-26 18:39:39 +0200

    SparseCCS CodeExpert templates ready

--- Assignments/Codes/SparseMatrix/SparseCCS/CodeExpert/template/sparseCCS.hpp
+++ Assignments/Codes/SparseMatrix/SparseCCS/CodeExpert/template/sparseCCS.hpp
@@ -32,38 +32,7 @@ void CCS(const MatrixXd & A, VectorXd & val, VectorXd & row_ind, VectorXd & col_
   // Do not use Eigen methods for Sparse matrices.
   
   //START
-  // Number of nonzero entries
-  int nnz = 0;
-  for(int i=0; i<m; ++i) { // Row iterator
-    for(int j=0; j<n; ++j) { // Col iterator
-      if(A(i,j) != 0) {
-        ++nnz;
-      }
-    }
-  }
-
-  // Initialization
-  val.resize(nnz);
-  row_ind.resize(nnz);
-  col_ptr.resize(n);
-
-  // Store $A$ in CCS format
-  int index = 0;
-  for(int j=0; j<n; ++j) { // Col iterator
-
-    // Update 'col\_ptr'
-    col_ptr(j) = index;
-
-    for(int i=0; i<m; ++i) { // Row iterator
-      if(A(i,j) != 0) {
-        // Record the value to 'val'
-        val(index) = A(i,j);
-        // Record the row index to 'row\_ind'
-        row_ind(index) = i;
-        ++index;
-      }
-    }
-  }
+  
   // END
 }
 /* SAM_LISTING_END_0 */
