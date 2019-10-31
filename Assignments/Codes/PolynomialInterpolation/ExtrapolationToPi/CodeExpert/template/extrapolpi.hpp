ee08554df4876ffb31024f01cdd487362430e8c7
Tandri Gauksson <tandri.gauksson@sam.math.ethz.ch>
2019-10-31 14:25:15 +0100

    ExtrapolationToPi: templates ready

--- Assignments/Codes/PolynomialInterpolation/ExtrapolationToPi/CodeExpert/template/extrapolpi.hpp
+++ Assignments/Codes/PolynomialInterpolation/ExtrapolationToPi/CodeExpert/template/extrapolpi.hpp
@@ -17,20 +17,6 @@ double extrapolate_to_pi(unsigned int k) {
   // when calculating $s_j$ for $j=2,...,k$.
   // START
   
-  // Data for interpolation
-  ArrayXd J = ArrayXd::LinSpaced(k-1,2,k);
-  ArrayXd T = 1.0/J; // $t_j = 1/j$
-  ArrayXd S = J*sin(M_PI*T);
-  
-  // Aitken-Neville scheme for evaluating the interpolating
-  // polynomial of $(t_j,s_j)$ at x = 0.
-  for (int l = 0; l < S.size(); l++) {
-    for (int m = l-1; m >= 0; m--) {
-      S(m) = S(m+1) + (S(m+1) - S(m))*(0.0 - T(l))/(T(l) - T(m));
-    }
-  }
-  // Now S(0) has contributions from all data points.
-  pi = S(0);  
   // END
   return pi;
 }
@@ -47,22 +33,7 @@ void plotExtrapolationError(void) {
   // matplotlib functions such as figure(), plot(), xlabel(), title(),...
   // You can use main.cpp of the LinearDataFit problem as a reference.
   // START
-  int kmax = 10;
-  VectorXd K = VectorXd::LinSpaced(kmax-1,2,kmax);
-  VectorXd Err(K.size());
-  std::cout << std::setw(5) << " k" << std::setw(15) << "approx. pi" << std::setw(15) << "error" << std::endl;
-  for(int l=0; l < K.size(); l++) {
-    double pi = extrapolate_to_pi( K(l) );
-    Err(l) = std::abs(pi - M_PI);
-    std::cout << std::setw(5) << K(l) << std::setw(15) << pi << std::setw(15) << Err(l) << std::endl;
-  }
-  // Number of data points is k-1, so polynomial degree is k-2.
-  VectorXd Deg = K - 2*VectorXd::Ones(kmax-1);
-  plt::semilogy(Deg,Err);
-  plt::title("Approximation of pi");
-  plt::xlabel("Polynomial degree");
-  plt::ylabel("Error");
-  plt::grid();
+  
   // END
   plt::savefig("./cx_out/pi_error.png");
 }
