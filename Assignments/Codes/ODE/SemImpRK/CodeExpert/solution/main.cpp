83067d4d6af134eca47baed81b2d9c7c6eb5e506
Tandri Gauksson <tandri.gauksson@sam.math.ethz.ch>
2019-12-17 11:32:21 +0100

    SemImpRK: First CodeExpert version, templates not ready

---
+++ Assignments/Codes/ODE/SemImpRK/CodeExpert/solution/main.cpp
@@ -0,0 +1,20 @@
+
+#include "rosenbrock.hpp"
+
+int main() {
+  auto f =  [] (const Vector3d y) { 
+    Vector3d fy;
+    fy << y(0)*y(1), y(1)*y(2), y(2)-y(0);
+    return fy;
+  };
+  auto df = [] (const Vector3d y) {
+    Matrix3d J;
+    J << y(1),y(0),0,0,y(2),y(1),-1,0,1;
+    return J;
+  };
+  Eigen::Vector3d y0;
+  y0 << 1,2,3;
+  std::cout << "Test of solveRosenbrock():" << solveRosenbrock(f,df,y0,10,2.).at(10).transpose() << std::endl;
+  
+  std::cout << "Convergence rate: " << std::round(cvgRosenbrock()) << std::endl;
+}
