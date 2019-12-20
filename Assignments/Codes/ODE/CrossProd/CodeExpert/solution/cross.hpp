5dbc9b22dd5a56aea0669ef6b77ab1eecd3932d2
ralfh <ralfh@ethz.ch>
2019-12-20 08:01:26 +0100

    Reformatting and added LaTeX tags

--- Assignments/Codes/ODE/CrossProd/CodeExpert/solution/cross.hpp
+++ Assignments/Codes/ODE/CrossProd/CodeExpert/solution/cross.hpp
@@ -19,31 +19,26 @@ using namespace Eigen;
 //! \param[in] N number of steps to perform.
 /* SAM_LISTING_BEGIN_1 */
 template <class Function, class Jacobian>
-std::vector<VectorXd> solve_imp_mid(Function &&f,
-                                    Jacobian &&Jf,
-                                    double T,
-                                    const VectorXd &y0,
-                                    unsigned int N) {
+std::vector<VectorXd> solve_imp_mid(Function &&f, Jacobian &&Jf, double T,
+                                    const VectorXd &y0, unsigned int N) {
   std::vector<VectorXd> res(N + 1);
   // TO DO (13-1.e): Construct the implicit mid-point method with the class
-  // implicit_RKIntegrator and execute the .solve() method. 
+  // implicit_RKIntegrator and execute the .solve() method.
   // Return the vector containing all steps including initial and final value.
   // START
   // Initialize implicit RK with Butcher scheme
   unsigned int s = 1;
-  MatrixXd A(s,s);
+  // Initialize coefficients for the implicit midpoint method
+  MatrixXd A(s, s);
   VectorXd b(s);
-  A << 1./2.;
+  A << 1. / 2.;
   b << 1.;
-  implicit_RKIntegrator RK(A,b);
-  
+  implicit_RKIntegrator RK(A, b);
   res = RK.solve(f, Jf, T, y0, N);
   // END
   return res;
 }
 /* SAM_LISTING_END_1 */
-
-
 
 //! \tparam Function type for function implementing the rhs function.
 //! Must have VectorXd operator()(VectorXd x)
@@ -57,14 +52,11 @@ std::vector<VectorXd> solve_imp_mid(Function &&f,
 //! \param[in] N number of steps to perform.
 /* SAM_LISTING_BEGIN_2 */
 template <class Function, class Jacobian>
-std::vector<VectorXd> solve_lin_mid(Function &&f,
-                                    Jacobian &&Jf,
-                                    double T,
-                                    const VectorXd &y0,
-                                    unsigned int N)  {
+std::vector<VectorXd> solve_lin_mid(Function &&f, Jacobian &&Jf, double T,
+                                    const VectorXd &y0, unsigned int N) {
   std::vector<VectorXd> res(N + 1);
   // TO DO (13-1.g): Implement the linear implicit mid-point method for
-  // an autonomous ODE y' = f(y), y(0) = y0. Return the vector containing 
+  // an autonomous ODE y' = f(y), y(0) = y0. Return the vector containing
   // all steps including initial and final value.
   // START
   // Initial step size
@@ -79,23 +71,19 @@ std::vector<VectorXd> solve_lin_mid(Function &&f,
   // Pointers to swap previous value
   VectorXd *yold = &ytemp1;
   VectorXd *ynew = &ytemp2;
-  MatrixXd eye = MatrixXd::Identity(3,3);
+  MatrixXd eye = MatrixXd::Identity(3, 3);
 
   // Loop over all fixed steps
-  for(unsigned int k = 0; k < N; ++k) {
-      // Compute, save and swap next step
-      *ynew = *yold +
-              h*(eye - h/2. * Jf(*yold)).lu().solve(f(*yold));
-      res.push_back(*ynew);
-      std::swap(yold, ynew);
+  for (unsigned int k = 0; k < N; ++k) {
+    // Compute, save and swap next step
+    *ynew = *yold + h * (eye - h / 2. * Jf(*yold)).lu().solve(f(*yold));
+    res.push_back(*ynew);
+    std::swap(yold, ynew);
   }
   // END
   return res;
 }
 /* SAM_LISTING_END_2 */
-
-
-
 
 /* SAM_LISTING_BEGIN_3 */
 void tab_crossprod(void) {
@@ -103,7 +91,7 @@ void tab_crossprod(void) {
   // defined in solve_imp_mid. Tabulate the norms of the results at all steps.
   // START
   double T = 10.;
-  int N =128;
+  int N = 128;
   // set data
   double c = 1.;
   Vector3d y0;
@@ -111,51 +99,52 @@ void tab_crossprod(void) {
   Vector3d a;
   a << 1., 0., 0.;
   // define rhs
-  auto f = [&a, &c] (const Vector3d &y) -> Vector3d {
-      return a.cross(y) + c*y.cross(a.cross(y));
+  auto f = [&a, &c](const Vector3d &y) -> Vector3d {
+    return a.cross(y) + c * y.cross(a.cross(y));
   };
-  //define Jacobian of rhs
-  auto Jf = [&a, &c] (const Vector3d &y) {
+  // define Jacobian of rhs
+  auto Jf = [&a, &c](const Vector3d &y) {
     Matrix3d temp;
-    temp << -c*(a(1)*y(1) + a(2)*y(2)),
-            c*(2*a(0)*y(1) - a(1)*y(0)) - a(2),
-            a(1) + c*(2*a(0)*y(2) - a(2)*y(0)),
-            a(2) - c*(a(0)*y(1) - 2*a(1)*y(0)),
-            -c*(a(0)*y(0) + a(2)*y(2)),
-            c*(2*a(1)*y(2) - a(2)*y(1)) - a(0),
-            - a(1) - c*(a(0)*y(2) - 2*a(2)*y(0)),
-            a(0) - c*(a(1)*y(2) - 2*a(2)*y(1)),
-            -c*(a(0)*y(0) + a(1)*y(1));
+    temp << -c * (a(1) * y(1) + a(2) * y(2)),
+        c * (2 * a(0) * y(1) - a(1) * y(0)) - a(2),
+        a(1) + c * (2 * a(0) * y(2) - a(2) * y(0)),
+        a(2) - c * (a(0) * y(1) - 2 * a(1) * y(0)),
+        -c * (a(0) * y(0) + a(2) * y(2)),
+        c * (2 * a(1) * y(2) - a(2) * y(1)) - a(0),
+        -a(1) - c * (a(0) * y(2) - 2 * a(2) * y(0)),
+        a(0) - c * (a(1) * y(2) - 2 * a(2) * y(1)),
+        -c * (a(0) * y(0) + a(1) * y(1));
     return temp;
   };
- 
+
   std::vector<VectorXd> res_imp = solve_imp_mid(f, Jf, T, y0, N);
-  
+
   std::cout << "1. Implicit midpoint method" << std::endl;
-  std::cout << std::setw(10) << "t" 
-            << std::setw(15) << "norm(y(t))" << std::endl;
-  
-  for (int i=0; i<N+1; ++i) {
-    std::cout << std::setw(10) << T*i/N  
-              << std::setw(15) << res_imp[i].norm() << std::endl;
+  std::cout << std::setw(10) << "t" << std::setw(15) << "norm(y(t))"
+            << std::endl;
+
+  for (int i = 0; i < N + 1; ++i) {
+    std::cout << std::setw(10) << T * i / N << std::setw(15)
+              << res_imp[i].norm() << std::endl;
   }
   // END
-  
+/* SAM_LISTING_END_3 */
+
+/* SAM_LISTING_BEGIN_4 */
   // TO DO (13-1.g): solve the cross-product ODE with the implicit RK method
   // defined in solve_lin_mid. Tabulate the norms of the results at all steps.
   // START
   std::vector<VectorXd> res_lin = solve_lin_mid(f, Jf, T, y0, N);
-  
+
   std::cout << "\n2. Linear implicit midpoint method" << std::endl;
-  std::cout << std::setw(10) << "t" 
-            << std::setw(15) << "norm(y(t))" << std::endl;
-  for (int i=0; i<N+1; ++i) {
-    std::cout << std::setw(10) << T*i/N  
-              << std::setw(15) << res_lin[i].norm() << std::endl;
+  std::cout << std::setw(10) << "t" << std::setw(15) << "norm(y(t))"
+            << std::endl;
+  for (int i = 0; i < N + 1; ++i) {
+    std::cout << std::setw(10) << T * i / N << std::setw(15)
+              << res_lin[i].norm() << std::endl;
   }
   // END
+  /* SAM_LISTING_END_4 */
 }
-/* SAM_LISTING_END_3 */
-
 
 #endif
