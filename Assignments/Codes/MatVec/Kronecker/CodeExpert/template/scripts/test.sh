6e48431c20382d21bc56b28d6bb5f9ca57f28818
Maximilian Herde <herdem@student.ethz.ch>
2020-04-09 17:58:55 +0200

    changed test.sh to run doctest

--- Assignments/Codes/MatVec/Kronecker/CodeExpert/template/scripts/test.sh
+++ Assignments/Codes/MatVec/Kronecker/CodeExpert/template/scripts/test.sh
@@ -4,4 +4,22 @@ export PYTHONIOENCODING="UTF-8"
 
 python3 copy_and_tweak.py
 
+echo "Compiling ..."
 
+mkdir -p bin
+
+find . -iname 'tests.cpp' | sort | xargs \
+  g++ -fdiagnostics-color=always -std=c++11 \
+  -I/usr/local/include/python3.7m \
+  -I/usr/local/lib/python3.7/site-packages/numpy/core/include \
+  -I/usr/include/eigen3/ \
+  -lpython3.7m \
+  -lpthread -lutil -ldl \
+  -Xlinker -export-dynamic \
+  -o bin/tests.out
+  
+echo "Compilation successful"
+
+echo "Running tests ..."
+
+bin/tests.out
