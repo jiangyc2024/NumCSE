ec7a6bc196e67d7f7a79c89183b78632fd073471
Maximilian Herde <herdem@student.ethz.ch>
2020-04-26 00:25:03 +0200

    resolved problem of multiple headers

--- Assignments/Codes/MatVec/Kronecker/CodeExpert/template/copy_and_tweak.py
+++ Assignments/Codes/MatVec/Kronecker/CodeExpert/template/copy_and_tweak.py
@@ -1,11 +1,9 @@
-import glob
 import re
 import sys
 
 #
 # CURRENT ISSUES:
 #   - The cmake version can only be called from the folder's local cmake file and its build folder.
-#   - Can only handle one file that is written by the student.
 #   - probably more issues
 #
 
@@ -22,26 +20,38 @@ def parseWriteChange(student_sol, copy, tests):
 		if parse:
 			function_signatures.append(parse)
 	
-	for line in student_sol:
-		write = True
-		for el in function_signatures:
-			parse = re.match("(\s*)" + el.group(1) + el.group(2) + el.group(3) + "(\s*\(.*)", line)
-			if parse:
-				copy.write(parse.group(1) + el.group(1) + el.group(2) + prefix + el.group(3) + parse.group(2) + "\n")
-				write = False
-		if write:
-			copy.write(line)
-			
-def compareHeadersWithExpected(expected, all):
+	for file in student_sol:
+		# expect include guards, change them
+		if(file.readline().startswith("#ifndef")):
+			next(file)
+			next(file)
+			no_include_guards = False
+		else:
+			no_include_guards = True
+		for line in file:
+			write = True
+			for el in function_signatures:
+				parse = re.match("(\s*)" + el.group(1) + el.group(2) + el.group(3) + "(\s*\(.*)", line)
+				if parse:
+					copy.write(parse.group(1) + el.group(1) + el.group(2) + prefix + el.group(3) + parse.group(2) + "\n")
+					write = False
+			if write:
+				copy.write(line)
+		if no_include_guards:
+			copy.write("#endif")
+				
+def searchStudentSolution(main):
 	"""
-	Compares the set expected with all and returns the file student_sol.
+	Searches the student solution using main.cpp and the exact order of inclusion in it.
+	Returns a list of filenames.
 	"""
-	diff = all.difference(expected)
-	# get the student header
-	if len(diff) == 1:
-		return diff.pop()
-	else:
-		raise SystemExit("Problem finding the student's solution.")
+	return_list = []
+	other_headers = ["polyfit", "solution", "solution2", "solution3"] # other headers ending with .hpp
+	for line in main:
+		parse = re.match('\s*\#include\s*"(.*).hpp"', line)
+		if parse and parse.group(1) not in other_headers:
+			return_list.append(parse.group(1) + ".hpp")
+	return return_list
 
 def cmake():
 	"""
@@ -49,22 +59,16 @@ def cmake():
 	The tweaked copy is named 'copy.hpp'.
 	Will also put header guards into the file.
 	"""
-	expected_hpps_wout_student_sol = set(["../polyfit.hpp", "../copy.hpp", "../solution.hpp"])
-	all_hpps = set(glob.glob("../*.hpp"))
+	main = open("../main.cpp", "r")
+	files = searchStudentSolution(main)
+	main.close()
+	student_sol = []
+	for file in files:
+		student_sol.append(open("../" + file, "r"))
 	
-	file = compareHeadersWithExpected(expected_hpps_wout_student_sol, all_hpps)
-	
-	student_sol = open(file, "r")
 	copy = open("../copy.hpp", "w")
 	tests = open("../tests.cpp", "r")
-	
-	# expect include guards, change them
-	if(student_sol.readline().startswith("#ifndef")):
-		next(student_sol)
-		next(student_sol)
-		no_include_guards = False
-	else:
-		no_include_guards = True
+
 	copy.write("#ifndef COPY_HPP\n")
 	copy.write("#define COPY_HPP\n")
 	
@@ -72,11 +76,9 @@ def cmake():
 	
 	parseWriteChange(student_sol, copy, tests)
 	
-	if no_include_guards:
-		copy.write("#endif")
-	
 	copy.close()
-	student_sol.close()
+	for item in student_sol:
+		item.close()
 	tests.close()
 
 def codeexpert():
@@ -86,22 +88,16 @@ def codeexpert():
 	Will also put header guards into the file.
 	Puts copy.hpp in a writable folder on the server – MUST BE REMOVED AFTER COMPILING!
 	"""
-	expected_hpps_wout_student_sol = set(["polyfit.hpp", "solution.hpp"])
-	all_hpps = set(glob.glob("*.hpp"))
+	main = open("main.cpp", "r")
+	files = searchStudentSolution(main)
+	main.close()
+	student_sol = []
+	for file in files:
+		student_sol.append(open(file, "r"))
 	
-	file = compareHeadersWithExpected(expected_hpps_wout_student_sol, all_hpps)
-	
-	student_sol = open(file, "r")
 	copy = open("copy.hpp", "w")
 	tests = open("tests.cpp", "r")
 	
-	# expect include guards, change them
-	if(student_sol.readline().startswith("#ifndef")):
-		next(student_sol)
-		next(student_sol)
-		no_include_guards = False
-	else:
-		no_include_guards = True
 	copy.write("#ifndef COPY_HPP\n")
 	copy.write("#define COPY_HPP\n")
 	
@@ -109,11 +105,9 @@ def codeexpert():
 	
 	parseWriteChange(student_sol, copy, tests)
 	
-	if no_include_guards:
-		copy.write("#endif")
-	
 	copy.close()
-	student_sol.close()
+	for item in student_sol:
+		item.close()
 	tests.close()
 
 
