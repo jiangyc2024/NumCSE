2a6fac57fe9aab3beabb098ecac7d4f342428b4e
Maximilian Herde <herdem@student.ethz.ch>
2020-04-28 21:56:56 +0200

    testing integrated into cmake hierarchy

---
+++ CMake/Macros/add_tests_numcse.cmake
@@ -0,0 +1,21 @@
+include(CMake/Macros/get_target_name_numcse.cmake)
+
+# similiar to add_executable_numcse.cmake
+
+macro (add_tests_numcse _name)
+  get_target_name_numcse(copy target_name_copy)
+  get_target_name_numcse(${_name} target_name)
+
+  set(argv_parsed ${ARGV})
+  LIST_REPLACE(argv_parsed 0 "${target_name}")
+
+  set(TARGET_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+  string(REPLACE "${CMAKE_SOURCE_DIR}" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}" TARGET_RUNTIME_OUTPUT_DIRECTORY "${TARGET_RUNTIME_OUTPUT_DIRECTORY}")
+
+  add_custom_target(${target_name_copy} ALL COMMAND python3 ${PROJECT_SOURCE_DIR}/Testing/copy_and_tweak.py ${CMAKE_CURRENT_SOURCE_DIR}/template)
+  add_executable(${argv_parsed})
+  add_dependencies(${target_name} ${target_name_copy})
+  set_target_properties(${target_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${TARGET_RUNTIME_OUTPUT_DIRECTORY}" OUTPUT_NAME "${_name}")
+  add_test(NAME ${target_name} COMMAND ${target_name})
+
+endmacro()
--- Testing/copy_and_tweak.py
+++ Testing/copy_and_tweak.py
@@ -56,26 +56,27 @@ def searchStudentSolution(main):
 			return_list.append(parse.group(1) + ".hpp")
 	return return_list
 
-def cmake():
+def cmake(path_string):
 	"""
 	Will perform the copy and tweak operation if called from a cmake build system inside a build folder.
 	The tweaked copy is named 'copy.hpp'.
 	Will also put header guards into the file.
 	"""
-	main = open("../main.cpp", "r")
+	main = open(path_string + "/main.cpp", "r")
 	files = searchStudentSolution(main)
 	main.close()
 	student_sol = []
 	for file in files:
-		student_sol.append(open("../" + file, "r"))
+		student_sol.append(open(path_string + "/" + file, "r"))
 	
-	copy = open("../copy.hpp", "w")
-	tests = open("../tests.cpp", "r")
+	copy = open(path_string + "/copy.hpp", "w")
+	tests = open(path_string + "/tests.cpp", "r")
 	
 	copy.write("#ifndef COPY_HPP\n")
 	copy.write("#define COPY_HPP\n")
 	
-	copy.write('#include "../solution/{}"\n'.format(file[3:]))
+	for el in files:
+		copy.write('#include "../solution/{}"\n'.format(el))
 	
 	parseWriteChange(student_sol, copy, tests)
 	
@@ -121,10 +122,6 @@ def codeexpert():
 
 if __name__ == "__main__":
 	if len(sys.argv) == 2:
-		if str(sys.argv[1]) == "cmake":
-			cmake()
-		else:
-			raise SystemExit("Wrong usage: either 'cmake' as argument to run cmake version or \
-			nothing implying the CodeExpert version.")
+		cmake(str(sys.argv[1]))
 	else:
 		codeexpert()
