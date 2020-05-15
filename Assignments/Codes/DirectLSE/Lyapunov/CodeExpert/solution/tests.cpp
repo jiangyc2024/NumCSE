d01ba83e96f7d7836bb5bf71a2e0ceb6ccd3782c
Maximilian Herde <herdem@student.ethz.ch>
2020-05-15 20:32:19 +0200

    added independent tests for Lyapunov problem

---
+++ Assignments/Codes/DirectLSE/Lyapunov/CodeExpert/solution/tests.cpp
@@ -0,0 +1,41 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "copy.hpp"
+
+// includes for test data
+#include <Eigen/Dense>
+
+struct TestData {
+	TestData() {
+		A.resize(5, 5);
+		A << 10, 2, 3, 4, 5, 6, 20, 8, 9, 1, 1, 2, 30, 4, 5, 6, 7, 8, 20, 0, 1, 2, 3, 4, 10;
+	}
+	
+	Eigen::MatrixXd A;
+};
+
+TestData data;
+
+TEST_SUITE("Lyapunov") {
+	
+	TEST_CASE("SparseMatrix<double> buildC" * doctest::description("System matrix")) {
+		auto C_sol = buildC(data.A);
+		auto C_stud = buildC_TEST(data.A);
+		
+		CHECK((C_sol - C_stud).norm() == doctest::Approx(0.).epsilon(1e-6));
+	}
+	
+	TEST_CASE("void solveLyapunov" * doctest::description("Solution")) {
+		Eigen::MatrixXd X_sol, X_stud;
+		
+		solveLyapunov(data.A, X_sol);
+		solveLyapunov_TEST(data.A, X_stud);
+		
+		CHECK((X_sol - X_stud).norm() == doctest::Approx(0.).epsilon(1e-6));
+	}
+	
+	TEST_CASE("bool testLyapunov" * doctest::description("skipped") * doctest::skip()) {}
+	
+}
+
