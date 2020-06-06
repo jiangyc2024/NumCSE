c34f63eb5d3ccc57000887aea8d9a2f2952d91f4
anwillia <anwillia@ethz.ch>
2020-06-06 18:46:55 +0200

    Fixed formatting & typos

--- Assignments/Codes/ODE/SDIRK/CodeExpert/solution/SDIRK.hpp
+++ Assignments/Codes/ODE/SDIRK/CodeExpert/solution/SDIRK.hpp
@@ -11,9 +11,12 @@
 /* SAM_LISTING_BEGIN_0 */
 //! \brief One step of autonomous IVP y'' + y' + y = 0, [y(0), y'(0)] = z0 using
 //! SDIRK method Use SDIRK method for first order ode z' = f(z). Steps of size
-//! h. \tparam Eigen::VectorXd type of solution space y and initial data y0
-//! \param[in] z0 initial data z(0) \param[in] h size of the step \param[in]
-//! gamma parameter \return next step z1
+//! h. 
+//! \tparam Eigen::VectorXd type of solution space y and initial data y0
+//! \param[in] z0 initial data z(0)
+//! \param[in] h size of the step 
+//! \param[in] gamma parameter
+//! \return next step z1
 Eigen::Vector2d sdirkStep(const Eigen::Vector2d &z0, double h, double gamma) {
  
   Eigen::Vector2d res;
@@ -39,11 +42,14 @@ Eigen::Vector2d sdirkStep(const Eigen::Vector2d &z0, double h, double gamma) {
 
 /* SAM_LISTING_BEGIN_3 */
 //! \brief Solve autonomous IVP y'' + y' + y = 0, [y(0), y'(0)] = z0 using SDIRK
-//! method Use SDIRK method for first order ode z' = f(z), with N equidistant
-//! steps \tparam Eigen::VectorXd type of solution space z = [y,y']! and initial
-//! data z0 = [y(0), y'(0)] \param[in] z0 initial data z(0) \param[in] N number
-//! of equidistant steps \param[in] T final time of simulation \param[in] gamma
-//! parameter \return vector containing each step of z_k (y and y')
+//! method. Use SDIRK method for first order ode z' = f(z), with N equidistant
+//! steps.
+//! \tparam Eigen::VectorXd type of solution space z = [y,y']! and initial
+//! data z0 = [y(0), y'(0)] \param[in] z0 initial data z(0)
+//! \param[in] N number of equidistant steps
+//! \param[in] T final time of simulation 
+//! \param[in] gamma parameter
+//! \return vector containing each step of z_k (y and y')
 std::vector<Eigen::Vector2d> sdirkSolve(const Eigen::Vector2d &z0, unsigned int N, double T, double gamma) {
   // Solution vector
   std::vector<Eigen::Vector2d> res(N + 1);
