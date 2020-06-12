126cd0a0fbb1aa06e0638d31af5563d8b3103f51
anwillia <anwillia@ethz.ch>
2020-06-12 15:58:23 +0200

    Fixed tests

--- Assignments/Codes/ODE/SDIRK/CodeExpert/solution/tests.cpp
+++ Assignments/Codes/ODE/SDIRK/CodeExpert/solution/tests.cpp
@@ -7,13 +7,11 @@
 #include <vector>
 
 struct TestData {
-	// TODO: test with the commented-out values (current ones are
-	// the ones mentioned in the assignment so should work...)
 	TestData() {
-		z0 << 0.0, 0.0;
-		gamma = {/*1.0,*/ (3 + std::sqrt(3))/6.0};
-		T = {/*5.0, 6.0, 7.0, 9.0,*/ 10.0};
-		N = {20, 40, 80, 160, 320, 640};
+		z0 << 1.0, 0.0;
+		gamma = {1.0, (3 + std::sqrt(3))/6.0};
+		T = {5.0, 6.0, 7.0, 9.0, 10.0};
+		N = {20, 40, 80, 160, 320};
 	}
 	
 	Eigen::Vector2d z0;
@@ -25,7 +23,6 @@ struct TestData {
 
 TestData data;
 
-// TODO: trivially passing. 0 is getting returned all the time.
 TEST_SUITE("SDIRK") {
 	TEST_CASE("Eigen::Vector2d sdirkStep" * doctest::description("sdirkStep")) {
 		for (int i = 0; i < data.gamma.size(); i++) {
@@ -37,32 +34,24 @@ TEST_SUITE("SDIRK") {
 					Eigen::Vector2d sol = sdirkStep(data.z0, h, data.gamma[i]);
 					Eigen::Vector2d stud = sdirkStep_TEST(data.z0, h, data.gamma[i]);
 					
-					/*
-					// Debugging
-					printf("Sol: %f, %f \n", sol(0), sol(1));
-					printf("Stud: %f, %f \n\n", stud(0), stud(1));
-					*/
-					
 					CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));
 				}
 			}
 		}
 	}
-
-	TEST_CASE("std::vector<Eigen::Vector2d> sdirkSolve" * doctest::description("sdirkSolve") * doctest::skip()) {
+	
+	TEST_CASE("std::vector<Eigen::Vector2d> sdirkSolve" * doctest::description("sdirkSolve")) {
 		for (int i = 0; i < data.gamma.size(); i++) {
 			for (int j = 0; j < data.T.size(); j++) {
 				for (int k = 0; k < data.N.size(); k++) {
 					std::vector<Eigen::Vector2d> sol_vec = sdirkSolve(data.z0, data.N[k], data.T[j], data.gamma[i]);
 					std::vector<Eigen::Vector2d> stud_vec = sdirkSolve_TEST(data.z0, data.N[k], data.T[j], data.gamma[i]);
 					
-					CHECK(sol_vec.size() == stud_vec.size());
-					for (int l = 0; l < sol_vec.size(); l++) {
-						Eigen::Vector2d sol = sol_vec[l];
-						Eigen::Vector2d stud = sol_vec[l];
-						
-						CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));
-					}
+					// Check final value
+					Eigen::Vector2d sol = sol_vec.back();
+					Eigen::Vector2d stud = stud_vec.back();
+					
+					CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));					
 				}
 			}
 		}
