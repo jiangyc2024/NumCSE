1097c98478f0df117744899d82074862dd29e591
anwillia <anwillia@ethz.ch>
2020-06-14 20:19:25 +0200

    Remove newlines in functions for testing

--- Assignments/Codes/IterativeMethods/QuadraticConvergence/CodeExpert/solution/quadraticconvergence.hpp
+++ Assignments/Codes/IterativeMethods/QuadraticConvergence/CodeExpert/solution/quadraticconvergence.hpp
@@ -83,9 +83,7 @@ void testSteffensen(void) {
  */
 
 /* SAM_LISTING_BEGIN_3 */
-template <class Function>
-double steffensen_log(Function &&f, double x0,
-                      Logger<double> *logger_p = nullptr) {
+template <class Function> double steffensen_log(Function &&f, double x0, Logger<double> *logger_p = nullptr) {
 
   double x = x0;
   // TO DO (9-4.c): Modify the function steffensen: use the class Logger to
