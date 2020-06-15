d36111988b95718260f81d943a2e924e1e960ae9
anwillia <anwillia@ethz.ch>
2020-06-15 11:55:51 +0200

    Prettier printing

--- Assignments/Codes/NumericalQuadrature/ImproperIntegrals/CodeExpert/template/main.cpp
+++ Assignments/Codes/NumericalQuadrature/ImproperIntegrals/CodeExpert/template/main.cpp
@@ -5,7 +5,7 @@ int main() {
     int n = 10;
     double I = quadinf(n, [] (double t) { return 1 / (1 + std::pow(t,2)); });
     //Note: exact integral = pi
-    std::cout << "Approximated integral =" << I << std::endl;
+    std::cout << "Approximated integral = " << I << std::endl;
     
     //plot error
     cvgQuadInf();
