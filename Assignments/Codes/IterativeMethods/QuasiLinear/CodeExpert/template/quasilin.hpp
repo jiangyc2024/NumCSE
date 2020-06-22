3286c7a7b11d7a2e3b8d0383350e98e4823a6c82
anwillia <anwillia@ethz.ch>
2020-06-22 18:56:10 +0200

    Removed temp return, now handled by test logic

--- Assignments/Codes/IterativeMethods/QuasiLinear/CodeExpert/template/quasilin.hpp
+++ Assignments/Codes/IterativeMethods/QuasiLinear/CodeExpert/template/quasilin.hpp
@@ -13,10 +13,6 @@ Eigen::VectorXd fixed_point_step(const Eigen::VectorXd &xk, const Eigen::VectorX
   Eigen::VectorXd x_new;
   // TO DO (9-10.b): implement one step of the fixed point iteration
   // START
- 
-  // Placeholder for compilation. Replace with your own solution.
-  Eigen::VectorXd temp = Eigen::VectorXd::Zero(b.size());
-  return temp;
   
   // END
   return x_new;
@@ -50,10 +46,6 @@ Eigen::VectorXd newton_step(const Eigen::VectorXd &x, const Eigen::VectorXd &b) 
   // TO DO (9-10.f): implement one Newton step exploiting the
   // Sherman-Morrison-Woodbury formula.
   // START
-  
-  // Placeholder for compilation. Replace with your own solution.
-  Eigen::VectorXd temp = Eigen::VectorXd::Zero(x.size());
-  return temp;
   
   // END
   return x_new;
