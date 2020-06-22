7931acf1ad0f821b377c22d9428ddec3003362eb
anwillia <anwillia@ethz.ch>
2020-06-22 18:59:50 +0200

    Added message for skipped printing functions

--- Assignments/Codes/IterativeMethods/QuadraticConvergence/CodeExpert/solution/tests.cpp
+++ Assignments/Codes/IterativeMethods/QuadraticConvergence/CodeExpert/solution/tests.cpp
@@ -22,17 +22,23 @@ TestData data;
 
 TEST_SUITE("Steffensen") {
 	TEST_CASE("double steffensen" * doctest::description("Testing Steffensen")) {
-		// Test on the same function that testSteffensen() uses
+		// Test on the same paramters that testSteffensen() uses
 		double sol = steffensen(data.f, data.x0);
 		double stud = steffensen_TEST(data.f, data.x0);
 		
 		CHECK(std::abs(sol - stud) == doctest::Approx(0.).epsilon(1e-6));
 	}
 	
-	TEST_CASE("void testSteffensen" * doctest::description("Test Steffensen") * doctest::skip()) {}
+	TEST_CASE("void testSteffensen" * doctest::description("Test Steffensen")) {
+		MESSAGE("This function wasn't tested. Run the program to see its output.");
+	}
 	
-	TEST_CASE("double steffensen_log" * doctest::description("Steffensen log") * doctest::skip()) {}
+	TEST_CASE("double steffensen_log" * doctest::description("Steffensen log") * doctest::skip()) {
+		MESSAGE("This function wasn't tested. Run the program to see its output.");
+	}
 	
-	TEST_CASE("void orderSteffensen" * doctest::description("Steffensen order") * doctest::skip()) {}
+	TEST_CASE("void orderSteffensen" * doctest::description("Steffensen order") * doctest::skip()) {
+		MESSAGE("This function wasn't tested. Run the program to see its output.");
+	}
 }
 
