182faa7c7f35712880a3c3640c077e3685ae244d
anwillia <anwillia@ethz.ch>
2020-09-19 16:53:35 +0200

    General structure

---
+++ LectureCodes/Evp/pcgbase/Eigen/main.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////
+/// Demonstration code for lecture "Numerical Methods for CSE" @ ETH Zurich
+/// Porting pcgbase.m to C++/Eigen.
+/// (C) 2020 SAM, D-MATH
+/// Author(s): William Andersson
+/// Repository: https://gitlab.math.ethz.ch/NumCSE/NumCSE/
+/// Do not remove this header.
+//////////////////////////////////////////////////////////////////////////
+
+#include <Eigen/Dense>
+
+#include "pcgbase.hpp"
+
+int main() {
+
+}
---
+++ LectureCodes/Evp/pcgbase/Eigen/pcgbase.cpp
@@ -0,0 +1,8 @@
+///////////////////////////////////////////////////////////////////////////
+/// Demonstration code for lecture "Numerical Methods for CSE" @ ETH Zurich
+/// Porting pcgbase.m to C++/Eigen.
+/// (C) 2020 SAM, D-MATH
+/// Author(s): William Andersson
+/// Repository: https://gitlab.math.ethz.ch/NumCSE/NumCSE/
+/// Do not remove this header.
+//////////////////////////////////////////////////////////////////////////
