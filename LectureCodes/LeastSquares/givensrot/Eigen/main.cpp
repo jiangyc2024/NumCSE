1b2971e5e89f41b4ca374d072067098d3ff9d2d4
ralfh <ralfh@ethz.ch>
2020-09-25 09:55:18 +0200

    Reformatting and slight changes in code computing givens rotations

--- LectureCodes/LeastSquares/givensrot/Eigen/main.cpp
+++ LectureCodes/LeastSquares/givensrot/Eigen/main.cpp
@@ -6,56 +6,70 @@
 /// Do not remove this header.
 //////////////////////////////////////////////////////////////////////////
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 #include <Eigen/Dense>
 
-#include "planerot.hpp"
 #include "givenscoltrf.hpp"
+#include "planerot.hpp"
 #include "qrgivens.hpp"
 
-int main () {
-	std::cout << "#####################################" << std::endl;
-	std::cout << "Test of planerot" << std::endl;
-	std::cout << "#####################################" << std::endl;
-	Eigen::Vector2d a;
-	a << 2,1;
-	Eigen::Matrix2d G;
-	Eigen::Vector2d x;
-	planerot(a, G, x);	
-	std::cout << "a=\n" << a << std::endl << "G=\n" << G << std::endl << "x=\n" << x << std::endl;
-	std::cout << "Error norm (Gx-a) = " << (G*a-x).norm() << std::endl;
+int main() {
+  std::cout << "#####################################" << std::endl;
+  std::cout << "Test of planerot" << std::endl;
+  std::cout << "#####################################" << std::endl;
+  Eigen::Vector2d a;
+  a << 2, 1;
+  Eigen::Matrix2d G;
+  Eigen::Vector2d x;
+  planerot(a, G, x);
+  std::cout << "a=\n"
+            << a << std::endl
+            << "G=\n"
+            << G << std::endl
+            << "x=\n"
+            << x << std::endl;
+  std::cout << "Error norm (Gx-a) = " << (G * a - x).norm() << std::endl;
 
+  std::cout << "#####################################" << std::endl;
+  std::cout << "Test of givenscoltrf" << std::endl;
+  std::cout << "#####################################" << std::endl;
+  int n = 4;
+  VectorXd r1(n);
+  Eigen::VectorXd aIn(n);
+  aIn << 1, 3, 4, 8; //, 9, 42, 2401, 343;
+  MatrixXd Q1(n, n);
+  givenscoltrf(aIn, Q1, r1);
+  std::cout << "aIn=\n"
+            << aIn << std::endl
+            << "Q=\n"
+            << Q1 << std::endl
+            << "aOut=\n"
+            << r1 << std::endl;
+  std::cout << "Error norm (Q'Q-I) = "
+            << (Q1.transpose() * Q1 - Eigen::MatrixXd::Identity(n, n)).norm()
+            << std::endl;
+  std::cout << "Error norm (Qr-a) = " << (Q1 * r1 - aIn).norm() << std::endl;
 
-	std::cout << "#####################################" << std::endl;
-	std::cout << "Test of givenscoltrf" << std::endl;
-	std::cout << "#####################################" << std::endl;
-	int n = 4;
-	VectorXd r1(n);
-	Eigen::VectorXd aIn(n);
-	aIn << 1, 3, 4, 8;//, 9, 42, 2401, 343;
-	MatrixXd Q1(n, n);
-	givenscoltrf(aIn, Q1, r1);
-	std::cout << "aIn=\n" << aIn << std::endl << "Q=\n" << Q1 << std::endl << "aOut=\n" << r1 << std::endl;
-	std::cout << "Error norm (Q'Q-I) = " << (Q1.transpose() * Q1 - Eigen::MatrixXd::Identity(n,n)).norm() << std::endl;
-	std::cout << "Error norm (Qr-a) = " << (Q1*r1 - aIn).norm() << std::endl;
-	
-
-	std::cout << "#####################################" << std::endl;
-	std::cout << "Test of qrgivens" << std::endl;
-	std::cout << "#####################################" << std::endl;
-	std::cout << "----------------------------------------" << std::endl;
-	MatrixXd AIn(n, n);
-	AIn << 1, 2, 4, 7,
-			9, 4, 2, 9,
-			7, 5, 9, 2,
-			3, 4, 4, 8;
-	MatrixXd R2(n, n);
-	MatrixXd Q2(n, n);	
-	qrgivens(AIn, Q2, R2);
-	std::cout << "AIn=\n" << AIn << std::endl << "Q=\n" << Q2 << std::endl << "R=\n" << R2 << std::endl;
-	std::cout << "Error norm (Q'Q-I) = " <<  (Q2.transpose() * Q2 - Eigen::MatrixXd::Identity(n,n)).norm() << std::endl;
-	std::cout << "Error norm (QR-A) = " << (Q2*R2 - AIn).norm() << std::endl;
-	return 0;
+  std::cout << "#####################################" << std::endl;
+  std::cout << "Test of qrgivens" << std::endl;
+  std::cout << "#####################################" << std::endl;
+  std::cout << "----------------------------------------" << std::endl;
+  MatrixXd AIn(n, n);
+  AIn << 1, 2, 4, 7, 9, 4, 2, 9, 7, 5, 9, 2, 3, 4, 4, 8;
+  MatrixXd R2(n, n);
+  MatrixXd Q2(n, n);
+  qrgivens(AIn, Q2, R2);
+  std::cout << "AIn=\n"
+            << AIn << std::endl
+            << "Q=\n"
+            << Q2 << std::endl
+            << "R=\n"
+            << R2 << std::endl;
+  std::cout << "Error norm (Q'Q-I) = "
+            << (Q2.transpose() * Q2 - Eigen::MatrixXd::Identity(n, n)).norm()
+            << std::endl;
+  std::cout << "Error norm (QR-A) = " << (Q2 * R2 - AIn).norm() << std::endl;
+  return 0;
 }
