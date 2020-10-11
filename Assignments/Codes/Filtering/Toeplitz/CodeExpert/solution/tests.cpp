8c932f9cf82f4f5e603261fbe1d684360668fcb2
Langwen Huang <huanglangwen@outlook.com>
2020-10-11 23:49:13 +0200

    Update Problem 5-4 Toeplitz, Update test doc and format code

--- Assignments/Codes/Filtering/Toeplitz/CodeExpert/solution/tests.cpp
+++ Assignments/Codes/Filtering/Toeplitz/CodeExpert/solution/tests.cpp
@@ -1,43 +1,43 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-
 #include "copy.hpp"
+#include "doctest.h"
 
 // includes for test data
 #include <Eigen/Dense>
 
 TEST_SUITE("Toeplitz") {
+  TEST_CASE("MatrixXd toeplitz" * doctest::description("Toeplitz matrix")) {
+    Eigen::VectorXd c = Eigen::VectorXd::Random(50);
+    Eigen::VectorXd r = Eigen::VectorXd::Random(50);
 
-	TEST_CASE("MatrixXd toeplitz" * doctest::description("Toeplitz matrix")) {
-		Eigen::VectorXd c = Eigen::VectorXd::LinSpaced(50, 1, 50);
-		Eigen::VectorXd r = Eigen::VectorXd::LinSpaced(50, 1, 40);
+    auto sol = toeplitz(c, r);
+    auto stud = toeplitz_TEST(c, r);
 
-		auto sol = toeplitz(c, r);
-		auto stud = toeplitz_TEST(c, r);
-		
-		REQUIRE(sol.rows() == stud.rows());
-		REQUIRE(sol.cols() == stud.cols());
-		CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));
-	}
-	
-	TEST_CASE("VectorXd toepmatmult" * doctest::description("skipped") * doctest::skip()) {}
-	
-	TEST_CASE("VectorXd toepmult" * doctest::description("skipped") * doctest::skip()) {}
-	
-	TEST_CASE("VectorXd ttmatsolve" * doctest::description("skipped") * doctest::skip()) {}
-	
-	TEST_CASE("VectorXd ttrecsolve" * doctest::description("skipped") * doctest::skip()) {}
-	
-	TEST_CASE("VectorXd ttsolve" * doctest::description("ttsolve wrapper")) {
-		Eigen::VectorXd h = Eigen::VectorXd::LinSpaced(50, 1, 50);
-		Eigen::VectorXd y = Eigen::VectorXd::LinSpaced(50, 51, 100);
+    REQUIRE(sol.rows() == stud.rows());
+    REQUIRE(sol.cols() == stud.cols());
+    CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));
+  }
 
-		auto sol = ttsolve(h, y);
-		auto stud = ttsolve_TEST(h, y);
-		
-		REQUIRE(sol.size() == stud.size());
-		CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));
-	}
-	
+  TEST_CASE("VectorXd toepmatmult" * doctest::description("skipped") *
+            doctest::skip()) {}
+
+  TEST_CASE("VectorXd toepmult" * doctest::description("skipped") *
+            doctest::skip()) {}
+
+  TEST_CASE("VectorXd ttmatsolve" * doctest::description("skipped") *
+            doctest::skip()) {}
+
+  TEST_CASE("VectorXd ttrecsolve" * doctest::description("skipped") *
+            doctest::skip()) {}
+
+  TEST_CASE("VectorXd ttsolve" * doctest::description("ttsolve wrapper")) {
+    Eigen::VectorXd h = Eigen::VectorXd::LinSpaced(50, 1, 50);
+    Eigen::VectorXd y = Eigen::VectorXd::LinSpaced(50, 51, 100);
+
+    auto sol = ttsolve(h, y);
+    auto stud = ttsolve_TEST(h, y);
+
+    REQUIRE(sol.size() == stud.size());
+    CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));
+  }
 }
-
