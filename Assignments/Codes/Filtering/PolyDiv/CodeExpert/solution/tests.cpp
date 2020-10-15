d901189edc4e4be62587f47ed33965e5ae0512b2
gbai <bgm1996@outlook.com>
2020-10-15 08:50:13 +0200

    Problem 5-3: Multiplication and division of polynomials based on FFT
    fixed format bug in tests.cpp to be compatible with copy script

--- Assignments/Codes/Filtering/PolyDiv/CodeExpert/solution/tests.cpp
+++ Assignments/Codes/Filtering/PolyDiv/CodeExpert/solution/tests.cpp
@@ -38,9 +38,9 @@ TEST_SUITE("polyDiv") {
     CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-6));
   }
 
-  TEST_CASE(
-      "VectorXd polyDiv" *
-      doctest::description("Polynomial division -- efficient implementation")) {
+  TEST_CASE("VectorXd polyDiv" *
+      doctest::description(
+              "Polynomial division -- efficient implementation")) {
     auto sol_uv = polyMult_fast(data.u, data.v);
     auto stud_uv = polyMult_fast_TEST(data.u, data.v);
     auto sol_v = polyDiv(sol_uv, data.v);
