cfa652ee584b6fe7fd3ac3f8615097aa81fdef52
tandrig <tandrig@sam.math.ethz.ch>
2020-10-29 18:20:22 +0100

    update template

--- Assignments/Codes/Filtering/PolyDiv/CodeExpert/template/polydiv.hpp
+++ Assignments/Codes/Filtering/PolyDiv/CodeExpert/template/polydiv.hpp
@@ -1,8 +1,8 @@
+#include <iostream>
+#include <Eigen/Dense>
 #include <algorithm>
 #include <cstdlib>
 #include <limits>
-
-#include <Eigen/Dense>
 #include <unsupported/Eigen/FFT>
 
 using namespace Eigen;
@@ -13,26 +13,21 @@ using namespace Eigen;
  * @param[out] uv Vector of coefficients of polynomial $uv = u*v$
  */
 /* SAM_LISTING_BEGIN_0 */
-VectorXd polyMult_naive(const VectorXd & u, const VectorXd & v)
-{
-	// Initialization
-	int m = u.size();
-	int n = v.size();
-	int dim = std::max(m, n);
-	
-	VectorXd u_tmp = u;
-	u_tmp.conservativeResizeLike(VectorXd::Zero(dim));
-	VectorXd v_tmp = v;
-	v_tmp.conservativeResizeLike(VectorXd::Zero(dim));
-	
-	VectorXd uv(m+n-1); // Degree is (m-1) + (n-1)
-	
-	// TODO: multiply polynomials $u$ and $v$ naively
-	// START
-	
-	// END
-	
-	return uv;
+VectorXd polyMult_naive(const VectorXd &u, const VectorXd &v) {
+  // Fetch degrees of input polynomials
+  int degu = u.size() - 1;
+  int degv = v.size() - 1;
+  // Object for product polynomial p = uv
+  int degp = degu + degv;
+
+  VectorXd uv(degp + 1);
+
+  // TODO: multiply polynomials $u$ and $v$ naively
+  // START
+
+  // END
+
+  return uv;
 }
 /* SAM_LISTING_END_0 */
 
@@ -42,25 +37,24 @@ VectorXd polyMult_naive(const VectorXd & u, const VectorXd & v)
  * @param[out] uv Vector of coefficients of polynomial $uv = u*v$
  */
 /* SAM_LISTING_BEGIN_1 */
-VectorXd polyMult_fast(const VectorXd & u, const VectorXd & v)
-{
-	// Initialization
-	int m = u.size();
-	int n = v.size();
-	
-	VectorXd u_tmp = u;
-	u_tmp.conservativeResizeLike(VectorXd::Zero(u.size() + n - 1));
-	VectorXd v_tmp = v;
-	v_tmp.conservativeResizeLike(VectorXd::Zero(v.size() + m - 1));
-	
-	VectorXd uv;
-	
-	// TODO: multiply polynomials $u$ and $v$ efficiently
-	// START
-	
-	// END
-	
-	return uv;
+VectorXd polyMult_fast(const VectorXd &u, const VectorXd &v) {
+  // Initialization
+  int m = u.size();
+  int n = v.size();
+
+  VectorXd u_tmp = u;
+  u_tmp.conservativeResizeLike(VectorXd::Zero(u.size() + n - 1));
+  VectorXd v_tmp = v;
+  v_tmp.conservativeResizeLike(VectorXd::Zero(v.size() + m - 1));
+
+  VectorXd uv;
+
+  // TODO: multiply polynomials $u$ and $v$ efficiently
+  // START
+
+  // END
+
+  return uv;
 }
 /* SAM_LISTING_END_1 */
 
@@ -71,31 +65,25 @@ VectorXd polyMult_fast(const VectorXd & u, const VectorXd & v)
  * from $uv = u*v$ (division without remainder)
  */
 /* SAM_LISTING_BEGIN_2 */
-VectorXd polyDiv(const VectorXd & uv, const VectorXd & u)
-{
-	// Initialization
-	int mn = uv.size();
-	int m = u.size();
-	int dim = std::max(mn, m);
-	
-	VectorXd uv_tmp = uv;
-	uv_tmp.conservativeResizeLike(VectorXd::Zero(dim));
-	VectorXd u_tmp  = u;
-	u_tmp.conservativeResizeLike(VectorXd::Zero(dim));
-	
-	/* SAM_LISTING_BEGIN_3 */
-	// TODO: make sure that $uv$ can be divided by $u$
-	// START
-	
-	// END
-	/* SAM_LISTING_END_3 */
-	
-	VectorXd v;
-	
-	// TODO: divide polynomials $uv$ and $u$ efficiently (no remainder)
-	// START
-	
-	// END
-	return v;
+VectorXd polyDiv(const VectorXd &uv, const VectorXd &u) {
+  // Initialization
+  int mn = uv.size();
+  int m = u.size();
+  // need well behaved input
+  assert(uv(mn - 1) != 0. && u(m - 1) != 0.);
+  if (mn < m) {
+    std::cerr << "uv can't be divided by u\n";
+    return Eigen::VectorXd(0);
+  }
+  int dim = mn;
+  int n = mn - m + 1;
+
+  VectorXd v;
+
+  // TODO: divide polynomials $uv$ and $u$ efficiently
+  // START
+  
+  // END
+  return v;
 }
 /* SAM_LISTING_END_2 */
