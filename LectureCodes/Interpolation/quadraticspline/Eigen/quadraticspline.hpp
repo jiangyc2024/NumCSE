373ec9bc582ce40840d3cce481c803557d354efd
ralfh <ralfh@ethz.ch>
2020-11-08 22:12:26 +0100

    Eidting of lecure codes

--- LectureCodes/Interpolation/quadraticspline/Eigen/quadraticspline.hpp
+++ LectureCodes/Interpolation/quadraticspline/Eigen/quadraticspline.hpp
@@ -1,17 +1,26 @@
-# include <Eigen/Dense>
-using Eigen::VectorXd;
 
+# include <Eigen/Dense>
+
+using Eigen::VectorXd;
 // choice of extra knots \Blue{$p_i \in \left]t_{i-1}, t_i\right[$}
 // for given nodeset \texttt{t} and coefficients \texttt{c}
-void extra_knots(const VectorXd& t, const VectorXd& c, VectorXd& p) {
+/* SAM_LISTING_BEGIN_1 */
+Eigen::VectorXd extra_knots(const Eigen::VectorXd& t, const Eigen::VectorXd &y,
+			    const Eigen::VectorXd& c) {
   const unsigned n = t.size();
-  p = (t(n - 1) - 1)*VectorXd::Ones(n - 1);
+  assert((n == y.size()) && (n == c.size()));
+  Eigen::VectorXd p = (t[n - 1] + 1.0)*Eigen::VectorXd::Ones(n - 1);
   for (unsigned j = 0; j < n - 1; ++j) {
-    if (c(j) != c(j + 1)) {
-      p(j) = (y(j+1) - y(j) + t(j)*c(j) - t(j+1)*c(j+1)) / (c(j+1)- c(j));
+    // Safe comparison, just to avoid division by zero exception
+    // If both slopes a close $\cob{p_j}$ will certainly lie outside the node interval 
+    if (c[j] != c[j + 1]) {
+      p[j] = (y[j+1] - y[j] + t[j]*c[j] - t[j+1]*c[j+1]) / (c[j+1]- c[j]);
     }
-    if (p(j) < t(j) || p(j) > t(j+1)) {
-      p(j) = 0.5*(t(j) + t(j+1));
+    if (p[j] < t[j] || p[j] > t[j+1]) {
+      p[j] = 0.5*(t[j] + t[j+1]);
     }
   }
+  return p;
 }
+
+/* SAM_LISTING_END_1 */
