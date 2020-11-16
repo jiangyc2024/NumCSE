17a35ee858203081b5141126212880867e7d38a7
Vivienne Langen <vlangen@stud.phys.ethz.ch>
2020-11-16 20:04:39 +0100

    clang formatted and small changes

--- Assignments/Codes/PolynomialInterpolation/ChebIntp2/CodeExpert/solution/main.cpp
+++ Assignments/Codes/PolynomialInterpolation/ChebIntp2/CodeExpert/solution/main.cpp
@@ -1,34 +1,33 @@
 #include <Eigen/Dense>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 #include "adaptivepolyintp.hpp"
 
 int main() {
-  
-  auto f = [](double t) { return std::sin(std::exp(2*t)); };
+  auto f = [](double t) { return std::sin(std::exp(2 * t)); };
 
   // Test interval
   const double a = 0, b = 1;
-  
+
   // Get interpolation nodes and print runtimes
-  const unsigned N = 100; // no. of sampling points
-  const double tol = 1e-4; // tolerance
-  
-  Eigen::VectorXd tf; // nodes for interpolation of f
+  const unsigned N = 100;   // no. of sampling points
+  const double tol = 1e-4;  // tolerance
+
+  Eigen::VectorXd tf;  // nodes for interpolation of f
   tf = adaptivepolyintp(f, a, b, tol, N);
-  
+
   std::cout << tf << std::endl << std::endl;
-  
-  std::vector<double> ef; // errors for f
+
+  std::vector<double> ef;  // errors for f
   tf = adaptivepolyintp(f, a, b, tol, N, &ef);
-  
-  std::cout << "n \t\t \\eps_n" << std::endl << 
-               "-------------------------" << std::endl;
-  for (int i = 0; i < ef.size(); ++i ) {
-    std::cout << i+1 << "\t\t" << ef[i] << std::endl;
+
+  std::cout << "n \t\t \\eps_n" << std::endl
+            << "-------------------------" << std::endl;
+  for (unsigned int i = 0; i < ef.size(); ++i) {
+    std::cout << i + 1 << "\t\t" << ef[i] << std::endl;
   }
-  
+
   plotInterpolationError();
   return 0;
 }
--- Assignments/Codes/PolynomialInterpolation/ChebIntp2/CodeExpert/template/adaptivepolyintp.hpp
+++ Assignments/Codes/PolynomialInterpolation/ChebIntp2/CodeExpert/template/adaptivepolyintp.hpp
@@ -1,20 +1,19 @@
 #ifndef ADAPTINTERP_HPP
 #define ADAPTINTERP_HPP
 
-#include <cmath>
 #include <Eigen/Dense>
+#include <cmath>
 #include <iostream>
 #include <vector>
 #include "intpolyval.hpp"
 #include "matplotlibcpp.h"
-
 
 using namespace Eigen;
 namespace plt = matplotlibcpp;
 /*!
  * \brief adaptivepolyintp
  * \tparam Function
- * \paramin f 
+ * \paramin f
  * \paramin a
  * \paramin b
  * \paramin tol
@@ -23,31 +22,31 @@ namespace plt = matplotlibcpp;
  */
 /* SAM_LISTING_BEGIN_1 */
 template <class Function>
-VectorXd adaptivepolyintp(Function&& f, double a, double b, double tol, int N, std::vector<double> *errortab = nullptr) {
+VectorXd adaptivepolyintp(Function&& f, double a, double b, double tol, int N,
+                          std::vector<double>* errortab = nullptr) {
   // TO DO (7-3.a) : implement the greedy algorithm for adaptive interpolation.
   // Ignore the errortab part of this function for now.
-  
+
   // Generate sampling points and evaluate $f$ there
   VectorXd sampling_points = VectorXd::LinSpaced(N, a, b),
-                  fvals_at_sampling_points = sampling_points.unaryExpr(f);
-  //START
-  
-  //END
-  return sampling_points; // return all sampling points
+           fvals_at_sampling_points = sampling_points.unaryExpr(f);
+  // START
+
+  // END
+  return sampling_points;  // return all sampling points
 }
 /* SAM_LISTING_END_1 */
-
 
 /* SAM_LISTING_BEGIN_2 */
 void plotInterpolationError(void) {
   // Declare test functions
-  auto f1 = [](double t) { return std::sin(std::exp(2*t)); };
-  auto f2 = [](double t) { return std::sqrt(t)/(1 + 16*t*t); };
+  auto f1 = [](double t) { return std::sin(std::exp(2 * t)); };
+  auto f2 = [](double t) { return std::sqrt(t) / (1 + 16 * t * t); };
   // TO DO (7-3.c): generate the plots of error vs number of nodes for f1, f2
-  
-  //START
 
-  //END
+  // START
+
+  // END
 }
 /* SAM_LISTING_END_2 */
 #endif
