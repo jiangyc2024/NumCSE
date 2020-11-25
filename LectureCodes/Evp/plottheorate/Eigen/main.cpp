7fe1ea81946837e20f49db1f4745da7dc363cb05
vlangen <vivienne.langen@math.ethz.ch>
2020-11-25 13:52:56 +0100

    Problem (7-1): Piecewise linear interpolation on graded meshes

---
+++ LectureCodes/Evp/plottheorate/Eigen/main.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////
+/// Demonstration code for lecture "Numerical Methods for CSE" @ ETH Zurich
+/// Porting plottheorate.m to C++/Eigen.
+/// (C) 2020 SAM, D-MATH
+/// Author(s): <Author>
+/// Repository: https://gitlab.math.ethz.ch/NumCSE/NumCSE/
+/// Do not remove this header.
+//////////////////////////////////////////////////////////////////////////
+
+#include <Eigen/Dense>
+
+#include "plottheorate.hpp"
+
+int main() {
+
+}
