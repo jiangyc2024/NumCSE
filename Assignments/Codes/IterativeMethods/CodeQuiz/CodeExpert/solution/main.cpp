0017221e336e9de284dcf7f0dfffa3c8a31bdcc0
Vivienne Langen <vlangen@stud.phys.ethz.ch>
2020-12-07 19:48:29 +0100

    small typo

--- Assignments/Codes/IterativeMethods/CodeQuiz/CodeExpert/solution/main.cpp
+++ Assignments/Codes/IterativeMethods/CodeQuiz/CodeExpert/solution/main.cpp
@@ -5,9 +5,9 @@
 
 int main(void) {
   // You can test myfunction() for different input values:
-  std::cout << "myfunction(10) = " << myfunction(1.3924) << std::endl;
+  std::cout << "myfunction(10) = " << myfunction(10) << std::endl;
 
   // Tests expect this line to be left unmodified:
-  std::cout << "myfunction_modified(10) = " << myfunction_modified(1.3924)
+  std::cout << "myfunction_modified(10) = " << myfunction_modified(10)
             << std::endl;
 }
