9b309f2abc8ece8b3ff3d3c7698f3ae9db81b850
Maximilian Herde <herdem@student.ethz.ch>
2020-12-15 12:09:29 +0100

    included testing of optional function

--- Assignments/Codes/NumericalQuadrature/ImproperIntegrals/CodeExpert/solution/tests.cpp
+++ Assignments/Codes/NumericalQuadrature/ImproperIntegrals/CodeExpert/solution/tests.cpp
@@ -25,7 +25,18 @@ TEST_SUITE("ImproperIntegrals") {
     CHECK(sol == doctest::Approx(stud).epsilon(1e-8));
   }
 
-  TEST_CASE("double quad" * doctest::description("Optional helper function") * doctest::skip()) {}
+  TEST_CASE("double quad" * doctest::description("quad (optional)")) {
+    MESSAGE("This tests the optional function quad.");
+    MESSAGE("If you choose not to implement it, this case will fail.");
+    
+    Eigen::VectorXd w, x;
+    golubwelsh(data.n, w, x);
+    
+    const double sol = quad(data.f, w, x, 0., M_PI);
+    const double stud = quad_TEST(data.f, w, x, 0., M_PI);
+    
+    CHECK(sol == doctest::Approx(stud).epsilon(1e-8));
+  }
 
   TEST_CASE("void cvgQuadInf" * doctest::description("Convergence")) {
     MESSAGE("This function wasn't tested. Run the program to see its output.");
