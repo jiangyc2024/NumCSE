a36b8e9ba31068ebf8ecacb33dc9554cae3822e6
ralfh <ralfh@ethz.ch>
2021-01-21 22:26:24 +0100

    Updated implementation of Broyden's method

--- LectureCodes/IterativeMethods/broyden/Eigen/broyd.hpp
+++ LectureCodes/IterativeMethods/broyden/Eigen/broyd.hpp
@@ -9,9 +9,8 @@
 #define BROYD_HPP
 
 #include <Eigen/Dense>
-#include "void_cb.hpp"
 
-using namespace Eigen;
+#include "void_cb.hpp"
 
 // convenience typedef
 template <typename T, int N>
@@ -19,7 +18,7 @@ using Vector = Eigen::Matrix<T, N, 1>;
 
 /**
  * \brief Good Broyden rank-1-update quasi-Newton method
- * Straightforward implementation for small problems
+ * Naive and inefficient implementation for small problems
  * \param F Non-linear mapping in n dimensions
  * \param x initial guess
  * \param J initial guess for Jacobi matrix at x0
@@ -27,33 +26,80 @@ using Vector = Eigen::Matrix<T, N, 1>;
  * \param callback to be run in every iteration step
  */
 template <typename FuncType, typename JacType, typename Scalar,
-          int N=Dynamic, typename CB=void_cb>
-Vector<Scalar, N> broyd(FuncType&& F, Vector<Scalar, N> x, 
-                        JacType J, const Scalar tol, 
-                        const unsigned maxit=20, CB callback=nullptr)
-{
-    // calculate LU factorization
-    auto fac = J.lu();
-
-    unsigned k = 1;
-    Vector2d s = fac.solve(F(x));
-    x -= s;
-    auto sn = s.squaredNorm();
-    auto f = F(x);
-
-    while ((sn > tol*tol) && (k < 10)) {
-        J = J - f*s.transpose()/sn;
-        fac = J.lu();
-        s = fac.solve(f);
-        x -= s;
-        f = F(x); sn=s.squaredNorm();
-
-        if (callback != nullptr)
-            callback(k, x, f, s);
-        k+=1; 
+          int N = Eigen::Dynamic, typename CB = void_cb>
+Vector<Scalar, N> broyd(FuncType&& F, Vector<Scalar, N> x, JacType J,
+                        const Scalar tol, const unsigned maxit = 20,
+                        CB callback = nullptr) {
+  // Compute first quasi-Newton update
+  Vector<Scalar, N> s = -J.lu().solve(F(x));
+  // Compute $\Vx^{(1)}$
+  x += s;
+  auto f = F(x);
+  for (unsigned int k = 1; ((s.norm() > tol * x.norm()) && (k < maxit)); ++k) {
+    // Rank-1 update of Jacobian \eqref{eq:broydenqn}
+    J += f * s.transpose() / s.squaredNorm();
+    // Next quasi-Newton correction
+    s = -J.lu().solve(f);
+    // Update of iterate
+    x += s;
+    // Evaluate F at next iterate
+    f = F(x);
+    // Optional output 
+    if (callback != nullptr) {
+      callback(k, x, f, s);
     }
-    return x;
+  }
+  return x;
 }
 
-
+/* SAM_LISTING_BEGIN_1 */
+template <typename FuncType, typename JacType, typename Scalar = double,
+          int N = Eigen::Dynamic, typename CB = void_cb>
+Vector<Scalar, N> upbroyd(const FuncType &F, Vector<Scalar, N> x, JacType J,
+                          const Scalar tol, const unsigned maxit = 20,
+                          CB callback = nullptr) {
+  // Calculate LU factorization of initial Jacobian once
+  auto fac = J.lu();
+  // First quasi-Newton correction $\cob{\Delta\Vx^{(0)} := -\VJ_0^{-1}F(\Vx^{(0)})}$
+  Vector<Scalar, N> s = -fac.solve(F(x));
+  // Store the first quasi-Newton correction $\cob{\Delta\Vx^{(0)}}$
+  std::vector<Vector<Scalar, N>> dx{s};
+  // First update of iterates: $\cob{\Vx^{(1)} := \Vx^{(0)} + \Delta\Vx^{(0)}}$
+  x += s;
+  auto f = F(x); // Here $\cob{ = F(\Vx^{(1)}}$
+  // Empty sequence of simplified quasi-Newton corrections 
+  std::vector<Vector<Scalar, N>> dxs{};
+  // Store denominators $\cob{\N{\Vx^{(\ell)}}^{2}+(\Delta\Vx^{(\ell)})^{\top}\cop{\Delta\overline{\Vx}^{(\ell+1)}}}$
+  std::vector<Scalar> den{};
+  // callback once before we start the algorithm
+  callback(0, x, f, s);
+  for (unsigned int k = 1; ((s.norm() > tol * x.norm()) && (k < maxit)); ++k) {
+    // Compute $\cob{\VJ_{0}^{-1}F(\Vx^{(k)})}$
+    s = fac.solve(f);
+    // Compute next simplified quasi-Newton correction recursively 
+    Vector<Scalar, N> ss = s;
+    for (unsigned int l = 1; l < k; ++l) {
+      ss -= dxs[l-1] * (dx[l - 1].dot(ss)) / den[l - 1];
+    }
+    // Store next denominator
+    den.push_back(dx[k - 1].squaredNorm() + dx[k - 1].dot(ss));
+    // Store current simplified quasi-Newton correction
+    dxs.push_back(ss);
+    // Compute next quasi-Newton correction recursively 
+    for (unsigned int l = 0; l < k; ++l) {
+      s -= dxs[l] * (dx[l].dot(s)) / den[l];
+    }
+    s *= (-1.0);
+    dx.push_back(s);
+    // Compute next iterate
+    x += s;
+    // Evaluation F at next iterate
+    f = F(x);
+    if (callback != nullptr) { // NOLINT
+      callback(k, x, f, s);
+    }
+  }
+  return x;
+}
+/* SAM_LISTING_END_1 */
 #endif
--- LectureCodes/IterativeMethods/broyden/Eigen/main.cpp
+++ LectureCodes/IterativeMethods/broyden/Eigen/main.cpp
@@ -6,60 +6,46 @@
 /// Do not remove this header.
 //////////////////////////////////////////////////////////////////////////
 #include <Eigen/Dense>
-#include <vector>
 #include <iostream>
 #include <tuple>
 #include <utility>
-#include <figure/figure.hpp>
+#include <vector>
 
 #include "broyd.hpp"
 #include "upbroyd.hpp"
-#include "../../newton/Eigen/newton.hpp"
-#include "../../simpnewton/Eigen/simpnewton.hpp"
-
-using namespace Eigen;
 
 int main() {
-    auto F = [] (const Eigen::Vector2d& x) -> Vector2d {
-        return Vector2d(pow(x[0], 2)-pow(x[1], 4), 
-                x[0] - pow(x[1], 3));
-    };
-    auto DF = [] (const Eigen::Vector2d& x) -> Matrix2d {
-        Matrix2d res;
-        res <<  2.*x[0],    -4.*pow(x[1], 3), 
-                1      ,    -3.*pow(x[1], 2);
-        return res;
-    };
+  // The function whose zero we want to find 
+  auto F = [](const Eigen::Vector2d& x) -> Eigen::Vector2d {
+    return Eigen::Vector2d(pow(x[0], 2) - pow(x[1], 4), x[0] - pow(x[1], 3));
+  };
+  // Jacobian of F 
+  auto DF = [](const Eigen::Vector2d& x) -> Eigen::Matrix2d {
+    Eigen::Matrix2d res;
+    res << 2. * x[0], -4. * pow(x[1], 3), 1, -3. * pow(x[1], 2);
+    return res;
+  };
+  // this function is called in every iteration and prints the progress
+  auto print_progress_cb = [](unsigned k, Eigen::Vector2d x, Eigen::Vector2d f,
+                              Eigen::Vector2d s) {
+    std::cout << "Iteration " << k << ": |s| = " << s.norm()
+              << ", |f(x)| =  " << f.norm() << std::endl;
+  };
+  // Initial guess
+  Eigen::Vector2d x0(0.7, 0.7);
+  {
+    std::cout << "Matrix-based implementation of quasi-Newton method" << std::endl;
+    const Eigen::Vector2d x = broyd(F, x0, DF(x0), 0.000001, 20, print_progress_cb);
+    std::cout << "solution = " << x.transpose() << std::endl;
+  }
 
-    Vector2d x0(0.7, 0.7);
 
-    /*
-     * Broyden
-     */
-    {
-        // this function is called in every iteration and prints the progress
-        auto print_progress_cb = [] (unsigned k, Vector2d x, Vector2d f, Vector2d s) {
-            std::cout << "Iteration " << k << ": |s| " << s.norm() << " " << f.norm() << std::endl;
-        };
-        // run the algorithm (we ignore the result)
-        broyd(F, x0, DF(x0), 0.000001, 20, print_progress_cb);
-    }
+  {
+    // run the algorithm (we ignore the result)
+    std::cout << "Recursion-based implementation of quasi-Newton method" << std::endl;
+    const Eigen::Vector2d x = upbroyd(F, x0, DF(x0), 0.000001, 20, print_progress_cb);
+    std::cout << "solution = " << x.transpose() << std::endl;
+  }
 
-    /*
-     * Upbroyd
-     */
-    {
-        // this function is called in every iteration and prints the progress
-        auto print_progress_cb = [] (unsigned k, Vector2d x, Vector2d f, Vector2d s, Vector2d w, std::vector<double>& dxn) {
-            std::cout << "Iteration(UPD) " << std::scientific << k << ": |s| = " << s.norm()
-                  << ", |F(x)| = " << f.norm();
-            if (dxn.size() > 1)
-                std::cout << ", theta = " << std::fixed << w.norm()/std::sqrt(dxn[k-1]);
-            std::cout << std::endl;
-        };
-        // run the algorithm (we ignore the result)
-        upbroyd(F, x0, DF(x0), 0.000001, 20, print_progress_cb);
-    }
-
-    return 0;
+  return 0;
 }
