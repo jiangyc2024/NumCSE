d5f2277018798ac245f3f39daa9da348374939cb
Maximilian Herde <herdem@student.ethz.ch>
2021-03-30 17:35:06 +0200

    clang format

--- Assignments/PolishedCodes/NumericalQuadrature/ZerosLegendre/solution/tests.cpp
+++ Assignments/PolishedCodes/NumericalQuadrature/ZerosLegendre/solution/tests.cpp
@@ -10,32 +10,36 @@ TEST_SUITE("ZerosLegendre") {
     constexpr unsigned int n = 10;
     const Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(N, 0., 10.);
     Eigen::MatrixXd Lx_sol(N, n), Lx_stud(N, n), DLx_sol(N, n), DLx_stud(N, n);
-    
+
     legvals(x, Lx_sol, DLx_sol);
     legvals_TEST(x, Lx_stud, DLx_stud);
-    
+
     CHECK((Lx_sol - Lx_stud).norm() == doctest::Approx(0.).epsilon(1e-9));
     CHECK((DLx_sol - DLx_stud).norm() == doctest::Approx(0.).epsilon(1e-9));
   }
-  
-  TEST_CASE("double Pnx" * doctest::description("optional helper function") * doctest::skip()) {}
-  
-  TEST_CASE("Eigen::MatrixXd gaussPts" * doctest::description("gaussPts (8-10.d)")) {
-    
-    // This case will pass even though the function gives wrong output (in this case for $P_8$). It just checks if the function is implemented like the solution.
-    
+
+  TEST_CASE("double Pnx" * doctest::description("optional helper function") *
+            doctest::skip()) {}
+
+  TEST_CASE("Eigen::MatrixXd gaussPts" *
+            doctest::description("gaussPts (8-10.d)")) {
+    // This case will pass even though the function gives wrong output (in this
+    // case for $P_8$). It just checks if the function is implemented like the
+    // solution.
+
     constexpr unsigned int n = 8;
     Eigen::MatrixXd sol = gaussPts(n, 1e-10, 1e-12);
     Eigen::MatrixXd stud = gaussPts_TEST(n, 1e-10, 1e-12);
-    
+
     CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-9));
   }
-  
-  TEST_CASE("Eigen::MatrixXd gaussPts_regulaFalsi" * doctest::description("gaussPts with regula falsi (8-10.f)")) {
+
+  TEST_CASE("Eigen::MatrixXd gaussPts_regulaFalsi" *
+            doctest::description("gaussPts with regula falsi (8-10.f)")) {
     constexpr unsigned int n = 8;
     Eigen::MatrixXd sol = gaussPts_regulaFalsi(n, 1e-10, 1e-12);
     Eigen::MatrixXd stud = gaussPts_regulaFalsi_TEST(n, 1e-10, 1e-12);
-    
+
     CHECK((sol - stud).norm() == doctest::Approx(0.).epsilon(1e-9));
   }
 }
--- Assignments/PolishedCodes/NumericalQuadrature/ZerosLegendre/template/legendre.hpp
+++ Assignments/PolishedCodes/NumericalQuadrature/ZerosLegendre/template/legendre.hpp
@@ -1,21 +1,22 @@
 #ifndef LEGENDRE_HPP
 #define LEGENDRE_HPP
 
-#include <cassert>
 #include <Eigen/Dense>
+#include <cassert>
 
 //! @brief Evaluate the Legendre polynomials and its derivatives in vector $x$
 //! using the 3-term recursion formulae.
 //! @param[in] evaluation points
 //! @param[out] matrices $Lx$ and $DLx$.
 /* SAM_LISTING_BEGIN_0 */
-void legvals(const Eigen::VectorXd& x, Eigen::MatrixXd& Lx, Eigen::MatrixXd& DLx) {
+void legvals(const Eigen::VectorXd& x, Eigen::MatrixXd& Lx,
+             Eigen::MatrixXd& DLx) {
   const unsigned int n = Lx.cols() - 1;
   const unsigned int N = x.size();
-  
-  // TODO: (8-10.c) Fill matrices Lx and DLx with values of the legendre polynomials at x, i.e. $\{P_k(x_j)\}_{jk}$ and $\{P'_k(x_j)\}_{jk}$
-  // START
-  
+
+  // TODO: (8-10.c) Fill matrices Lx and DLx with values of the legendre
+  // polynomials at x, i.e. $\{P_k(x_j)\}_{jk}$ and $\{P'_k(x_j)\}_{jk}$ START
+
   // END
 }
 /* SAM_LISTING_END_0 */
@@ -24,10 +25,10 @@ void legvals(const Eigen::VectorXd& x, Eigen::MatrixXd& Lx, Eigen::MatrixXd& DLx
 /* SAM_LISTING_BEGIN_1 */
 double Pnx(const double x, const int n) {
   Eigen::VectorXd Px(n + 1);
-  
-  // TODO: (8-10.d, optional) Evaluate $P_n(x)$ for a scalar $x$ and integer $n$.
-  // START
-  
+
+  // TODO: (8-10.d, optional) Evaluate $P_n(x)$ for a scalar $x$ and integer
+  // $n$. START
+
   // END
   return Px(n);
 }
@@ -36,12 +37,12 @@ double Pnx(const double x, const int n) {
 //! @brief Find the Gauss points using the secant method without regula falsi.
 /* SAM_LISTING_BEGIN_2 */
 Eigen::MatrixXd gaussPts(const unsigned int n, const double rtol = 1e-10,
-                  const double atol = 1e-12) {
+                         const double atol = 1e-12) {
   Eigen::MatrixXd zeros = Eigen::MatrixXd::Zero(n, n);
-  
-  // TODO: (8-10.d) Find the zeros of the legendre polynomials using the secant method.
-  // START
-  
+
+  // TODO: (8-10.d) Find the zeros of the legendre polynomials using the secant
+  // method. START
+
   // END
   return zeros;
 }
@@ -49,12 +50,14 @@ Eigen::MatrixXd gaussPts(const unsigned int n, const double rtol = 1e-10,
 
 //! @brief Find the Gauss points using the secant method with regula falsi.
 /* SAM_LISTING_BEGIN_3 */
-Eigen::MatrixXd gaussPts_regulaFalsi(const unsigned int n, const double rtol = 1e-10, const double atol = 1e-12) {
+Eigen::MatrixXd gaussPts_regulaFalsi(const unsigned int n,
+                                     const double rtol = 1e-10,
+                                     const double atol = 1e-12) {
   Eigen::MatrixXd zeros = Eigen::MatrixXd::Zero(n, n);
-  
-  // TODO: (8-10.f) Find the zeros of the legendre polynomials using the secant method with regula falsi.
-  // START
-  
+
+  // TODO: (8-10.f) Find the zeros of the legendre polynomials using the secant
+  // method with regula falsi. START
+
   // END
   return zeros;
 }
