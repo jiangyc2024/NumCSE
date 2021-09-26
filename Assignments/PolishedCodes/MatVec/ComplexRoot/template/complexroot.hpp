32838a7ce6083394a1658565ab1814b4ff170e80
Pascal Mueller <mueller.pascal@hotmail.com>
2021-09-26 18:19:31 +0200

    Polished problem 1-11 ComplexRoot

--- Assignments/PolishedCodes/MatVec/ComplexRoot/template/complexroot.hpp
+++ Assignments/PolishedCodes/MatVec/ComplexRoot/template/complexroot.hpp
@@ -1,29 +1,30 @@
-//// 
+////
 //// Copyright (C) 2016 SAM (D-MATH) @ ETH Zurich
-//// Author(s): lfilippo <filippo.leonardi@sam.math.ethz.ch> 
+//// Author(s): lfilippo <filippo.leonardi@sam.math.ethz.ch>
 //// Contributors: tille, jgacon, dcasati
 //// This file is part of the NumCSE repository.
 ////
-#include <iostream>
-#include <complex>
 #include <cmath>
+#include <complex>
+#include <iostream>
 
-//! \brief Compute complex root
-//! \param[in] w complex number with non negative imaginary parts
-//! \return the square root of w with non negative real and imaginary parts
+// \brief Compute complex root
+// @param w complex number with non negative imaginary parts
+// @param the square root of w with non negative real and imaginary parts
 
 /* SAM_LISTING_BEGIN_0 */
-std::complex<double> myroot( std::complex<double> w ) {
-    double x,y;
-    double u = w.real();
-    double v = w.imag();
-    
-    // TO DO: (2-11.c) Compute the square root of w avoiding cancellation,
-    // use only real arithmetic.
-    // START
+std::complex<double> myroot(std::complex<double> w) {
+  double x, y;
+  double u = w.real();
+  double v = w.imag();
 
-    // END 
-    
-    return std::complex<double> (x,y);
+  // TODO: (1-11.c) Compute the square root of w avoiding cancellation,
+  // use only real arithmetic.
+
+  // START
+
+  // END
+
+  return std::complex<double>(x, y);
 }
 /* SAM_LISTING_END_0 */
