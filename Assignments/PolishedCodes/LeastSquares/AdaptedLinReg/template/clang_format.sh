93ca489eb90536b2b68885ad4309d6fc5a1c07b6
Pascal Mueller <mueller.pascal@hotmail.com>
2021-10-04 16:18:15 +0200

    removed clang format script from problem 3-1

--- Assignments/PolishedCodes/LeastSquares/AdaptedLinReg/template/clang_format.sh
+++
@@ -1,8 +0,0 @@
-# This script will format every .cpp or .hpp file,
-# also in subdirectories of the directory from which it is called.
-
-for cpp_file in $(find . -name '*.cpp' -o -name '*.hpp')
-do
-	# does inplace formatting using Google style guidelines
-	clang-format -i --verbose --style=Google $cpp_file
-done
