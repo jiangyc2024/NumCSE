c1589b36d28aceff19357f269f88bbad79197510
pamuelle <pamuelle@student.ethz.ch>
2021-10-21 12:32:15 +0200

    Polished Problem 4-2 and 4-3.

---
+++ Assignments/PolishedCodes/Filtering/FFTLSQ/template/fftlsq.hpp
@@ -0,0 +1,112 @@
+#ifndef FFTLSQ_HPP
+#define FFTLSQ_HPP
+
+#include <math.h>
+
+#include "matplotlibcpp.h"
+#include <Eigen/Dense>
+#include <iomanip>
+#include <unsupported/Eigen/FFT>
+
+namespace plt = matplotlibcpp;
+
+/*!
+ * @brief eval_p Given polynomial coefficients, return value of polynomial
+ * at $n$ equidistant points.
+ * @param[in] c Coefficient vector of trigonometrix polynomial.
+ * @param[in] n Number of equidistant points at which to evaluate.
+ * @return Value of polynomial $p$ at $2\pi i / n$.
+ */
+Eigen::VectorXd eval_p(const Eigen::VectorXd &c, const unsigned int n) {
+  // Degree of polynomial
+  const unsigned int m = c.size();
+
+  Eigen::VectorXd ret(n);
+  // Loop over all points
+  for (unsigned int j = 0; j < n; ++j) {
+    double r = 0;
+    // Loop over all coefficients
+    for (unsigned int k = 0; k < m; ++k) {
+      r += c(k) * std::cos(2.0 * M_PI * k * j / n);
+    }
+    ret(j) = r;
+  }
+  
+  return ret;
+}
+
+/*!
+ * @brief testNormEqMatrix Create the matrix $A^TA$ in two different ways and
+ * make sure they are approximately equal.
+ * @param[in] n number of different measurements
+ * @param[in] m degree of the trigonometric polynomial to
+ * be fitted
+ * @return Boolean indicating if test passed or failed.
+ */
+/* SAM_LISTING_BEGIN_0 */
+bool testNormEqMatrix(unsigned int n, unsigned int m) {
+  // TODO: (4-2.c) Test if the two definitions, given in (4.2.4) and
+  // (4.2.7), of $A^T*A$ are equal.
+
+  // START
+  
+  // END
+  
+  return false;
+}
+/* SAM_LISTING_END_0 */
+
+/*!
+ * @brief find_c Find Best trigonometric polynomial passing trough distances
+ * $\mathbf{d}$. Using Least Squares formulation and assuming best fitting
+ * polynomial has degree $m$.
+ * @param[in] d Vector of size $n$ distances at angle $2*\pi*i/n$.
+ * @param[in] m Degree of the trigonometric polynomial $p$.
+ * @return The coefficients of the trigonometric polynomial.
+ */
+/* SAM_LISTING_BEGIN_1 */
+Eigen::VectorXd find_c(const Eigen::VectorXd &d, unsigned int m) {
+  unsigned int n = d.size();
+
+  // We will use a real to complex, discrete Fourier transform.
+  Eigen::FFT<double> fft;
+  Eigen::VectorXd rhs;
+
+  // TODO: (4-2.d) find the coefficients
+  // START
+
+  // END
+ 
+  return rhs;
+}
+/* SAM_LISTING_END_1 */
+
+/*!
+ * @brief using implementation of find_c compute coefficients c_k and p* for
+ * m=1,2,3 using matplotlibcpp's plt() create a plot showing the ellipse in the
+ * same plot insert the curves descriped by p* of degrees m=1,2,3
+ */
+/* SAM_LISTING_BEGIN_2 */
+void fitEllipse() {
+  unsigned int n = 10;
+  unsigned int m = 3;
+
+  // Test points
+  const unsigned int npoints = 10;
+  Eigen::VectorXd d(npoints);
+  d << 0.987214, 1.03579, 0.997689, 0.917471, 1.00474, 0.92209, 1.03517,
+      1.08863, 0.904992, 0.956089;
+
+  plt::figure();
+
+  // TODO: (4-2.e) Tabulate the coefficients of find\_c for all $m=1,2,3$
+  // plot the ellipse and also the curves of the trigonimetric polynomials
+  // START
+
+  // END
+
+  plt::savefig("cx_out/orbit.png");
+}
+/* SAM_LISTING_END_2 */
+
+#endif
---
+++ Assignments/PolishedCodes/Filtering/PolyDiv/template/polydiv.hpp
@@ -0,0 +1,98 @@
+#include <Eigen/Dense>
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <unsupported/Eigen/FFT>
+
+/* @brief Polynomial multiplication -- naive implementation
+ * @param[in] u Vector of coefficients of polynomial $u$
+ * @param[in] v Vector of coefficients of polynomial $v$
+ * @return uv Vector of coefficients of polynomial $uv = u*v$
+ */
+/* SAM_LISTING_BEGIN_0 */
+Eigen::VectorXd polyMult_naive(const Eigen::VectorXd &u,
+                               const Eigen::VectorXd &v) {
+  // Fetch degrees of input polynomials
+  int degu = u.size() - 1;
+  int degv = v.size() - 1;
+
+  // Object for product polynomial p = uv
+  int degp = degu + degv;
+
+  Eigen::VectorXd uv(degp + 1);
+
+  // TODO: (4-3.a) Multiply polynomials $u$ and $v$ naively.
+  // START
+  
+  // END
+
+  return uv;
+}
+/* SAM_LISTING_END_0 */
+
+/* @brief Polynomial multiplication -- efficient implementation
+ * @param[in] u Vector of coefficients of polynomial $u$
+ * @param[in] v Vector of coefficients of polynomial $v$
+ * @return uv Vector of coefficients of polynomial $uv = u*v$
+ */
+/* SAM_LISTING_BEGIN_1 */
+Eigen::VectorXd polyMult_fast(const Eigen::VectorXd &u,
+                              const Eigen::VectorXd &v) {
+  // Initialization
+  int m = u.size();
+  int n = v.size();
+
+  Eigen::VectorXd u_tmp(m + n - 1);
+  u_tmp.head(m) = u;
+  u_tmp.tail(n - 1).setZero();
+
+  Eigen::VectorXd v_tmp(m + n - 1);
+  v_tmp.head(n) = v;
+  v_tmp.tail(m - 1).setZero();
+
+  // Eigen::VectorXd u_tmp = u;
+  // u_tmp.conservativeResizeLike(Eigen::VectorXd::Zero(u.size() + n - 1));
+  // Eigen::VectorXd v_tmp = v;
+  // v_tmp.conservativeResizeLike(Eigen::VectorXd::Zero(v.size() + m - 1));
+
+  Eigen::VectorXd uv;
+
+  // TODO: (4-3.b) Multiply polynomials $u$ and $v$ efficiently.
+  // START
+  
+  // END
+
+  return uv;
+}
+/* SAM_LISTING_END_1 */
+
+/* @brief Polynomial division -- efficient implementation
+ * @param[in] uv Vector of coefficients of polynomial $uv$
+ * @param[in] u Vector of coefficients of polynomial $u$
+ * @return uv Vector of coefficients of polynomial $v$
+ * from $uv = u*v$ (division without remainder)
+ */
+/* SAM_LISTING_BEGIN_2 */
+Eigen::VectorXd polyDiv(const Eigen::VectorXd &uv, const Eigen::VectorXd &u) {
+  // Initialization
+  int mn = uv.size();
+  int m = u.size();
+  // need well behaved input
+  if (mn < m) {
+    std::cerr << "uv can't be divided by u\n";
+    return Eigen::VectorXd(0);
+  }
+  int dim = mn;
+  int n = mn - m + 1;
+
+  Eigen::VectorXd v;
+
+  // TODO: (4-3.e) Divide polynomials $uv$ and $u$ efficiently.
+  // START
+
+  // END
+
+  return v;
+}
+/* SAM_LISTING_END_2 */
