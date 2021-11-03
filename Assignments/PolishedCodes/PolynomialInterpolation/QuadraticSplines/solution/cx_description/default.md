dc09504ae11b31188a15f5ea0fec8e845719b635
Langwen Huang <huanglangwen@outlook.com>
2021-11-03 22:46:13 +0100

    Polish 5-6 Quadratic Splines

---
+++ Assignments/PolishedCodes/PolynomialInterpolation/QuadraticSplines/solution/cx_description/default.md
@@ -0,0 +1,10 @@
+# Problem 5-6: Quadratic Splines
+
+> For the task description of this exercise, please refer to [NCSE21_Problems.pdf](
+https://www.sam.math.ethz.ch/~grsam/NCSE21/HOMEWORK/NCSE21_Problems.pdf). 
+
+> Open `quadsplines.hpp` and fill in the missing code in between the delimiters `// START` and `// END` according to the instructions preceded by `// TO DO:`.
+
+> To view plots, click the "Files" button on the toolbar at the bottom of the page.
+
+> You may hand in any handwritten solutions directly to your teaching assistant. Alternatively, you may write them in the markdown document `written_solution.md`.
---
+++ Assignments/PolishedCodes/PolynomialInterpolation/QuadraticSplines/template/main.cpp
@@ -0,0 +1,38 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+#include "quadsplines.hpp"
+
+using namespace Eigen;
+
+int main(void) {
+  VectorXd t(3);
+  t << .1, .2, .5;
+  std::pair<VectorXd, VectorXd> p = increments(t);
+  std::cout << p.first << std::endl << p.second << std::endl;
+
+  VectorXd y(4);
+  y << 1, 2, 1, 2;
+
+  VectorXd c = compute_c(t, y);
+  std::cout << "c = " << c << std::endl << std::endl;
+
+  VectorXd d = compute_d(c, t);
+  std::cout << "d = " << d << std::endl << std::endl;
+
+  Vector3d x(0, .4, .9);
+
+  VectorXd fval = quadspline(t, y, x);
+  std::cout << "fval = " << fval << std::endl << std::endl;
+  plotquadspline("splines");
+
+  std::vector<double> Err = qsp_error(8);
+  std::cout << "n \t"
+            << "Error values: \n";
+  std::cout << "------------------------ \n";
+  for (unsigned int i = 0; i < Err.size(); ++i) {
+    std::cout << std::pow(2, i + 1) << "\t" << Err[i] << std::endl;
+  }
+
+  return 0;
+}
