337f97f91aa57cf01a56b72ca2887dc9012a56cb
ralfh <ralfh@ethz.ch>
2021-11-03 07:24:59 +0100

    Reformatting of main.cpp in Filterin/dftbasics

--- LectureCodes/Filtering/dftbasics/Eigen/main.cpp
+++ LectureCodes/Filtering/dftbasics/Eigen/main.cpp
@@ -1,24 +1,27 @@
-#include <complex>
-#include <iostream>
+/* Demonstraction code for course Numerical Methods  for CSE, ETH Zurich
+   FFT in Eigen
+   @author Ralf Hiptmair
+   @date August 2020
+*/
+
 #include <Eigen/Dense>
 #include <unsupported/Eigen/FFT>
-
-using namespace std;
-using namespace Eigen;
+#include <complex>
+#include <iostream>
 
 /* SAM_LISTING_BEGIN_0 */
-int main() {
-  using Comp = complex<double>;
-  const VectorXcd::Index n = 5;
-  VectorXcd y(n),c(n),x(n);
-  y << Comp(1,0),Comp(2,1),Comp(3,2),Comp(4,3),Comp(5,4);
-  FFT<double> fft; // DFT transform object
-  c = fft.fwd(y);  // DTF of y, see \cref{def:DFT} 
-  x = fft.inv(c);  // inverse DFT of c, see \eqref{eq:invdft}
+int main(int /*argc*/, char ** /*argv*/) {
+  using Comp = std::complex<double>;
+  const Eigen::VectorXcd::Index n = 5;
+  Eigen::VectorXcd y(n), c(n), x(n);
+  y << Comp(1, 0), Comp(2, 1), Comp(3, 2), Comp(4, 3), Comp(5, 4);
+  Eigen::FFT<double> fft;  // DFT transform object
+  c = fft.fwd(y);          // DTF of y, see \cref{def:DFT}
+  x = fft.inv(c);          // inverse DFT of c, see \eqref{eq:invdft}
 
-  cout << "y = " << y.transpose() << endl
-       << "c = " << c.transpose() << endl
-       << "x = " << x.transpose() << endl;
+  std::cout << "y = " << y.transpose() << std::endl
+            << "c = " << c.transpose() << std::endl
+            << "x = " << x.transpose() << std::endl;
   return 0;
 }
 /* SAM_LISTING_END_0 */
