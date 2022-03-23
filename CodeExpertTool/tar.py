bea1f6f6dd69accadfe845f2f6021143ed4fcf29
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2022-03-23 12:35:16 +0100

    added documentation

--- CodeExpertTool/tar.py
+++ CodeExpertTool/tar.py
@@ -6,4 +6,5 @@ source_dir = sys.argv[ 1 ]
 output_filename = f"{ os.path.basename( source_dir )}.tar"
 
 with tarfile.open( output_filename, "w:gz" ) as tar:
+    
     tar.add( source_dir, arcname = os.path.sep )
