9f03e5300666c59b7f31456155f6460cfc1fdc79
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2022-05-12 15:03:08 +0200

    fixed missing cx_out directory

--- CodeExpert/nmcse/run.sh
+++ CodeExpert/nmcse/run.sh
@@ -2,6 +2,9 @@
 
 parent_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)"
 
+# necessary for the output files
+mkdir -p "$parent_dir/projectfiles/cx_out"
+
 # uncomment the /bin/bash in the end for debugging the container (this will skip the ACTION)
 # increasing MAXFILESIZE was necessary for the test action
-docker run -it -e ACTION=test -e MAXFILESIZE=100000 -v "$parent_dir/projectfiles:/var/lib/cxrun/projectfiles" cx-nmcse #/bin/bash
+docker run -it -e ACTION=run -e MAXFILESIZE=100000 -v "$parent_dir/projectfiles:/var/lib/cxrun/projectfiles" cx-nmcse #/bin/bash
