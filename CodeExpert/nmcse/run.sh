206ff7aec4ac16d813f6062f3e4b008adc5fd35d
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2022-05-12 15:22:26 +0200

    added option and documentation

--- CodeExpert/nmcse/run.sh
+++ CodeExpert/nmcse/run.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
+# find absolute path of the directory of this script
 parent_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)"
 
-# necessary for the output files
+# if no action is specified, use the "run" action
+cx_action=${1-"run"}
+
+# make an output directory, for the output files (plots, etc.)
 mkdir -p "$parent_dir/projectfiles/cx_out"
 
 # uncomment the /bin/bash in the end for debugging the container (this will skip the ACTION)
-# increasing MAXFILESIZE was necessary for the test action
-docker run -it -e ACTION=run -e MAXFILESIZE=100000 -v "$parent_dir/projectfiles:/var/lib/cxrun/projectfiles" cx-nmcse #/bin/bash
+# increasing MAXFILESIZE was necessary for the test action, see https://docs.expert.ethz.ch/lecturers/#environment-variables
+docker run -it -e ACTION=$cx_action -e MAXFILESIZE=100000 -v "$parent_dir/projectfiles:/var/lib/cxrun/projectfiles" cx-nmcse #/bin/bash
