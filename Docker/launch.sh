89c63e9fa269ee2a6f4172329efa4371d39a77da
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2022-07-15 13:16:47 +0200

    first successful compilation of entire repository

--- Docker/launch.sh
+++ Docker/launch.sh
@@ -3,8 +3,12 @@
 # find absolute path of the directory of this script
 parent_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)"
 
-#path to the NumCSE repository
-repo_root="$parent_dir/../.."
+# path to the NumCSE repository
+repo_root="$parent_dir/.."
 
-docker build -t nmcse-dev .
-docker run -it -v "$repo_root:/numcse" nmcse-dev /bin/bash
+# build base image
+docker build -t nmcse-dev-base base
+
+# build 'last-mile' top image locally
+cd "$repo_root" && cp "$parent_dir/top/Dockerfile" . && docker build -t nmcse-dev-top . && rm Dockerfile
+docker run -it -v "$repo_root:/numcse" nmcse-dev-top /bin/bash
