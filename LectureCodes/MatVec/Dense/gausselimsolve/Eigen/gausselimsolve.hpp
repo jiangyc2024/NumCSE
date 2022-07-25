3cdfc6b7fda003bc676c6016767bc7e962ae35d1
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2022-07-25 14:08:48 +0200

    code tidy test

--- LectureCodes/MatVec/Dense/gausselimsolve/Eigen/gausselimsolve.hpp
+++ LectureCodes/MatVec/Dense/gausselimsolve/Eigen/gausselimsolve.hpp
@@ -15,6 +15,7 @@ namespace gausselimsolve {
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using Eigen::Index;
 
 inline
 /* SAM_LISTING_BEGIN_0 */
@@ -23,21 +24,21 @@ inline
 //! The result is returned in \texttt{x}
 void gausselimsolve(const MatrixXd &A, const VectorXd& b,
 		    VectorXd& x) {
-  int n = A.rows();
+  Index n = A.rows();
   MatrixXd Ab(n,n+1); // Augmented matrix \Blue{$[\VA,\Vb]$}
   Ab << A, b; //\Label[line]{cppgse:1}
   // \com{Forward elimination} (\textit{cf.} step \ding{192} in Ex.~\ref{ex:GE})
-  for(int i = 0; i < n-1; ++i) {
+  for(Index i = 0; i < n-1; ++i) {
     double pivot = Ab(i,i);
-    for(int k = i+1; k < n; ++k) {
+    for(Index k = i+1; k < n; ++k) {
       double fac = Ab(k, i) / pivot; // the multiplier \Label[line]{cppgse:fac}
       Ab.block(k,i+1,1,n-i)-= fac * Ab.block(i,i+1,1,n-i); //\Label[line]{cppgse:vec}
     }
   }
   // {\Hyperlink{RUECKSUBST}{\com{Back substitution}}} (\textit{cf.} step \ding{193} in Ex.~\ref{ex:GE})
   Ab(n-1,n) = Ab(n-1,n) / Ab(n-1,n-1);
-  for(int i = n-2; i >= 0; --i) {
-    for(int l = i+1; l < n; ++l) {
+  for(Index i = n-2; i >= 0; --i) {
+    for(Index l = i+1; l < n; ++l) {
       Ab(i,n) -= Ab(l,n)*Ab(i,l);
     }
     Ab(i,n) /= Ab(i,i);
