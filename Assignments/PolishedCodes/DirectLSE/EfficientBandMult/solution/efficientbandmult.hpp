843c12ef32bfe8339b02a33b14aed8c48e3cef7d
Maximilian Herde <herdem@student.ethz.ch>
2022-09-28 13:18:51 +0200

    update 2-6

--- Assignments/PolishedCodes/DirectLSE/EfficientBandMult/solution/efficientbandmult.hpp
+++ Assignments/PolishedCodes/DirectLSE/EfficientBandMult/solution/efficientbandmult.hpp
@@ -7,36 +7,36 @@
 /**
  * @brief Compute $y = A*x$ with A banded matrix with diagonal structure
  *
- * @tparam Vector random-access container, must provide size(), operator(),
- * resize()
  * @param a An $(n-1)$-dimensional vector for the upper diagonal
  * @param b An $(n-2)$-dimensional vector for the second lower diagonal
  * @param x An $n$-dimensional vector for $Ax = y$
- * @param y The $n$-dimensional vector $y = Ax$
+ * @return The $n$-dimensional vector $y = Ax$
  */
 /* SAM_LISTING_BEGIN_0 */
-template <class Vector>
-void multAx(const Vector& a, const Vector& b, const Vector& x, Vector& y) {
+Eigen::VectorXd multAx(Eigen::VectorXd& a, const Eigen::VectorXd& b,
+                       const Eigen::VectorXd& x) {
   const unsigned int n = x.size();
+  Eigen::VectorXd y = Eigen::VectorXd::Zero(n);
   if (a.size() < n - 1 || b.size() < n - 2) {
     std::cerr << "Error: size mismatch!" << std::endl;
-    return;
+    return y;
   }
-  y.resize(n);
 
   // TODO: (2-6.a) Multiply A*x and exploit the diagonal structure of the
-  // matrix. START Handle first two rows
+  // matrix.
+  // START
+  // Handle first two rows
   if (n > 1) {
     y(0) = 2 * x(0) + a(0) * x(1);
   } else {
     y(0) = 2 * x(0);
-    return;
+    return y;
   }
   if (n > 2) {
     y(1) = 2 * x(1) + a(1) * x(2);
   } else {
     y(1) = 2 * x(1);
-    return;
+    return y;
   }
 
   // Row if $n > 3$ and without last row
@@ -47,6 +47,7 @@ void multAx(const Vector& a, const Vector& b, const Vector& x, Vector& y) {
   // Last row special case
   if (n > 2) y(n - 1) = 2 * x(n - 1) + b(n - 3) * x(n - 3);
   // END
+  return y;
 }
 /* SAM_LISTING_END_0 */
 
@@ -54,22 +55,20 @@ void multAx(const Vector& a, const Vector& b, const Vector& x, Vector& y) {
  * @brief Solve $r = A*x$ with $A$ banded matrix with upper triangular sparse
  * structure
  *
- * @tparam Vector random-access container, must provide size(), operator(),
- * resize()
  * @param a An $(n-1)$-dimensional vector for the upper diagonal
  * @param r An $n$-dimensional vector for $Ax = r$
- * @param x The $n$-dimensional vector from $Ax = r$
+ * @return The $n$-dimensional vector from $Ax = r$
  */
 /* SAM_LISTING_BEGIN_1 */
-template <class Vector>
-void solvelseAupper(const Vector& a, const Vector& r, Vector& x) {
+Eigen::VectorXd solvelseAupper(const Eigen::VectorXd& a,
+                               const Eigen::VectorXd& r) {
   // Set up dimensions
   const unsigned int n = r.size();
+  Eigen::VectorXd x = Eigen::VectorXd::Zero(n);
   if (a.size() < n - 1) {
     std::cerr << "Error: size mismatch!" << std::endl;
-    return;
+    return x;
   }
-  x.resize(n);
 
   // TODO: (2-6.c) Solve the LSE A*x = r for x by exploiting the diagonal
   // structure of A and the fact that b is zero.
@@ -80,6 +79,7 @@ void solvelseAupper(const Vector& a, const Vector& r, Vector& x) {
     x(j) = 0.5 * (r(j) - a(j) * x(j + 1));
   }
   // END
+  return x;
 }
 /* SAM_LISTING_END_1 */
 
@@ -87,27 +87,26 @@ void solvelseAupper(const Vector& a, const Vector& r, Vector& x) {
  * @brief Solve $r = A*x$ with $A$ banded matrix using Gaussian elimination (no
  * pivot)
  *
- * @tparam Vector random-access container, must provide size(), operator(),
- * resize()
  * @param a An $(n-1)$-dimensional vector for the upper diagonal
  * @param b An $(n-2)$-dimensional vector for the second lower diagonal
  * @param r An $n$-dimensional vector for $Ax = r$
- * @param x The $n$-dimensional vector from $Ax = r$
+ * @return The $n$-dimensional vector from $Ax = r$
  */
 /* SAM_LISTING_BEGIN_2 */
-template <class Vector>
-void solvelseA(const Vector& a, const Vector& b, const Vector& r, Vector& x) {
+Eigen::VectorXd solvelseA(const Eigen::VectorXd& a, const Eigen::VectorXd& b,
+                          const Eigen::VectorXd& r) {
   // Set up dimensions
   const unsigned int n = r.size();
+  Eigen::VectorXd x = Eigen::VectorXd::Zero(n);
   if (a.size() < n - 1 || b.size() < n - 2) {
     std::cerr << "Error: size mismatch!" << std::endl;
-    return;
+    return x;
   }
 
   // TODO: (2-6.d) Compute the solution to A*x=r by Gaussian elimination and
   // without the help of Eigen's solvers.
   // START
-  Vector c, d, y;
+  Eigen::VectorXd c, d, y;
   c.resize(n - 1);
   d.resize(n);
   for (unsigned int i = 0; i < n - 1; ++i) {
@@ -133,36 +132,35 @@ void solvelseA(const Vector& a, const Vector& b, const Vector& r, Vector& x) {
     x(i) = (y(i) - a(i) * x(i + 1)) / d(i);
   }
   // END
+  return x;
 }
 /* SAM_LISTING_END_2 */
 
 /**
  * @brief Solve $r = A*x$ with $A$ banded matrix using Eigen::SparseLU
  *
- * @tparam Vector random-access container, must provide size(), operator(),
- * resize()
  * @param a An $(n-1)$-dimensional vector for the upper diagonal
  * @param b An $(n-2)$-dimensional vector for the second lower diagonal
  * @param r An $n$-dimensional vector for $Ax = r$
- * @param x The $n$-dimensional vector from $Ax = r$
+ * @return The $n$-dimensional vector from $Ax = r$
  */
 /* SAM_LISTING_BEGIN_3 */
-template <class Vector>
-void solvelseAEigen(const Vector& a, const Vector& b, const Vector& r,
-                    Vector& x) {
+Eigen::VectorXd solvelseASparse(const Eigen::VectorXd& a,
+                                const Eigen::VectorXd& b,
+                                const Eigen::VectorXd& r) {
   // Set up dimensions
-  typedef typename Vector::Scalar Scalar;
   const unsigned int n = r.size();
+  Eigen::VectorXd x = Eigen::VectorXd::Zero(n);
   if (a.size() < n - 1 || b.size() < n - 2) {
     std::cerr << "Error: size mismatch!" << std::endl;
-    return;
+    return x;
   }
 
   // TODO: (2-6.f) Solve A*x=r using Eigen's sparse solver.
   // START
   // Fill in matrix:
   // We reserve three nonzero entries per row for Gaussian fill-in
-  Eigen::SparseMatrix<Scalar> A(n, n);
+  Eigen::SparseMatrix<double> A(n, n);
   A.reserve(3);
   for (unsigned int i = 0; i < n; ++i) {
     A.insert(i, i) = 2;
@@ -172,11 +170,12 @@ void solvelseAEigen(const Vector& a, const Vector& b, const Vector& r,
   A.makeCompressed();
 
   // Call SparseLU
-  Eigen::SparseLU<Eigen::SparseMatrix<Scalar>> solver;
+  Eigen::SparseLU<Eigen::SparseMatrix<double>> solver;
   solver.analyzePattern(A);
   solver.compute(A);
   x = solver.solve(r);
   // END
+  return x;
 }
 /* SAM_LISTING_END_3 */
 
