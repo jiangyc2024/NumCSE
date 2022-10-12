ff648a2853cf5c4d4e5cb19cae296d3a20cbb1d4
Maximilian Herde <herdem@student.ethz.ch>
2022-10-12 10:29:41 +0200

    enforce c++17

--- Testing/scripts/compile.sh
+++ Testing/scripts/compile.sh
@@ -5,7 +5,7 @@ echo "Compiling ..."
 mkdir -p bin
 
 # Returns true if the compilation passes (even with warnings).
-if g++ main.cpp -fdiagnostics-color=always -std=c++11 \
+if g++ main.cpp -fdiagnostics-color=always -std=c++17 \
   -I/usr/local/include/python3.7m \
   -I/usr/local/lib/python3.7/site-packages/numpy/core/include \
   -I/usr/include/eigen3/ \
