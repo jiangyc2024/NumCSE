f6a30b293e171da5e62731dde97df0cf6eb9ab28
ppanchal <piyush.panchal@sam.math.ethz.ch>
2022-10-20 13:06:16 +0200

    Fixed efficientbandmult solution

--- Assignments/PolishedCodes/DirectLSE/EfficientBandMult/solution/efficientbandmult.hpp
+++ Assignments/PolishedCodes/DirectLSE/EfficientBandMult/solution/efficientbandmult.hpp
@@ -123,9 +123,9 @@ void solvelseA(const Vector& a, const Vector& b, const Vector& r, Vector& x) {
   // vector $c$
   for (unsigned int i = 0; i < n - 2; ++i) {
     c(i + 1) = -b(i) / d(i) * a(i);
-    d(i + 1) -= c(i) / d(i) * a(i);
-    y(i + 1) -= c(i) / d(i) * y(i);
     y(i + 2) -= b(i) / d(i) * y(i);
+    d(i + 2) -= c(i+1) / d(i+1) * a(i+1);
+    y(i + 2) -= c(i+1)/ d(i+1) * y(i+1);
   }
 
   x(n - 1) = y(n - 1) / d(n - 1);
