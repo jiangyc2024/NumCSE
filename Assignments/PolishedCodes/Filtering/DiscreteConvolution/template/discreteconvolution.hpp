1ed1efe461009b724ceb9f2d80d294e9c2112de0
Maximilian Herde <herdem@student.ethz.ch>
2022-11-03 11:21:19 +0100

    4-5 update

--- Assignments/PolishedCodes/Filtering/DiscreteConvolution/template/discreteconvolution.hpp
+++ Assignments/PolishedCodes/Filtering/DiscreteConvolution/template/discreteconvolution.hpp
@@ -1,13 +1,19 @@
+#ifndef DISCRETECONVOLUTION_HPP
+#define DISCRETECONVOLUTION_HPP
 
 #include <Eigen/Dense>
+#include <cassert>
 #include <cmath>
-#include <iostream>
 #include <unsupported/Eigen/FFT>
 
-using namespace Eigen;
-
-// For reference, a ``naive'' two-loop implementation
-// of discrete periodic convolution.
+/**
+ * @brief A ``naive'' two-loop implementation of discrete periodic convolution
+ *
+ * @param u vector of size n
+ * @param x vector of size n to be convolved with u
+ * @return Eigen::VectorXd discrete periodic convolution of u and x
+ */
+/* SAM_LISTING_BEGIN_1 */
 Eigen::VectorXd pconv(const Eigen::VectorXd &u, const Eigen::VectorXd &x) {
   const int n = x.size();
   assert(n == u.size());
@@ -23,16 +29,28 @@ Eigen::VectorXd pconv(const Eigen::VectorXd &u, const Eigen::VectorXd &x) {
   }
   return z;
 }
+/* SAM_LISTING_END_1 */
 
+/**
+ * @brief Efficient discrete periodic convolution
+ *
+ * @param p vector of size n
+ * @param x vector of size n to be convolved with p
+ * @return Eigen::VectorXd discrete periodic convolution of p and x
+ */
+/* SAM_LISTING_BEGIN_2 */
 Eigen::VectorXd pconv_fast(const Eigen::VectorXd &p, const Eigen::VectorXd &x) {
   const int n = x.size();
   assert(n == p.size());
   Eigen::VectorXd z = Eigen::VectorXd::Zero(n);
 
-  // TO DO: Compute the discrete periodic convolution of p with x
+  // TODO: (4-5.b) Compute the discrete periodic convolution of p with x
   // in an efficient manner.
   // START
- 
+
   // END
   return z;
 }
+/* SAM_LISTING_END_2 */
+
+#endif
