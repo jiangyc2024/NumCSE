edbc8f11b8d48a459fd0d4a212ca3830afd257bb
Maximilian Herde <herdem@student.ethz.ch>
2022-12-05 09:54:37 +0100

    fix

--- Assignments/PolishedCodes/ODE/RK3Prey/solution/rkintegrator.hpp
+++ Assignments/PolishedCodes/ODE/RK3Prey/solution/rkintegrator.hpp
@@ -8,10 +8,10 @@
 #include <vector>
 
 /**
- * @brief A Runge-Kutta explicit solver for a given
+ * \brief A Runge-Kutta explicit solver for a given
  * Butcher tableau for autonomous ODEs.
  *
- * @tparam State State a type representing the space in which the solution
+ * \tparam State State a type representing the space in which the solution
  * lies, e.g. R^d, represented by e.g. VectorXd.
  */
 /* SAM_LISTING_BEGIN_0 */
@@ -19,12 +19,12 @@ template <class State>
 class RKIntegrator {
  public:
   /**
-   * @brief Constructor for the RK method.
+   * \brief Constructor for the RK method.
    * Performs size checks and copies $\VA$ and $\Vb$ into internal storage.
    *
-   * @param A Matrix containing coefficents of the Butcher tableau,
+   * \param A Matrix containing coefficents of the Butcher tableau,
    * must be (strictly) lower triangular (no check is done).
-   * @param b Vector containing coefficients of lower
+   * \param b Vector containing coefficients of lower
    * part of Butcher tableau.
    */
   RKIntegrator(const Eigen::MatrixXd &A, const Eigen::VectorXd &b)
@@ -45,21 +45,21 @@ class RKIntegrator {
   }
 
   /**
-   * @brief Perform the solution of the ODE.
+   * \brief Perform the solution of the ODE.
    * Solve an autonomous ODE $y' = f(y)$, $y(0) = y0$, using a
    * RK scheme given in the Butcher tableau provided in the
    * constructor. Performs $N$ equidistant steps up to time
    * $T$ with initial data $y_0$.
    *
-   * @tparam Function type for function implementing the rhs function.
+   * \tparam Function type for function implementing the rhs function.
    * Must have State operator()(const State & x).
-   * @param f function handle for rhs in $y' = f(y)$, e.g.
+   * \param f function handle for rhs in $y' = f(y)$, e.g.
    * implemented using lambda funciton.
-   * @param T The final time $T$.
-   * @param y0 Initial data $y(0) = y_0$ for $y' = f(y)$.
-   * @param N  Number of steps to perform. Step size is $h = T / N$.
+   * \param T The final time $T$.
+   * \param y0 Initial data $y(0) = y_0$ for $y' = f(y)$.
+   * \param N  Number of steps to perform. Step size is $h = T / N$.
    * Steps are equidistant.
-   * @return std::vector<State> containing all steps $y^n$ (for each $n$)
+   * \return std::vector<State> containing all steps $y^n$ (for each $n$)
    * including initial and final value.
    */
   template <class Function>
@@ -99,17 +99,17 @@ class RKIntegrator {
 
  private:
   /**
-   * @brief Perform a single step of the RK method.
+   * \brief Perform a single step of the RK method.
    * Solve an autonomous ODE using an explicit Runge Kutta Method.
    * Compute a single explicit RK step $y^{n+1} = y_n + \sum \dots$
    * starting from value $y_0$ and storing next value in $y_1$.
    *
-   * @tparam Function  type for function implementing the rhs.
+   * \tparam Function  type for function implementing the rhs.
    * Must have State operator()(State x)
-   * @param f function handle for rhs $f$, s.t. $y' = f(y)$
-   * @param h step size
-   * @param y0 initial state
-   * @param y1 next step $y^{n+1} = y^n + \dots$
+   * \param f function handle for rhs $f$, s.t. $y' = f(y)$
+   * \param h step size
+   * \param y0 initial state
+   * \param y1 next step $y^{n+1} = y^n + \dots$
    */
   template <class Function>
   void step(const Function &f, double h, const State &y0, State &y1) const {
