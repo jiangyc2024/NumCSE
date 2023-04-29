9d5c0852e46569bcff136266b128766f705f7408
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2023-04-29 19:45:00 +0200

    tidying

--- LectureCodes/FuncApproximation/coeffortho/Eigen/coeffortho.hpp
+++ LectureCodes/FuncApproximation/coeffortho/Eigen/coeffortho.hpp
@@ -5,26 +5,32 @@
 /// Repository: https://gitlab.math.ethz.ch/NumCSE/NumCSE/
 /// Do not remove this header.
 //////////////////////////////////////////////////////////////////////////
-# include <cmath>
 # include <Eigen/Dense>
-using Eigen::VectorXd;
+# include <cmath>
 
+namespace coeffortho {
+
+
+using Eigen::VectorXd;
+using Eigen::Index;
+
+inline
 /* SAM_LISTING_BEGIN_0 */
 // Computation of coefficients \Blue{$\alpha$, $\beta$} from \ref{thm:onp:rec}
 // IN : \texttt{t} = points in the definition of the discrete $L^2$-inner product
 //      \texttt{n} = maximal index desired
 //      \texttt{alpha}, \texttt{beta} are used to save coefficients of recursion 
-void coeffortho(const VectorXd& t, const unsigned n, VectorXd& alpha, VectorXd& beta) {
-  const unsigned m = t.size(); // maximal degree of orthogonal polynomial
+void coeffortho(const VectorXd& t, const Index n, VectorXd& alpha, VectorXd& beta) {
+  const Index m = t.size(); // maximal degree of orthogonal polynomial
   alpha = VectorXd( std::min(n-1, m-2) + 1 );
   beta = VectorXd( std::min(n-1, m-2) + 1 );
-  alpha(0) = t.sum()/m;
+  alpha(0) = t.sum()/static_cast<double>(m);
   // initialization of recursion; we store only the values of 
   // the polynomials a the points in \Blue{$\Ct$}
-  VectorXd p0,
-           p1 = VectorXd::Ones(m),
-           p2 = t - alpha(0)*VectorXd::Ones(m);
-  for (unsigned k = 0; k < std::min(n-1, m-2); ++k) {
+  VectorXd p0;
+  VectorXd p1 = VectorXd::Ones(m);
+  VectorXd p2 = t - alpha(0)*VectorXd::Ones(m);
+  for (Index k = 0; k < std::min(n-1, m-2); ++k) {
     p0 = p1; p1 = p2;
     // 3-term recursion \eqref{eq:onp:rec}
     alpha(k+1) = p1.dot(t.cwiseProduct(p1))/p1.squaredNorm();
@@ -33,3 +39,6 @@ void coeffortho(const VectorXd& t, const unsigned n, VectorXd& alpha, VectorXd& 
   }
 }
 /* SAM_LISTING_END_0 */
+
+
+} //namespace coeffortho
--- LectureCodes/FuncApproximation/coeffortho/Eigen/main.cpp
+++ LectureCodes/FuncApproximation/coeffortho/Eigen/main.cpp
@@ -1,12 +1,15 @@
-# include <iostream>
-# include <Eigen/Dense>
 # include "coeffortho.hpp"
+# include <Eigen/Dense>
+# include <iostream>
+
 
 int main() {
-  Eigen::VectorXd a, b, t = Eigen::VectorXd::LinSpaced(10, 0, 1); 
-  const unsigned n = 5;
+  Eigen::VectorXd a;
+  Eigen::VectorXd b;
+  Eigen::VectorXd const t = Eigen::VectorXd::LinSpaced(10, 0, 1); 
+  const Eigen::Index n = 5;
 
-  coeffortho(t, n, a, b);
+  coeffortho::coeffortho(t, n, a, b);
 
   std::cout << a.transpose() << "\n\n" << b.transpose() << "\n";
   return 0;
