a9d976aa3a1494f085dd2d92289222e38260ea83
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2023-05-21 21:45:39 +0200

    tidying

--- LectureCodes/FuncApproximation/trigipequid/Eigen/main.cpp
+++ LectureCodes/FuncApproximation/trigipequid/Eigen/main.cpp
@@ -1,13 +1,15 @@
-# include "./trigipequid.hpp"
+# include "trigipequid.hpp"
 
 int main() {
-  VectorXd y = VectorXd::LinSpaced(5, 0, 1);
-  VectorXcd a, b;
-  trigipequid(y, a, b);
+  const Eigen::VectorXd y = Eigen::VectorXd::LinSpaced(5, 0, 1);
+  Eigen::VectorXcd a;
+  Eigen::VectorXcd b;
+  trigipequid::trigipequid(y, a, b);
   std::cout << "a = " << a.transpose() << "\n"
             << "b = " << b.transpose() << "\n";
-  VectorXd ar,br;
-  std::tie(ar,br) = trigipequid(y);
+  Eigen::VectorXd ar;
+  Eigen::VectorXd br;
+  std::tie(ar,br) = trigipequid::trigipequid(y);
   std::cout << "Re(a) = " << ar.transpose() << "\n"
 	    << "Re(b) = " << br.transpose() << "\n";
   return 0;
--- LectureCodes/FuncApproximation/trigipequid/Eigen/trigipequid.hpp
+++ LectureCodes/FuncApproximation/trigipequid/Eigen/trigipequid.hpp
@@ -1,11 +1,16 @@
-# include <iostream>
+# include <Eigen/Dense>
 # include <cmath> 
 # include <complex>
-# include <Eigen/Dense>
+# include <iostream>
 # include <unsupported/Eigen/FFT>
+
+namespace trigipequid {
+
+
 using Eigen::VectorXd;
 using Eigen::VectorXcd; // complex Eigen vector
 
+inline
 /* SAM_LISTING_BEGIN_0 */
 // Efficient FFT-based computation of coefficients in expansion \eqref{eq:trigpreal}
 // for a trigonometric interpolation polynomial in equidistant points
@@ -15,28 +20,35 @@ using Eigen::VectorXcd; // complex Eigen vector
 // with respect to trigonometric basis from \cref{def:trip}
 std::pair<VectorXd,VectorXd> trigipequid(const VectorXd& y) {
   using index_t = VectorXcd::Index;
-  const index_t N = y.size(), n = (N - 1)/2;
-  if (N % 2 != 1) throw "Number of points must be odd!";
+  const index_t N = y.size();
+  const index_t n = (N - 1)/2;
+  if (N % 2 != 1) {
+    throw std::logic_error("Number of points must be odd!");
+  }
   // prepare data for fft
-  std::complex<double> M_I(0,1); // imaginary unit
+  const std::complex<double> M_I(0,1); // imaginary unit
   // right hand side vector \Blue{$\Vb$} from \eqref{tip:FM}
   VectorXcd b(N);
-  for (index_t k = 0; k < N; ++k) 
-    b(k) = y(k) * std::exp(2*M_PI*M_I*(double(n)/N*k)); 
+  for (index_t k = 0; k < N; ++k) {
+    b(k) = y(k) * std::exp(2*M_PI*M_I*(static_cast<double>(n)/static_cast<double>(N*k))); 
+  }
   Eigen::FFT<double> fft; // DFT helper class
   VectorXcd c = fft.fwd(b); // means that ``c = fft(b)''
 
   // By \eqref{eq:trigpcomp} we can recover
   // \Blue{$\alpha_j = \frac{1}{2}(\gamma_{n-j}+\gamma_{n+j})$} and \Blue{$\beta_j = \frac{1}{2i}(\gamma_{n-j}-\gamma_{n+j})$}, \Blue{$j=1,\ldots,n$}, \Blue{$\alpha_0 = \gamma_n$}.
-  VectorXcd alpha(n + 1),beta(n); alpha(0) = c(n)/((double)N);
+  VectorXcd alpha(n + 1);
+  VectorXcd beta(n);
+  alpha(0) = c(n)/static_cast<double>(N);
   for (index_t l = 1; l <= n; ++l) {
-    alpha(l) = (c(n-l)+c(n+l))/((double)N);
-    beta(l-1) = -M_I*(c(n-l)-c(n+l))/((double)N);
+    alpha(l) = (c(n-l)+c(n+l))/static_cast<double>(N);
+    beta(l-1) = -M_I*(c(n-l)-c(n+l))/static_cast<double>(N);
   }
-  return std::pair<VectorXd,VectorXd>(alpha.real(),beta.real());
+  return {alpha.real(),beta.real()};
 }
 /* SAM_LISTING_END_0 */
 
+inline
 /* SAM_LISTING_BEGIN_1 */
 // Efficient FFT-based computation of coefficients in expansion \eqref{eq:trigpreal}
 // for a trigonometric interpolation polynomial in equidistant points
@@ -51,13 +63,13 @@ void trigipequid(const VectorXd& y, VectorXcd& a, VectorXcd& b) {
   }
   const unsigned n = (N - 1)/2;
   // prepare data for fft
-  std::complex< double > i(0,1); // imaginary unit
+  const std::complex< double > i(0,1); // imaginary unit
   std::vector< std::complex<double> > f(N);
   std::vector< std::complex<double> > c;
 
   for (unsigned k = 0; k < N; ++k) {
     // see \eqref{tip:FM}
-    f[k] = y(k) * std::exp(2*M_PI*i*(double(n)/N*k)); 
+    f[k] = y(k) * std::exp(2*M_PI*i*(static_cast<double>(n)/static_cast<double>(N*k))); 
   }
   Eigen::FFT<double> fft;
   fft.fwd(c, f); // -> c = fft(f);
@@ -75,3 +87,6 @@ void trigipequid(const VectorXd& y, VectorXcd& a, VectorXcd& b) {
   a /= N; b /= N;
 }
 /* SAM_LISTING_END_2 */
+
+
+} //namespace trigipequid
