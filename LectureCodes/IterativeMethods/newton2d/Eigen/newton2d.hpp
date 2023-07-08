b06fe63d63fbf42cbf1534c22e3490893459e414
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2023-07-08 09:54:26 +0200

    tidying

--- LectureCodes/IterativeMethods/newton2d/Eigen/newton2d.hpp
+++ LectureCodes/IterativeMethods/newton2d/Eigen/newton2d.hpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <Eigen/Dense>
 #include <cmath>
+#include <iostream>
 #include <vector>
 
-void newton2d()
+inline void newton2d()
 {
 	// F
 	auto F = [](const Eigen::Vector2d &x){ 
@@ -20,10 +20,12 @@ void newton2d()
 	  	return J;
 	};	
 
-	Eigen::Vector2d x, x_ast, s;
+	Eigen::Vector2d x;
+	Eigen::Vector2d x_ast;
+	Eigen::Vector2d s;
 	x << 0.7, 0.7; // initial value
 	x_ast << 1, 1; // solution
-   	double tol=1E-10;
+   	const double tol=1E-10;
 
 	std::vector<double> errors;
 	errors.push_back((x-x_ast).norm());
@@ -37,15 +39,15 @@ void newton2d()
 	while (s.norm() > tol*x.norm());
 
 	// create eigen vector from std::vector
-	unsigned int n = errors.size();
+	const unsigned int n = errors.size();
 	Eigen::Map<Eigen::VectorXd> err(errors.data(), n);
 	
 	std::cout << "Errors:" << std::endl;
 	std::cout << err << std::endl;
 
 	// compute the convergence rate of each iteration	
-	Eigen::VectorXd logDiff = err.bottomRows(n-1).array().log() - err.topRows(n-1).array().log();
-	Eigen::VectorXd rates = logDiff.bottomRows(n-2).cwiseQuotient(logDiff.topRows(n-2));
+	const Eigen::VectorXd logDiff = err.bottomRows(n-1).array().log() - err.topRows(n-1).array().log();
+	const Eigen::VectorXd rates = logDiff.bottomRows(n-2).cwiseQuotient(logDiff.topRows(n-2));
 	
 	std::cout << "Rates:" << std::endl;
 	std::cout << rates << std::endl;
