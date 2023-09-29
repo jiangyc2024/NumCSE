8420ef83f31b728cd8fab0f0fb4e534a9c6b09e7
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2023-09-29 16:58:42 +0200

    tidying

--- LectureCodes/LeastSquares/pas/Eigen/main.cpp
+++ LectureCodes/LeastSquares/pas/Eigen/main.cpp
@@ -17,8 +17,9 @@ int main () {
 	Eigen::MatrixXd X(2,5);
 	X 	<<	10,	2,	3,	4,	5,
 			2,	4,	8,	1,	25;
-	Eigen::VectorXi i1, i2;
-	std::tie(i1, i2) = princaxissep(X);
+	Eigen::VectorXi i1;
+	Eigen::VectorXi i2;
+	std::tie(i1, i2) = princaxissep::princaxissep(X);
 	std::cout << i1 << std::endl << std::endl << i2 << std::endl;
 	return 0;
 }
