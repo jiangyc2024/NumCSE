7698e8850ef3f246b0123201dce46bbb10d2abcc
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2023-10-30 00:08:20 +0100

    tidying

--- LectureCodes/MatVec/Dense/eigensolve/Eigen/main.cpp
+++ LectureCodes/MatVec/Dense/eigensolve/Eigen/main.cpp
@@ -6,25 +6,29 @@
 /// Do not remove this header.
 //////////////////////////////////////////////////////////////////////////
 /* SAM_LISTING_BEGIN_0 */
-#include <iostream>
 #include <Eigen/Dense>
-using namespace Eigen;
-using namespace std;
+#include <iostream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Eigen::RowVectorXd;
+using std::cout;
+using std::endl;
 
 int main(){	
-	int n = 10;
+	const int n = 10;
 	// Initialize a special invertible matrices
-	MatrixXd mat = MatrixXd::Identity(n,n) +
+	const MatrixXd mat = MatrixXd::Identity(n,n) +
 	VectorXd::Constant(n,1.0)*RowVectorXd::Constant(n,1.0);
 	cout << "Matrix mat = " << endl << mat << endl;
 	// Multiple right hand side vectors stored in matrix, cf. \matlab{}
-	MatrixXd B = MatrixXd::Random(n,2);
+	const MatrixXd B = MatrixXd::Random(n,2);
 	// Solve linear system using various decompositions
-	MatrixXd X = mat.lu().solve(B);
-	MatrixXd X2 = mat.fullPivLu().solve(B);
-	MatrixXd X3 = mat.householderQr().solve(B);
-	MatrixXd X4 = mat.llt().solve(B);
-	MatrixXd X5 = mat.ldlt().solve(B);
+	const MatrixXd X = mat.lu().solve(B);
+	const MatrixXd X2 = mat.fullPivLu().solve(B);
+	const MatrixXd X3 = mat.householderQr().solve(B);
+	const MatrixXd X4 = mat.llt().solve(B);
+	const MatrixXd X5 = mat.ldlt().solve(B);
 	cout << "|X2-X| = " << (X2-X).norm() << endl;
 	cout << "|X3-X| = " << (X3-X).norm() << endl;
 	cout << "|X4-X| = " << (X4-X).norm() << endl;
