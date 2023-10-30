93db1622fcb1ecbd3d32756ff65ccbceddefccb8
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2023-10-30 15:16:15 +0100

    tidying

--- LectureCodes/MatVec/Dense/epsaddone/Eigen/epsaddone.cpp
+++ LectureCodes/MatVec/Dense/epsaddone/Eigen/epsaddone.cpp
@@ -8,12 +8,13 @@
 
 #include <iostream>
 #include <limits> 
-using namespace std;
+using std::cout;
+using std::endl;
 int main(){	
 /* SAM_LISTING_BEGIN_0 */
 cout.precision(25);
-double eps = numeric_limits<double>::epsilon();
-cout << fixed << 1.0 + 0.5*eps << endl
+const double eps = std::numeric_limits<double>::epsilon();
+cout << std::fixed << 1.0 + 0.5*eps << endl
 	 << 1.0 - 0.5*eps << endl
   	 << (1.0 + 2/eps) - 2/eps << endl;
 /* SAM_LISTING_END_0 */
