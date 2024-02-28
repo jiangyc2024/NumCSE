80527025ec030b251b22a9d4ac90b0da7f4a79fd
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2024-02-28 20:49:48 +0100

    tidying rowbandwidth

--- LectureCodes/MatVec/Sparse/rowbandwidth/Eigen/main.cpp
+++ LectureCodes/MatVec/Sparse/rowbandwidth/Eigen/main.cpp
@@ -14,40 +14,45 @@
 
 #include "bandwidth.hpp"
 
-using namespace Eigen;
+using Eigen::Triplet;
+using Eigen::MatrixXd;
+using Eigen::SparseMatrix;
 
 int main() {
-  int n = 7;
+  const int n = 7;
   typedef Triplet<double> triplet_t;
   std::vector<triplet_t> triplets;
 
+  triplets.reserve(n);
   // example in lecture document ex:env
-  for (int i = 0; i < n; ++i) triplets.push_back({i, i, 1.0});  // diagonal
-  triplets.push_back({0, 2, 1.0});
-  triplets.push_back({1, 4, 1.0});
-  triplets.push_back({2, 0, 1.0});
-  triplets.push_back({2, 6, 1.0});
-  triplets.push_back({3, 4, 1.0});
-  triplets.push_back({3, 6, 1.0});
-  triplets.push_back({4, 1, 1.0});
-  triplets.push_back({4, 3, 1.0});
-  triplets.push_back({4, 5, 1.0});
-  triplets.push_back({5, 4, 1.0});
-  triplets.push_back({6, 2, 1.0});
-  triplets.push_back({6, 3, 1.0});
+  for (int i = 0; i < n; ++i) {
+    triplets.emplace_back(i, i, 1.0);  // diagonal
+  }
+  triplets.emplace_back(0, 2, 1.0);
+  triplets.emplace_back(1, 4, 1.0);
+  triplets.emplace_back(2, 0, 1.0);
+  triplets.emplace_back(2, 6, 1.0);
+  triplets.emplace_back(3, 4, 1.0);
+  triplets.emplace_back(3, 6, 1.0);
+  triplets.emplace_back(4, 1, 1.0);
+  triplets.emplace_back(4, 3, 1.0);
+  triplets.emplace_back(4, 5, 1.0);
+  triplets.emplace_back(5, 4, 1.0);
+  triplets.emplace_back(6, 2, 1.0);
+  triplets.emplace_back(6, 3, 1.0);
   SparseMatrix<double> A(n, n);
   A.setFromTriplets(triplets.begin(), triplets.end());
   A.makeCompressed();
 
-  MatrixXd B(A);
+  const MatrixXd B(A);
 
   std::cout << A << std::endl;
   std::cout << B << std::endl;
 
-  std::cout << "Rowbandwidth\n" << rowbandwidth(A) << std::endl << std::endl;
-  std::cout << "Rowbandwidth\n" << rowbandwidth(B) << std::endl << std::endl;
+  std::cout << "Rowbandwidth\n" << bandwidth::rowbandwidth(A) << std::endl << std::endl;
+  std::cout << "Rowbandwidth\n" << bandwidth::rowbandwidth(B) << std::endl << std::endl;
 
-  std::cout << "colbandwidth\n" << colbandwidth(A) << std::endl << std::endl;
-  std::cout << "colbandwidth\n" << colbandwidth(B) << std::endl << std::endl;
+  std::cout << "colbandwidth\n" << bandwidth::colbandwidth(A) << std::endl << std::endl;
+  std::cout << "colbandwidth\n" << bandwidth::colbandwidth(B) << std::endl << std::endl;
   return 0;
 }
