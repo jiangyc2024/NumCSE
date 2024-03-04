17cc041c2786769ff12c4332752c5b77497f0418
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2024-03-04 15:33:19 +0100

    tidying totriplets

--- LectureCodes/MatVec/Sparse/totriplets/main.cpp
+++ LectureCodes/MatVec/Sparse/totriplets/main.cpp
@@ -11,23 +11,24 @@
 int main(int /*argc*/, char ** /*argv*/) {
   // Initialize a (small) sparse matrix
   std::vector<Eigen::Triplet<double>> trp;
-  trp.push_back(Eigen::Triplet<double>(0, 0, 3.0));
-  trp.push_back(Eigen::Triplet<double>(0, 2, 4.0));
-  trp.push_back(Eigen::Triplet<double>(1, 2, 1.0));
-  trp.push_back(Eigen::Triplet<double>(2, 1, 2.0));
-  trp.push_back(Eigen::Triplet<double>(2, 3, 5.0));
-  trp.push_back(Eigen::Triplet<double>(2, 3, 1.0));
-  trp.push_back(Eigen::Triplet<double>(3, 0, 4.0));
+  trp.emplace_back(0, 0, 3.0);
+  trp.emplace_back(0, 2, 4.0);
+  trp.emplace_back(1, 2, 1.0);
+  trp.emplace_back(2, 1, 2.0);
+  trp.emplace_back(2, 3, 5.0);
+  trp.emplace_back(2, 3, 1.0);
+  trp.emplace_back(3, 0, 4.0);
 
-  int rows = 4, cols = 4;
+  const int rows = 4;
+  const int cols = 4;
   Eigen::SparseMatrix<double> A(rows, cols);
   A.setFromTriplets(trp.begin(), trp.end());
   A.makeCompressed();
   std::cout << "Sparse matrix A = " << std::endl << A << std::endl;
   // Extract triplets
-  std::vector<Eigen::Triplet<double>> extr_trip{convertToTriplets(A)};
+  const std::vector<Eigen::Triplet<double>> extr_trip{convertToTriplets(A)};
   // List triplets
-  for (Eigen::Triplet<double> &triplet : extr_trip) {
+  for (const Eigen::Triplet<double> &triplet : extr_trip) {
     std::cout << "triplet (" << triplet.row() << ", " << triplet.col() << ", "
               << triplet.value() << ")" << std::endl;
   }
