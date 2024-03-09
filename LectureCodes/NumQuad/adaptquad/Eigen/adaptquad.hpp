b41734ea24ae8662d990f094a155f10b7a7392e5
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2024-03-09 21:03:15 +0100

    tidying adaptquad

--- LectureCodes/NumQuad/adaptquad/Eigen/adaptquad.hpp
+++ LectureCodes/NumQuad/adaptquad/Eigen/adaptquad.hpp
@@ -1,5 +1,8 @@
-# include <vector>
 # include <Eigen/Dense>
+# include <vector>
+
+namespace adaptquad {
+
 
 using Eigen::VectorXd;
 
@@ -9,12 +12,18 @@ using Eigen::VectorXd;
 template <class Function>
 double adaptquad(Function& f, VectorXd& M,double rtol,double atol) {
   const std::size_t n = M.size(); // number of nodes
-  VectorXd h = M.tail(n-1)-M.head(n-1), // distance of quadature nodes \Label[line]{aq:1}
-           mp = 0.5*(M.head(n-1)+M.tail(n-1)); // midpoints \Label[line]{aq:2}
+  // distance of quadature nodes \Label[line]{aq:1}
+  const VectorXd h = M.tail(n-1)-M.head(n-1); 
+  const VectorXd mp = 0.5*(M.head(n-1)+M.tail(n-1)); // midpoints \Label[line]{aq:2}
   // Values of integrand at nodes and midpoints \Label[line]{aq:3}
-  VectorXd fx(n), fm(n - 1);
-  for (unsigned i = 0; i < n; ++i) fx(i) = f(M(i));
-  for (unsigned j = 0; j < n - 1; ++j) fm(j) = f(mp(j)); 
+  VectorXd fx(n);
+  VectorXd fm(n - 1);
+  for (unsigned i = 0; i < n; ++i) {
+    fx(i) = f(M(i));
+  }
+  for (unsigned j = 0; j < n - 1; ++j) {
+    fm(j) = f(mp(j)); 
+  }
   // trapezoidal rule \eqref{eq:comptrap} \Label[line]{aq:4}
   const VectorXd trp_loc = 1./4*h.cwiseProduct(fx.head(n-1)+2*fm+fx.tail(n-1));
   // Simpson rule \eqref{eq:compsimp} \Label[line]{aq:5}
@@ -33,20 +42,24 @@ double adaptquad(Function& f, VectorXd& M,double rtol,double atol) {
     std::vector<double> new_cells;
     for (unsigned i = 0; i < est_loc.size(); ++i) {
       // \com{MARK} by criterion \eqref{eq:mark} \& \com{REFINE} by \eqref{eq:aqref}
-      if (est_loc(i) > 0.9/(n-1)*err_tot) { 
+      if (est_loc(i) > 0.9/static_cast<double>(n-1)*err_tot) { 
         // new quadrature point = midpoint of interval with large error
         new_cells.push_back(mp(i));
       }}
 
     // create new set of quadrature nodes
     // (necessary to convert std::vector to Eigen vector)
-    Eigen::Map<VectorXd> tmp(new_cells.data(), new_cells.size()); 
+    const Eigen::Map<VectorXd> tmp(new_cells.data(), 
+      static_cast<Eigen::Index>(new_cells.size())); 
     VectorXd new_M(M.size() + tmp.size()); 
     new_M << M, tmp; // concatenate old cells and new cells
     // nodes of a mesh are supposed to be sorted
-    std::sort(new_M.data(),new_M.data()+new_M.size()); 
+    std::sort(new_M.begin(),new_M.end()); 
     I = adaptquad(f, new_M, rtol, atol); // recursion \Label[line]{aq:10}
   }
   return I;
 }
 /* SAM_LISTING_END_0 */
+
+
+} //namespace adaptquad
--- LectureCodes/NumQuad/adaptquad/Eigen/main.cpp
+++ LectureCodes/NumQuad/adaptquad/Eigen/main.cpp
@@ -1,12 +1,15 @@
 # include "./adaptquad.hpp"
-# include <iostream>
+# include <Eigen/Dense>
 # include <cmath>
+# include <iostream>
+
+using Eigen::VectorXd;
 
 int main () {
   auto f =[](double x) { return std::exp(-x*x); };
   VectorXd M(4);
   M << -100, 0.1, 0.5, 100;
   std::cout << "Sqrt(Pi) - Int_{-100}^{100} exp(-x*x) dx = ";
-  std::cout << adaptquad(f, M, 1e-10, 1e-12) - std::sqrt(M_PI) << "\n";
+  std::cout << adaptquad::adaptquad(f, M, 1e-10, 1e-12) - std::sqrt(M_PI) << "\n";
   return 0;
 }
