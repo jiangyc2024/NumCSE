2de26b3139c5704e0a6e794efd146ee12a937b8c
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2024-03-14 20:36:36 +0100

    cleanup expressiontemplates

--- LectureCodes/EigenTutorial/expressiontemplates.cpp
+++ LectureCodes/EigenTutorial/expressiontemplates.cpp
@@ -6,43 +6,6 @@
 #include <Eigen/Dense>
 using Eigen::MatrixXd;
 
-/*
-
-// Invert D as expression template
-// Not forcing the type of certain eigen expressions enables internal
-// optimizations in eigen with expression templates (by lazy evaluation)
-void fastInvertDiagonal() {
-  auto D = M.diagonal().asDiagonal();
-  R = D.inverse();
-}
-
-// Invert D (forced) as dense matrix
-void slowInvertDiagonal() {
-  MatrixXd D = M.diagonal().asDiagonal();
-  R = D.inverse();
-}
-
-// Add three matrices as one line
-// Large expressions give eigen more opportunities for optimization, in this
-// case eigen can compile the whole computation into one loop instead of 3 (or
-// more)
-void fastAddThree() { R = 3 * M + 4 * A + 5 * B; }
-
-// Add three matrices with intermediate evaluations
-void slowAddThree() {
-  R = 3 * M;
-  R += 4 * A;
-  R += 5 * B;
-}
-
-void init(size_t s) {
-  M = MatrixXd::Random(s, s);
-  A = MatrixXd::Random(s, s);
-  B = MatrixXd::Random(s, s);
-}
-
-*/
-
 int main() {
 
   MatrixXd M;
