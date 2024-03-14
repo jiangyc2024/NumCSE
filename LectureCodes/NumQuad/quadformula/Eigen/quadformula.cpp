56ed0a3820a8d56fbac70955472ecc62824653ba
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2024-03-14 20:29:33 +0100

    tidying quadformula

--- LectureCodes/NumQuad/quadformula/Eigen/quadformula.cpp
+++ LectureCodes/NumQuad/quadformula/Eigen/quadformula.cpp
@@ -6,12 +6,12 @@
 /// Do not remove this header.
 //////////////////////////////////////////////////////////////////////////
 
-#include <iostream>
 #include "quadformula.hpp"
+#include <iostream>
 
 int main(int /*argc*/, char **/*argv*/) {
-  Eigen::VectorXd c { (Eigen::VectorXd(4) << 0.0,0.33,0.66,1.0).finished() };
-  Eigen::VectorXd w { 0.25 * Eigen::VectorXd::Ones(4) };
+  const Eigen::VectorXd c { (Eigen::VectorXd(4) << 0.0,0.33,0.66,1.0).finished() };
+  const Eigen::VectorXd w { 0.25 * Eigen::VectorXd::Ones(4) };
   std::cout << "integral = " << quadformula([] (double t) { return t*t; }, c, w) << std::endl;
   return 0;
 }
--- LectureCodes/NumQuad/quadformula/Eigen/quadformula.hpp
+++ LectureCodes/NumQuad/quadformula/Eigen/quadformula.hpp
@@ -8,18 +8,16 @@
 
 #include <Eigen/Dense>
 
-using Eigen::VectorXd;
-
 /* SAM_LISTING_BEGIN_0 */
 // Generic numerical quadrature routine implementing \eqref{eq:quadform}: 
 // \texttt{f} is a handle to a function, e.g. as lambda function
 // \texttt{c}, \texttt{w} pass quadrature nodes \Blue{$\qn_j\in [a,b]$}, and weights \Blue{$\qw_j\in\bbR$}
 // in a Eigen::VectorXd
 template <class Function>
-double quadformula(Function &&f, const VectorXd& c,const VectorXd& w) {
-  const std::size_t n = c.size();
+double quadformula(Function &&f, const Eigen::VectorXd& c,const Eigen::VectorXd& w) {
+  const Eigen::Index n = c.size();
   double I = 0;
-  for (std::size_t i = 0; i < n; ++i) { I += w(i)*f(c(i)); }
+  for (Eigen::Index i = 0; i < n; ++i) { I += w(i)*f(c(i)); }
   return I;
 }
 /* SAM_LISTING_END_0 */
