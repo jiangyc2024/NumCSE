da7f27cd7ee0cfdf3b5284abf05d587b2b9e1277
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2024-05-01 09:54:22 +0200

    tidying ode adpative step-size control integrator

--- LectureCodes/SingleStepMethods/ode45/Eigen_new/odeintssctrl.h
+++ LectureCodes/SingleStepMethods/ode45/Eigen_new/odeintssctrl.h
@@ -13,37 +13,45 @@
 /* SAM_LISTING_BEGIN_0 */
 // Auxiliary function: default norm for an \eigen vector type
 template <class State>
-double _norm(const State &y) { return y.norm(); }
+double defaultnorm(const State &y) { return y.norm(); }
+// Auxilary struct to hold user options
+struct Odeintssctrl_options {
+  double T;       // terminal time
+  double h0;      // initial time step
+  double reltol;  // norm-relative error tolerance
+  double abstol;  // absolute error tolerance
+  double hmin;    // smallest allowed time step
+} __attribute__((aligned(64)));
 // Adaptive single-step integrator
 template <class DiscEvolOp, class State,
-          class NormFunc = decltype(_norm<State>)>
+          class NormFunc = decltype(defaultnorm<State>)>
 std::vector<std::pair<double, State>> odeintssctrl(
     DiscEvolOp &&Psilow, unsigned int p, DiscEvolOp &&Psihigh, const State &y0,
-    double T, double h0, double reltol, double abstol, double hmin,
-    NormFunc &norm = _norm<State>) {
-  double t = 0;   // initial time $\cob{t_0=0}$\Label[line]{odeintadapt:1}
-  State y = y0;   // current state, initialized here
-  double h = h0;  // timestep to start with
+    const Odeintssctrl_options &opt,
+    NormFunc &norm = defaultnorm<State>) {
+  double t = 0;           // initial time $\cob{t_0=0}$\Label[line]{odeintadapt:1}
+  State y = y0;           // current state, initialized here
+  double h = opt.h0;  // timestep to start with
   // Array for returning pairs of times/states $\cob{\left(t_k,\Vy_k\right)_k}$
   std::vector<std::pair<double, State>> states;  
   states.push_back({t, y}); // initial time and state
   // Main timestepping loop
-  while ((states.back().first < T) && (h >= hmin)) {  // \Label[line]{ssctrl:2}
-     State yh = Psihigh(h, y);  // high-order discrete evolution \Blue{$\widetilde{\Psibf}^h$}\Label[line]{ssctrl:3}
+  while ((states.back().first < opt.T) && (h >= opt.hmin)) {  // \Label[line]{ssctrl:2}
+    State yh = Psihigh(h, y);  // high-order discrete evolution \Blue{$\widetilde{\Psibf}^h$}\Label[line]{ssctrl:3}
     State yH = Psilow(h, y);  // low-order discrete evolution \Blue{${\Psibf}^h$}\Label[line]{ssctrl:4}
-    double est = norm(yH - yh);  // $\leftrightarrow$ \Blue{$\mathrm{EST}_k$}\Label[line]{ssctrl:5}
-    double tol = std::max(reltol * norm(y), abstol);  // effective tolerance \Label[line]{ssctrl:6a}
+    const double est = norm(yH - yh);  // $\leftrightarrow$ \Blue{$\mathrm{EST}_k$}\Label[line]{ssctrl:5}
+    const double tol = std::max(opt.reltol * norm(y), opt.abstol);  // effective tolerance \Label[line]{ssctrl:6a}
     // Optimal stepsize according to \eqref{eq:ssc}
     if (est < tol) {  // step \Magenta{accepted} \Label[line]{ssctrl:7}\Label[line]{ssctrl:6}
       // store next approximate state
-      states.push_back({t = t + std::min(T - t, h), y = yh});  
+      states.push_back({t = t + std::min(opt.T - t, h), y = yh});  
     }
     // New timestep size according to \eqref{eq:ssc}
     h *= std::max(0.5, std::min(2., 0.9 * std::pow(tol / est, 1. / (p + 1)))); //\Label[line]{ssctrl:6b}
-    if (h < hmin) {
+    if (h < opt.hmin) {
       std::cerr << "Warning: Failure at t=" << states.back().first
           << ". Unable to meet integration tolerances without reducing the step"
-          << " size below the smallest value allowed (" << hmin
+          << " size below the smallest value allowed (" << opt.hmin
           << ") at time t = " << t << "." << std::endl;
     }
   } // end main loop
--- LectureCodes/SingleStepMethods/ode45/Eigen_new/odeintssctrltest.cc
+++ LectureCodes/SingleStepMethods/ode45/Eigen_new/odeintssctrltest.cc
@@ -9,32 +9,30 @@ int main() {
   using State_t = double;
   using DiscEvolOp = std::function<State_t(double, State_t)>;
   // Definition of right-hand-side for ODE
-  auto f = [](double y) { return y * y; };
+  const auto f = [](double y) { return y * y; };
   // Initial value
-  double y0 = 0.5;
+  const double y0 = 0.5;
   // Exact solution
   auto y = [](double t) { return 1.0 / (2.0 - t); };
-  // Final time
-  double T = 1.9;
   // The norm is just the modulus in the scalar case
-  auto norm = [](double x) { return std::fabs(x); };
+  const auto norm = [](double x) { return std::fabs(x); };
 
   // Low-order method: explicit euler (order 1)
-  DiscEvolOp psilow = [&](double h, double y) { return y + h * f(y); };
+  const DiscEvolOp psilow = [&](double h, double y) { return y + h * f(y); };
 
   // "High-order method": explicit trapezoidal rule (order 2)
-  DiscEvolOp psihigh = [&](double h, double y) {
-    double k1 = f(y);
-    double k2 = f(y + h * k1);
+  const DiscEvolOp psihigh = [&](double h, double y) {
+    const double k1 = f(y);
+    const double k2 = f(y + h * k1);
     return y + (h / 2.) * (k1 + k2);
   };
   // Call simple adaptive integrator
-  std::vector<std::pair<double, double>> states =
-      odeintssctrl(psilow, 1, psihigh, y0, 1.9, 0.2, 1e-3, 1e-4, 1e-4, norm);
+  const std::vector<std::pair<double, double>> states =
+      odeintssctrl(psilow, 1, psihigh, y0, {.T=1.9, .h0=0.2, .reltol=1e-3, .abstol=1e-4, .hmin=1e-4}, norm);
   // Output solution and error
   std::cout << "Adaptive integration, " << states.size() - 1 << " timesteps"
             << std::endl;
-  for (auto &ty : states) {
+  for (const auto &ty : states) {
     std::cout << "t = " << ty.first << ": y = " << ty.second
               << ", error = " << norm(ty.second - y(ty.first)) << std::endl;
   }
