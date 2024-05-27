f9b6e2e9ba91c3d417cea3cfa752011dbb824254
Heinrich Grattenthaler <heinrich@grattenthaler.de>
2024-05-27 12:09:44 +0200

    lint simpleblowupode

--- LectureCodes/SingleStepMethods/simpleblowupode45/Eigen/main.cpp
+++ LectureCodes/SingleStepMethods/simpleblowupode45/Eigen/main.cpp
@@ -1,20 +1,21 @@
 #include "ode45.hpp"
 #include <iostream> 
 
-
+//NOLINTBEGIN(bugprone-exception-escape)
 int main()
 {
 	auto f = [](double x){ return pow(x,2); };
-	double y0 = 1;
-	double T = 2;
+	const double y0 = 1;
+	const double T = 2;
 
 
-	ode45<double> ode = ode45<double>(f);
+	Ode45<double> ode = Ode45<double>(f);
 
-	std::vector<std::pair<double, double>> states = ode.solve(y0, T);
+	const std::vector<std::pair<double, double>> states = ode.solve(y0, T);
 
 	for (auto state : states)
 	{
 		std::cout << "t = " << state.first << ", y = " << state.second << std::endl;
 	}
 }
+//NOLINTEND(bugprone-exception-escape)
